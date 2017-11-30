f41640558640ce0aa4eb4e98508a411c7cad602f
Wenquan Xing <wenquanx@uber.com>
2017-11-30 10:15:43 -0800

    ?

---
+++ common/collection/defs.go
@@ -0,0 +1,6 @@
+package collection
+
+const (
+	// UUIDStringLength is the length of an UUID represented as a hex string
+	UUIDStringLength = 36 // xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+)
---
+++ common/collection/util.go
@@ -0,0 +1,53 @@
+package collection
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+)
+
+// UUIDHashCode is a hash function for hashing string uuid
+// if the uuid is malformed, then the hash function always
+// returns 0 as the hash value
+func UUIDHashCode(key string) uint32 {
+	if len(key) != UUIDStringLength {
+		return 0
+	}
+	// Use the first 4 bytes of the uuid as the hash
+	b, err := hex.DecodeString(key[:8])
+	if err != nil {
+		return 0
+	}
+	return binary.BigEndian.Uint32(b)
+}
+
+// MinInt returns the min of given two integers
+func MinInt(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// MaxInt returns the max of given two integers
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// MinInt64 returns the min of given two integers
+func MinInt64(a, b int64) int64 {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// MaxInt64 returns the max of given two integers
+func MaxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
