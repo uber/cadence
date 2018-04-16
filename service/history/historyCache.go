84e1128f0ceb37131a73753ccdedef9382d0a3bb
Wenquan Xing <wenquanx@uber.com>
2018-04-16 14:30:24 -0700

    should clear if EntityNotExistsError, since EntityNotExistsError can be thrown byt domain cache

--- service/history/historyCache.go
+++ service/history/historyCache.go
@@ -123,14 +123,14 @@ func (c *historyCache) getOrCreateWorkflowExecution(domainID string,
 		if atomic.CompareAndSwapInt32(&status, cacheNotReleased, cacheReleased) {
 			if err != nil {
 				switch err.(type) {
-				case *workflow.EntityNotExistsError:
-					// do not clear cache for this type: EntityNotExistsError
 				case *workflow.CancellationAlreadyRequestedError:
 					// do not clear cache for this type: CancellationAlreadyRequestedError
 				case *workflow.WorkflowExecutionAlreadyStartedError:
 					// do not clear cache for this type: WorkflowExecutionAlreadyStartedError
 				case *h.EventAlreadyStartedError:
 					// do not clear cache for this type: EventAlreadyStartedError
+				// we cannot skip the clear of mutable state if there is entity not exist error
+				// since sometimes the domain cache is used and there can be error thrown by it.
 				// we cannot skip the clear of mutable state if there is a bad request,
 				// since the decision is translated to history events in a loop and we do not do the check
 				// at the very begining.
