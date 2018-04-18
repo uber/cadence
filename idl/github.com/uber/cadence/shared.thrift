bce641300ac6726015638903358b500b2c544f30
Wenquan Xing <wenquanx@uber.com>
2018-04-18 10:05:45 -0700

    address comments

--- idl/github.com/uber/cadence/shared.thrift
+++ idl/github.com/uber/cadence/shared.thrift
@@ -676,10 +676,10 @@ struct ChildWorkflowExecutionTerminatedEventAttributes {
 }
 
 struct HistoryEvent {
-  01:  optional i64 (js.type = "Long") version
   10:  optional i64 (js.type = "Long") eventId
   20:  optional i64 (js.type = "Long") timestamp
   30:  optional EventType eventType
+  35:  optional i64 (js.type = "Long") version
   40:  optional WorkflowExecutionStartedEventAttributes workflowExecutionStartedEventAttributes
   50:  optional WorkflowExecutionCompletedEventAttributes workflowExecutionCompletedEventAttributes
   60:  optional WorkflowExecutionFailedEventAttributes workflowExecutionFailedEventAttributes
