c1731e83ead3a607a8f6503ba36848c39ef1d0d4
Quanzheng Long <longer@uber.com>
2018-05-18 16:26:27 -0700

    add admin.thrift

---
+++ idl/github.com/uber/cadence/admin.thrift
@@ -0,0 +1,49 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace java com.uber.cadence.admin
+
+include "shared.thrift"
+
+exception AccessDeniedError {
+  1: required string message
+}
+
+/**
+* AdminService provides advanced APIs for debugging and analysis with admin privillege
+**/
+service AdminService {
+  /**
+  * InquiryWorkflowExecution returns information about the internal states of workflow execution.
+  **/
+  InquiryWorkflowExecutionResponse InquiryWorkflowExecution(1: shared.DescribeWorkflowExecutionRequest inquiryRequest)
+    throws (
+      1: shared.BadRequestError badRequestError,
+      2: shared.InternalServiceError internalServiceError,
+      3: shared.EntityNotExistsError entityNotExistError,
+      4: AccessDeniedError accessDeniedError,
+    )
+}
+
+struct InquiryWorkflowExecutionResponse{
+  10: optional string shardId
+  20: optional string historyAddr
+  30: optional string matchingAddr
+}
