a820c202a428f146c08c659f2e7b6de3fe9659b8
Maxim Fateev <maxim@uber.com>
2018-09-14 16:42:27 -0700

    Fixed default schemaDir

--- common/persistence/cassandra/cassandraPersistenceTest.go
+++ common/persistence/cassandra/cassandraPersistenceTest.go
@@ -80,7 +80,7 @@ func InitTestSuiteWithMetadata(tb *persistencetests.TestBase, options *persisten
 		panic("nil metadata")
 	}
 	if options.SchemaDir == "" {
-		options.SchemaDir = "schema"
+		options.SchemaDir = testSchemaDir
 	}
 	log := bark.NewLoggerFromLogrus(log.New())
 	tb.PersistenceTestCluster = &TestCluster{}
