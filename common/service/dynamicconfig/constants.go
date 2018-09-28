fe1522a41237166cfe3ea2eca80c24342ba484c5
Quanzheng Long <longer@uber.com>
2018-09-28 16:32:17 -0700

    Add dynamic config for encoding type

--- common/service/dynamicconfig/constants.go
+++ common/service/dynamicconfig/constants.go
@@ -129,6 +129,7 @@ var keys = map[Key]string{
 	MaximumBufferedEventsBatch:                            "history.maximumBufferedEventsBatch",
 	ShardUpdateMinInterval:                                "history.shardUpdateMinInterval",
 	ShardSyncMinInterval:                                  "history.shardSyncMinInterval",
+	EventEncodingPropertyFilteredByDomainKey:              "history.eventEncodingPropertyFilteredByDomainKey",
 
 	// worker settings
 	WorkerPersistenceMaxQPS: "worker.persistenceMaxQPS",
@@ -308,7 +309,8 @@ const (
 	ShardUpdateMinInterval
 	// ShardSyncMinInterval is the minimal time interval which the shard info should be sync to remote
 	ShardSyncMinInterval
-
+	// EventEncodingPropertyFilteredByDomainKey is the encoding type for history events
+	EventEncodingPropertyFilteredByDomainKey
 	// key for histoworkerry
 
 	// WorkerPersistenceMaxQPS is the max qps worker host can query DB
