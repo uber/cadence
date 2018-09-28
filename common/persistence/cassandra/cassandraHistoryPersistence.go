336ca471c6e860b9101d601eb0f5b7e30add56de
Quanzheng Long <longer@uber.com>
2018-09-28 14:16:53 -0700

    remove debug code

--- common/persistence/cassandra/cassandraHistoryPersistence.go
+++ common/persistence/cassandra/cassandraHistoryPersistence.go
@@ -161,7 +161,6 @@ func (h *cassandraHistoryPersistence) GetWorkflowExecutionHistory(request *p.Int
 
 	found := false
 	nextPageToken := iter.PageState()
-	fmt.Printf("longer nextpage: %v, %v, %v, %v", nextPageToken, request.FirstEventID, request.NextEventID, request.PageSize)
 
 	//NOTE: in this method, we need to make sure is NOT decreasing(otherwise we skip the events)
 	lastEventBatchVersion := request.LastEventBatchVersion
