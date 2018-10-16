d12d0a5afeb15299dd6bc584435f3754aeb2ff66
Venkatraghavan Srinivasan <venkat@uber.com>
2018-10-16 16:07:42 -0700

    revert to v56

--- common/persistence/sql/sqlPersistenceTest.go
+++ common/persistence/sql/sqlPersistenceTest.go
@@ -37,7 +37,7 @@ const (
 	testPort                 = 3306
 	testUser                 = "uber"
 	testPassword             = "uber"
-	testSchemaDir            = "schema/mysql/v57"
+	testSchemaDir            = "schema/mysql/v56"
 )
 
 // TestCluster allows executing cassandra operations in testing.
