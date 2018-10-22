0404ce59fdda34f6c5c0a470d7c19b189b72d30b
Quanzheng Long <longer@uber.com>
2018-10-21 19:41:14 -0700

    disable perf test

--- common/persistence/persistence-tests/cassandraPerf_test.go
+++ common/persistence/persistence-tests/cassandraPerf_test.go
@@ -20,16 +20,10 @@
 
 package persistencetests
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/suite"
-)
-
 // Manually enable the test when needed
-func TestCassandraHistoryPerformance(t *testing.T) {
-	s := new(HistoryPerfSuite)
-	s.TestBase = NewTestBaseWithCassandra(&TestBaseOptions{})
-	s.TestBase.Setup()
-	suite.Run(t, s)
-}
+//func TestCassandraHistoryPerformance(t *testing.T) {
+//	s := new(HistoryPerfSuite)
+//	s.TestBase = NewTestBaseWithCassandra(&TestBaseOptions{})
+//	s.TestBase.Setup()
+//	suite.Run(t, s)
+//}
