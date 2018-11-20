f62930431fb84703b773b2e7765c43ee5350949c
Samar Abbas <samar@uber.com>
2018-11-20 15:12:42 -0800

    fix unit test

--- common/service/dynamicconfig/constants.go
+++ common/service/dynamicconfig/constants.go
@@ -131,6 +131,7 @@ var keys = map[Key]string{
 	ExecutionMgrNumConns:                                  "history.executionMgrNumConns",
 	HistoryMgrNumConns:                                    "history.historyMgrNumConns",
 	MaximumBufferedEventsBatch:                            "history.maximumBufferedEventsBatch",
+	MaximumSignalsPerExecution:                            "history.maximumSignalsPerExecution",
 	ShardUpdateMinInterval:                                "history.shardUpdateMinInterval",
 	ShardSyncMinInterval:                                  "history.shardSyncMinInterval",
 	DefaultEventEncoding:                                  "history.defaultEventEncoding",
