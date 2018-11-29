6dbc253125654c24a801419393b4a160e91e001e
Samar Abbas <samar@uber.com>
2018-11-28 17:48:30 -0800

    remove unused function from mock

--- service/history/MockMutableState.go
+++ service/history/MockMutableState.go
@@ -1135,29 +1135,6 @@ func (_m *mockMutableState) GetActivityScheduledEvent(_a0 int64) (*shared.Histor
 	return r0, r1
 }
 
-// GetActivityStartedEvent provides a mock function with given fields: _a0
-func (_m *mockMutableState) GetActivityStartedEvent(_a0 int64) (*shared.HistoryEvent, bool) {
-	ret := _m.Called(_a0)
-
-	var r0 *shared.HistoryEvent
-	if rf, ok := ret.Get(0).(func(int64) *shared.HistoryEvent); ok {
-		r0 = rf(_a0)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*shared.HistoryEvent)
-		}
-	}
-
-	var r1 bool
-	if rf, ok := ret.Get(1).(func(int64) bool); ok {
-		r1 = rf(_a0)
-	} else {
-		r1 = ret.Get(1).(bool)
-	}
-
-	return r0, r1
-}
-
 // GetAllBufferedReplicationTasks provides a mock function with given fields:
 func (_m *mockMutableState) GetAllBufferedReplicationTasks() map[int64]*persistence.BufferedReplicationTask {
 	ret := _m.Called()
