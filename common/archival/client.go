66f00fdbdd7a63180834ed8d0166da30c5fc44c1
Andrew Dawson <dandrew@uber.com>
2018-12-05 10:14:11 -0800

    plumb through archival interface

--- common/archival/client.go
+++ common/archival/client.go
@@ -1,20 +1,47 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package archival
 
 import (
 	"context"
 	"errors"
 	"go.uber.org/cadence/.gen/go/shared"
+	"time"
 )
+
+// ArchiveRequest is request for Archive
+type ArchiveRequest struct {
+	DomainName   string
+	DomainID     string
+	WorkflowID   string
+	RunID        string
+	WorkflowType string
+	Status       string
+	CloseTime    time.Time
+	History      *shared.History
+}
 
 // Client is used to store and retrieve blobs
 type Client interface {
-	Archive(
-		ctx context.Context,
-		domainName string,
-		domainID string,
-		workflowInfo *shared.WorkflowExecutionInfo,
-		history *shared.History,
-	) error
+	Archive(ctx context.Context, request *ArchiveRequest) error
 
 	GetWorkflowExecutionHistory(
 		ctx context.Context,
@@ -29,14 +56,8 @@ type Client interface {
 
 type nopClient struct{}
 
-func (c *nopClient) Archive(
-	ctx context.Context,
-	domainName string,
-	domainID string,
-	workflowInfo *shared.WorkflowExecutionInfo,
-	history *shared.History,
-) error {
- 	return errors.New("not implemented")
+func (c *nopClient) Archive(ctx context.Context, request *ArchiveRequest) error {
+	return errors.New("not implemented")
 }
 
 func (c *nopClient) GetWorkflowExecutionHistory(
@@ -53,6 +74,7 @@ func (c *nopClient) ListClosedWorkflowExecutions(
 	return nil, errors.New("not implemented")
 }
 
+// NewNopClient creates a nop client
 func NewNopClient() Client {
 	return &nopClient{}
 }
