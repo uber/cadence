7f8a8da3a03f251dddcd8e0b6a59db2355358b0e
Andrew Dawson <dandrew@uber.com>
2019-01-16 12:56:40 -0800

    add comments for how I plan to construct json blobs

--- service/worker/sysworkflow/history_blob_util.go
+++ service/worker/sysworkflow/history_blob_util.go
@@ -21,6 +21,7 @@
 package sysworkflow
 
 import (
+	"code.uber.internal/devexp/cadence-server/.tmp/.go/goroot/src/encoding/json"
 	"fmt"
 	"github.com/dgryski/go-farm"
 	"github.com/uber/cadence/.gen/go/shared"
@@ -36,15 +37,18 @@ const (
 type (
 	// HistoryBlobHeader is the header attached to all history blobs
 	HistoryBlobHeader struct {
-		DomainName     *string `json:"domain_name"`
-		DomainID       *string `json:"domain_id"`
-		WorkflowID     *string `json:"workflow_id"`
-		RunID          *string `json:"run_id"`
-		PageToken      *string `json:"page_token"`
-		Version        *string `json:"version"`
-		UploadDateTime *string `json:"upload_date_time"`
-		UploadCluster  *string `json:"update_cluster"`
-		EventCount     *int64  `json:"event_count"`
+		DomainName           *string `json:"domain_name"`
+		DomainID             *string `json:"domain_id"`
+		WorkflowID           *string `json:"workflow_id"`
+		RunID                *string `json:"run_id"`
+		PageToken            *string `json:"page_token"`
+		StartFailoverVersion *string `json:"start_failover_version"`
+		CloseFailoverVersion *string `json"close_failover_version"`
+		StartEventID         *int64  `json:"start_event_id"`
+		CloseEventID         *int64  `json:"close_event_id"`
+		UploadDateTime       *string `json:"upload_date_time"`
+		UploadCluster        *string `json:"upload_cluster"`
+		EventCount           *int64  `json:"event_count"`
 	}
 
 	// HistoryBlob is the serialized that histories get uploaded as
@@ -60,3 +64,39 @@ func NewHistoryBlobKey(domainID, workflowID, runID, pageToken, version string) (
 	hash := fmt.Sprintf("%v", farm.Fingerprint64([]byte(hashInput)))
 	return blobstore.NewKey(HistoryBlobKeyExt, hash, pageToken, version)
 }
+
+// TODO: come up with some better naming for this method
+// perhaps there is not such method as this maybe there is just a method that given historyBlob marshall it
+// given a history blob header construct the tags
+// these two methods are easily testable
+// they allow me to construct a blob with a single line and two method calls
+func MakeBlobOrWhateverMethodName(historyBlob *HistoryBlob) (blobstore.Blob, error) {
+
+	// TODO: this code all needs to be cleaned up, factored in different methods and tested
+	body, err := json.Marshal(historyBlob)
+	if err != nil {
+		return nil, err
+	}
+
+	var m map[string]interface{}
+	inrec, err := json.Marshal(historyBlob.Header)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = json.Unmarshal(inrec, &m); err != nil {
+		return nil, err
+	}
+
+	var tags map[string]string
+	for k, v := range m {
+		tags[k] = fmt.Sprintf("%v", v)
+	}
+
+	return blobstore.NewBlob(body, tags), nil
+}
+
+// maybe you make it symmetric so given a tag map[string]string you can get the header
+// given a byte array which is the blob body you can construct the HistoryBlob again
+
+// That way we have four small methods which are self contained and give us all the flexibility we need
