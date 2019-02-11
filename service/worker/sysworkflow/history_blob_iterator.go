8ccc656c42911605362a96c5400f9d5dc5140703
Andrew Dawson <dandrew@uber.com>
2019-02-11 07:13:12 -0800

    remove unneeded comment

--- service/worker/sysworkflow/history_blob_iterator.go
+++ service/worker/sysworkflow/history_blob_iterator.go
@@ -140,7 +140,6 @@ func (i *historyBlobIterator) Next() (*HistoryBlob, error) {
 		UploadCluster:        &i.clusterName,
 		EventCount:           &eventCount,
 		CloseFailoverVersion: &i.closeFailoverVersion,
-		// TODO: add deterministic blob construction algorithm identifier here (will do this on this diff)...
 	}
 	if i.HasNext() {
 		i.blobPageToken++
