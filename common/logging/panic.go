9e2cf9b84b13e6a64d7041305ca659645567176d
Quanzheng Long <longer@uber.com>
2019-03-21 16:41:51 -0700

    add for frontend/matching

--- common/logging/panic.go
+++ common/logging/panic.go
@@ -27,9 +27,9 @@ import (
 	"github.com/uber-common/bark"
 )
 
-var defaultPanicError = fmt.Errorf("panic object is not error")
+var errDefaultPanic = fmt.Errorf("panic object is not error")
 
-// This function is used to capture panic, it will log the panic and also return the error through pointer.
+// CapturePanic is used to capture panic, it will log the panic and also return the error through pointer.
 // If the panic value is not error then a default error is returned
 // We have to use pointer is because in golang: "recover return nil if was not called directly by a deferred function."
 // And we have to set the returned error otherwise our handler will return nil as error which is incorrect
@@ -37,7 +37,7 @@ func CapturePanic(logger bark.Logger, retError *error) {
 	if errPanic := recover(); errPanic != nil {
 		err, ok := errPanic.(error)
 		if !ok {
-			err = defaultPanicError
+			err = errDefaultPanic
 		}
 
 		st := string(debug.Stack())
