3483ab7c29bb4b85f96e46fa43a9a1d5e3d6c162
Venkatraghavan Srinivasan <venkat@uber.com>
2019-03-27 16:48:24 -0700

    update unit test

--- service/worker/scanner/executor/executor_test.go
+++ service/worker/scanner/executor/executor_test.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package executor
 
 import (
@@ -34,12 +54,16 @@ func (s *ExecutorTestSuite) TestTaskExecution() {
 	e := NewFixedSizePoolExecutor(32)
 	e.Start()
 	var runCounter int64
-	for i := 0; i < 100; i++ {
-		if i%2 == 0 {
-			e.Submit(&testTask{TaskStatusDefer, &runCounter})
-			continue
-		}
-		e.Submit(&testTask{TaskStatusDone, &runCounter})
+	for i := 0; i < 5; i++ {
+		go func() {
+			for i := 0; i < 20; i++ {
+				if i%2 == 0 {
+					e.Submit(&testTask{TaskStatusDefer, &runCounter})
+					continue
+				}
+				e.Submit(&testTask{TaskStatusDone, &runCounter})
+			}
+		}()
 	}
 	s.True(s.awaitCompletion(e))
 	s.Equal(int64(150), runCounter)
