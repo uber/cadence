43cc751bd462063f3a4e48b501e6f90907420073
Quanzheng Long <longer@uber.com>
2019-03-28 17:39:14 -0700

    fix comment

--- common/log/interface.go
+++ common/log/interface.go
@@ -6,12 +6,12 @@ type Fields map[tagType]interface{}
 // Logger is our abstraction for logging
 // Usage examples:
 //  1) logger = logger.WithFields(log.Fields{
-//		 log.TagWorkflowEventID: 123,
-//		 log.TagDomainID       : "test-domain-id"})
+//          log.TagWorkflowEventID: 123,
+//          log.TagDomainID       : "test-domain-id"})
 //     logger.Info("hello world")
-//	2) logger.Info("hello world", log.Fields{
-//		log.TagWorkflowEventID : 123,
-//		log.TagDomainID        : "test-domain-id",
+//  2) logger.Info("hello world", log.Fields{
+//          log.TagWorkflowEventID : 123,
+//          log.TagDomainID        : "test-domain-id",
 //	   })
 //  Note: msg should be static, it is not recommended to use fmt.Sprintf() for msg.
 //        Anything dynamic should be tagged.
