e216a16ff103a42413af10a7952facdf926febe0
Yichao Yang <ycyang@uber.com>
2019-04-08 20:50:43 -0700

    fix sql unit test failure

--- common/persistence/sql/storage/mysql/visibility.go
+++ common/persistence/sql/storage/mysql/visibility.go
@@ -35,7 +35,7 @@ const (
 
 	templateCreateWorkflowExecutionClosed = `REPLACE INTO executions_visibility (` +
 		`domain_id, workflow_id, run_id, start_time, execution_time, workflow_type_name, close_time, close_status, history_length) ` +
-		`VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+		`VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
 	// RunID condition is needed for correct pagination
 	templateConditions = ` AND domain_id = ?
