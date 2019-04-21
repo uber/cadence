9fe7ceb0cea339d73769ce644a842eac31b67e6c
Shreyas Srivatsan <shreyass@uber.com>
2019-04-21 14:14:52 -0700

    license stuff

--- common/service/service.go
+++ common/service/service.go
@@ -182,7 +182,6 @@ func (h *serviceImpl) Start() {
 		h.logger.WithTags(tag.Error(err)).Fatal("Membership monitor creation failed")
 	}
 
-	h.logger.Infof("create membership monitor for %v", h.sName)
 	err = h.membershipMonitor.Start()
 	if err != nil {
 		h.logger.WithTags(tag.Error(err)).Fatal("starting membership monitor failed")
