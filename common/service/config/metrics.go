db0e00e096850b7bec5ee0e9d48c2113add4b959
Shreyas Srivatsan <shreyass@uber.com>
2019-05-01 23:13:51 -0700

    cleanup metrics

--- common/service/config/metrics.go
+++ common/service/config/metrics.go
@@ -21,15 +21,21 @@
 package config
 
 import (
-	"log"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/cactus/go-statsd-client/statsd"
+	prom "github.com/m3db/prometheus_client_golang/prometheus"
 	"github.com/uber-go/tally"
 	"github.com/uber-go/tally/prometheus"
 	tallystatsdreporter "github.com/uber-go/tally/statsd"
+	"github.com/uber/cadence/common/log"
+	"github.com/uber/cadence/common/log/tag"
 	statsdreporter "github.com/uber/cadence/common/metrics/tally/statsd"
 )
+
+var prometheusScope tally.Scope
 
 // NewScope builds a new tally scope
 // for this metrics configuration
@@ -39,25 +45,25 @@ import (
 // only one of them will be used for
 // reporting. Currently, m3 is preferred
 // over statsd
-func (c *Metrics) NewScope() tally.Scope {
+func (c *Metrics) NewScope(logger log.Logger) tally.Scope {
 	if c.M3 != nil {
-		return c.newM3Scope()
+		return c.newM3Scope(logger)
 	}
 	if c.Statsd != nil {
-		return c.newStatsdScope()
+		return c.newStatsdScope(logger)
 	}
 	if c.Prometheus != nil {
-		return c.newPrometheusScope()
+		return c.newPrometheusScope(logger)
 	}
 	return tally.NoopScope
 }
 
 // newM3Scope returns a new m3 scope with
 // a default reporting interval of a second
-func (c *Metrics) newM3Scope() tally.Scope {
+func (c *Metrics) newM3Scope(logger log.Logger) tally.Scope {
 	reporter, err := c.M3.NewReporter()
 	if err != nil {
-		log.Fatalf("error creating m3 reporter, err=%v", err)
+		logger.Fatal("error creating m3 reporter", tag.Error(err))
 	}
 	scopeOpts := tally.ScopeOptions{
 		Tags:           c.Tags,
@@ -69,14 +75,14 @@ func (c *Metrics) newM3Scope() tally.Scope {
 
 // newM3Scope returns a new statsd scope with
 // a default reporting interval of a second
-func (c *Metrics) newStatsdScope() tally.Scope {
+func (c *Metrics) newStatsdScope(logger log.Logger) tally.Scope {
 	config := c.Statsd
 	if len(config.HostPort) == 0 {
 		return tally.NoopScope
 	}
 	statter, err := statsd.NewBufferedClient(config.HostPort, config.Prefix, config.FlushInterval, config.FlushBytes)
 	if err != nil {
-		log.Fatalf("error creating statsd client, err=%v", err)
+		logger.Fatal("error creating statsd client", tag.Error(err))
 	}
 	//NOTE: according to ( https://github.com/uber-go/tally )Tally's statsd implementation doesn't support tagging.
 	// Therefore, we implement Tally interface to have a statsd reporter that can support tagging
@@ -91,10 +97,17 @@ func (c *Metrics) newStatsdScope() tally.Scope {
 
 // newPrometheusScope returns a new prometheus scope with
 // a default reporting interval of a second
-func (c *Metrics) newPrometheusScope() tally.Scope {
-	reporter, err := c.Prometheus.NewReporter(prometheus.ConfigurationOptions{})
+func (c *Metrics) newPrometheusScope(logger log.Logger) tally.Scope {
+	reporter, err := NewPrometheusReporter(
+		c.Prometheus,
+		prometheus.ConfigurationOptions{
+			OnError: func(err error) {
+				logger.Warn("error creating prometheus reporter", tag.Error(err))
+			},
+		},
+	)
 	if err != nil {
-		log.Fatalf("error creating prometheus reporter, err=%v", err)
+		logger.Fatal("error creating prometheus reporter", tag.Error(err))
 	}
 	scopeOpts := tally.ScopeOptions{
 		Tags:           c.Tags,
@@ -103,4 +116,59 @@ func (c *Metrics) newPrometheusScope() tally.Scope {
 	}
 	scope, _ := tally.NewRootScope(scopeOpts, time.Second)
 	return scope
+}
+
+// NewPrometheusReporter - creates a prometheus reporter
+func NewPrometheusReporter(
+	config *prometheus.Configuration,
+	configOpts prometheus.ConfigurationOptions,
+) (prometheus.Reporter, error) {
+	var opts prometheus.Options
+	opts.OnRegisterError = configOpts.OnError
+
+	switch config.TimerType {
+	case "summary":
+		opts.DefaultTimerType = prometheus.SummaryTimerType
+	case "histogram":
+		opts.DefaultTimerType = prometheus.HistogramTimerType
+	}
+
+	if len(config.DefaultHistogramBuckets) > 0 {
+		var values []float64
+		for _, value := range config.DefaultHistogramBuckets {
+			values = append(values, value.Upper)
+		}
+		opts.DefaultHistogramBuckets = values
+	}
+
+	if len(config.DefaultSummaryObjectives) > 0 {
+		values := make(map[float64]float64)
+		for _, value := range config.DefaultSummaryObjectives {
+			values[value.Percentile] = value.AllowedError
+		}
+		opts.DefaultSummaryObjectives = values
+	}
+
+	opts.Registerer = prom.NewRegistry()
+
+	reporter := prometheus.NewReporter(opts)
+
+	path := "/metrics"
+	if handlerPath := strings.TrimSpace(config.HandlerPath); handlerPath != "" {
+		path = handlerPath
+	}
+
+	if addr := strings.TrimSpace(config.ListenAddress); addr == "" {
+		http.Handle(path, reporter.HTTPHandler())
+	} else {
+		mux := http.NewServeMux()
+		mux.Handle(path, reporter.HTTPHandler())
+		go func() {
+			if err := http.ListenAndServe(addr, mux); err != nil {
+				configOpts.OnError(err)
+			}
+		}()
+	}
+
+	return reporter, nil
 }
