8b96ed5ffda4581f1d584834c55fa2d74dfe06fc
yux0 <yx@uber.com>
2019-05-10 09:48:15 -0700

    update code

--- service/history/conflictResolverV2.go
+++ service/history/conflictResolverV2.go
@@ -21,6 +21,7 @@
 package history
 
 import (
+	"fmt"
 	"github.com/uber/cadence/.gen/go/shared"
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/common/cluster"
@@ -31,7 +32,8 @@ import (
 )
 
 const (
-	defaultPageSize = 100
+	defaultPageSize   = 100
+	eventStoreVersion = persistence.EventStoreVersionV2
 )
 
 type (
@@ -65,87 +67,42 @@ func newConflictResolverV2(shard ShardContext, context workflowExecutionContext,
 func (r *conflictResolverV2Impl) reset(prevRunID string, requestID string, replayEventID int64, info *persistence.WorkflowExecutionInfo, localVersionHistories persistence.VersionHistories) (mutableState, error) {
 	domainID := r.context.getDomainID()
 	execution := *r.context.getExecution()
-	startTime := info.StartTimestamp
-	eventStoreVersion := info.EventStoreVersion
 	branchToken := info.GetCurrentBranch()
 	replayNextEventID := replayEventID + 1
 
 	var resetMutableStateBuilder *mutableStateBuilder
 	var sBuilder stateBuilder
 	var lastEvent *shared.HistoryEvent
-	var lastFirstEventID int64
 
-	eventsToApply := replayNextEventID - common.FirstEventID
-
-	iter := collection.NewPagingIterator(func(paginationToken []byte) ([]interface{}, []byte, error) {
-		var paginateItems []interface{}
-		historyBatch, _, token, err := r.getHistoryBatch(domainID, execution, common.FirstEventID, replayNextEventID, paginationToken, eventStoreVersion, branchToken)
-		for _, history := range historyBatch {
-			paginateItems = append(paginateItems, history)
-		}
-		return paginateItems, token, err
-	})
-
+	iter := collection.NewPagingIterator(r.getHistoryBatch(domainID, execution, common.FirstEventID, replayNextEventID, nil, eventStoreVersion, branchToken))
 	for iter.HasNext() {
-		item, err := iter.Next()
+		batch, err := iter.Next()
 		if err != nil {
 			r.logger.Error("Conflict resolution err getting history.", tag.Error(err))
 			return nil, err
 		}
 
-		history := item.(*shared.History).Events
-		batchSize := int64(len(history))
-		// NextEventID could be in the middle of the batch.  Trim the history events to not have more events then what
-		// need to be applied
-		if batchSize > eventsToApply {
-			history = history[0:eventsToApply]
-		}
-		eventsToApply -= int64(len(history))
-
-		if len(history) == 0 {
-			break
-		}
-
+		history := batch.(*shared.History).Events
 		firstEvent := history[0]
 		lastEvent = history[len(history)-1]
 		if firstEvent.GetEventId() == common.FirstEventID {
-			resetMutableStateBuilder = newMutableStateBuilderWithReplicationState(
-				r.clusterMetadata.GetCurrentClusterName(),
-				r.shard,
-				r.shard.GetEventsCache(),
-				r.logger,
-				firstEvent.GetVersion(),
-			)
-
-			resetMutableStateBuilder.executionInfo.EventStoreVersion = eventStoreVersion
-			sBuilder = newStateBuilder(r.shard, resetMutableStateBuilder, r.logger)
+			resetMutableStateBuilder, sBuilder = r.initializeBuilders(firstEvent)
 		}
-
-		// NOTE: passing 0 as newRunEventStoreVersion is safe here, since we don't need the newMutableState of the new run
-		_, _, _, err = sBuilder.applyEvents(domainID, requestID, execution, history, nil, resetMutableStateBuilder.GetEventStoreVersion(), 0)
+		_, _, _, err = sBuilder.applyEvents(domainID, requestID, execution, history, nil, eventStoreVersion, eventStoreVersion)
 		if err != nil {
 			r.logger.Error("conflict resolution err applying events.", tag.Error(err))
 			return nil, err
 		}
-		resetMutableStateBuilder.executionInfo.SetLastFirstEventID(lastFirstEventID)
 		resetMutableStateBuilder.IncrementHistorySize(len(history))
 	}
+	//Sanity check on last event id of the last history batch
+	if *lastEvent.EventId != replayEventID {
+		return resetMutableStateBuilder, &shared.BadRequestError{
+			Message: fmt.Sprintf("failed to resolve conflict as the last even id %v and replay event id %v are not matched", lastEvent, replayNextEventID),
+		}
+	}
 
-	// reset branchToken to the original one(it has been set to a wrong branchToken in applyEvents for startEvent)
-	resetMutableStateBuilder.executionInfo.BranchToken = branchToken
-	// similarly, in case of resetWF, the runID in startEvent is incorrect
-	resetMutableStateBuilder.executionInfo.RunID = info.RunID
-	// Applying events to mutableState does not move the nextEventID.  Explicitly set nextEventID to new value
-	resetMutableStateBuilder.executionInfo.SetNextEventID(replayNextEventID)
-	resetMutableStateBuilder.executionInfo.StartTimestamp = startTime
-	// the last updated time is not important here, since this should be updated with event time afterwards
-	resetMutableStateBuilder.executionInfo.LastUpdatedTimestamp = startTime
-
-	sourceCluster := r.clusterMetadata.ClusterNameForFailoverVersion(lastEvent.GetVersion())
-	resetMutableStateBuilder.UpdateReplicationStateLastEventID(sourceCluster, lastEvent.GetVersion(), replayEventID)
-
-	r.logger.Info("All events applied for execution.", tag.WorkflowResetNextEventID(resetMutableStateBuilder.GetNextEventID()))
-	msBuilder, err := r.context.resetMutableState(prevRunID, resetMutableStateBuilder)
+	msBuilder, err := r.resetMutableState(resetMutableStateBuilder, info, lastEvent, replayEventID, prevRunID)
 	if err != nil {
 		r.logger.Error("conflict resolution err reset workflow.", tag.Error(err))
 	}
@@ -170,22 +127,60 @@ func (r *conflictResolverV2Impl) resolveVersionHistoryConflict(local persistence
 }
 
 func (r *conflictResolverV2Impl) getHistoryBatch(domainID string, execution shared.WorkflowExecution, firstEventID,
-	nextEventID int64, nextPageToken []byte, eventStoreVersion int32, branchToken []byte) ([]*shared.History, int, []byte, error) {
-	_, historyBatches, token, size, err := PaginateHistory(
-		r.historyMgr,
-		r.historyV2Mgr,
-		nil,
+	nextEventID int64, nextPageToken []byte, eventStoreVersion int32, branchToken []byte) collection.PaginationFn {
+	return func(paginationToken []byte) ([]interface{}, []byte, error) {
+		var paginateItems []interface{}
+		_, historyBatches, token, _, err := PaginateHistory(
+			r.historyMgr,
+			r.historyV2Mgr,
+			nil,
+			r.logger,
+			true,
+			domainID,
+			*execution.WorkflowId,
+			*execution.RunId,
+			firstEventID,
+			nextEventID,
+			nextPageToken,
+			eventStoreVersion,
+			branchToken,
+			defaultPageSize,
+			common.IntPtr(r.shard.GetShardID()))
+		for _, history := range historyBatches {
+			paginateItems = append(paginateItems, history)
+		}
+		return paginateItems, token, err
+	}
+}
+
+func (r *conflictResolverV2Impl) initializeBuilders(firstEvent *shared.HistoryEvent) (*mutableStateBuilder, *stateBuilderImpl) {
+	resetMutableStateBuilder := newMutableStateBuilderWithReplicationState(
+		r.clusterMetadata.GetCurrentClusterName(),
+		r.shard,
+		r.shard.GetEventsCache(),
 		r.logger,
-		true,
-		domainID,
-		*execution.WorkflowId,
-		*execution.RunId,
-		firstEventID,
-		nextEventID,
-		nextPageToken,
-		eventStoreVersion,
-		branchToken,
-		defaultPageSize,
-		common.IntPtr(r.shard.GetShardID()))
-	return historyBatches, size, token, err
+		firstEvent.GetVersion(),
+	)
+	resetMutableStateBuilder.executionInfo.EventStoreVersion = eventStoreVersion
+	sBuilder := newStateBuilder(r.shard, resetMutableStateBuilder, r.logger)
+	return resetMutableStateBuilder, sBuilder
+}
+
+func (r *conflictResolverV2Impl) resetMutableState(msBuilder *mutableStateBuilder,
+	execution *persistence.WorkflowExecutionInfo,
+	lastEvent *shared.HistoryEvent,
+	replayEventID int64,
+	prevRunID string) (mutableState, error) {
+	// reset branchToken to the original one(it has been set to a wrong branchToken in applyEvents for startEvent)
+	msBuilder.executionInfo.BranchToken = msBuilder.GetCurrentBranch()
+	// similarly, in case of resetWF, the runID in startEvent is incorrect
+	msBuilder.executionInfo.RunID = execution.RunID
+	msBuilder.executionInfo.StartTimestamp = execution.StartTimestamp
+	// the last updated time is not important here, since this should be updated with event time afterwards
+	msBuilder.executionInfo.LastUpdatedTimestamp = execution.StartTimestamp
+	sourceCluster := r.clusterMetadata.ClusterNameForFailoverVersion(lastEvent.GetVersion())
+	msBuilder.UpdateReplicationStateLastEventID(sourceCluster, lastEvent.GetVersion(), replayEventID)
+
+	r.logger.Info("All events applied for execution.", tag.WorkflowResetNextEventID(msBuilder.GetNextEventID()))
+	return r.context.resetMutableState(prevRunID, msBuilder)
 }
