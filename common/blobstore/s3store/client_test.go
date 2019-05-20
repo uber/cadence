272f691ac0f9cc457fed5cc24fafb92a6e146dd9
Jonas Trollvik <jonas@weightpoint.se>
2019-05-20 21:59:13 +0200

    Rewrite mocking interface using mockery

--- common/blobstore/s3store/client_test.go
+++ common/blobstore/s3store/client_test.go
@@ -28,11 +28,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"github.com/uber/cadence/common/blobstore"
 	"github.com/uber/cadence/common/blobstore/blob"
+	"github.com/uber/cadence/common/blobstore/s3store/mocks"
 	"io/ioutil"
 	"net/url"
 	"sort"
@@ -52,6 +53,7 @@ const (
 type ClientSuite struct {
 	*require.Assertions
 	suite.Suite
+	s3cli *mocks.S3API
 }
 
 func TestClientSuite(t *testing.T) {
@@ -60,10 +62,14 @@ func TestClientSuite(t *testing.T) {
 
 func (s *ClientSuite) SetupTest() {
 	s.Assertions = require.New(s.T())
+	s.s3cli = &mocks.S3API{}
 }
 
 func (s *ClientSuite) TestUploadDownload_Success() {
 	client := s.constructClient()
+	s.setupFsEmulation()
+	s.s3cli.On("DeleteObjectWithContext", mock.Anything, mock.Anything).Return(nil, nil)
+
 	b := blob.NewBlob([]byte("body version 1"), map[string]string{})
 	key, err := blob.NewKeyFromString("blob.blob")
 	s.NoError(err)
@@ -84,6 +90,7 @@ func (s *ClientSuite) TestUploadDownload_Success() {
 
 func (s *ClientSuite) TestUpload_Fail_BucketNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
 
 	b := blob.NewBlob([]byte("blob body"), map[string]string{"tagKey": "tagValue"})
 	key, err := blob.NewKeyFromString("blob.blob")
@@ -93,6 +100,7 @@ func (s *ClientSuite) TestUpload_Fail_BucketNotExists() {
 
 func (s *ClientSuite) TestDownload_Fail_BucketNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("GetObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
 
 	key, err := blob.NewKeyFromString("blobname.ext")
 	s.NoError(err)
@@ -103,6 +111,7 @@ func (s *ClientSuite) TestDownload_Fail_BucketNotExists() {
 
 func (s *ClientSuite) TestDownload_Fail_BlobNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("GetObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New(s3.ErrCodeNoSuchKey, "", nil))
 
 	key, err := blob.NewKeyFromString("blobname.ext")
 	s.NoError(err)
@@ -112,8 +121,8 @@ func (s *ClientSuite) TestDownload_Fail_BlobNotExists() {
 }
 
 func (s *ClientSuite) TestUploadDownload_Success_CustomBucket() {
-
 	client := s.constructClient()
+	s.setupFsEmulation()
 	b := blob.NewBlob([]byte("blob body"), map[string]string{})
 	key, err := blob.NewKeyFromString("blob.blob")
 	s.NoError(err)
@@ -127,16 +136,19 @@ func (s *ClientSuite) TestUploadDownload_Success_CustomBucket() {
 
 func (s *ClientSuite) TestExists_Fail_BucketNotExists() {
 	client := s.constructClient()
-
+	s.s3cli.On("HeadObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New("NotFound", "", nil))
 	key, err := blob.NewKeyFromString("blob.blob")
 	s.NoError(err)
-	exists, err := client.Exists(context.Background(), "bucket1-not-exists", key)
+	exists, err := client.Exists(context.Background(), "bucket-not-exists", key)
 	s.NoError(err)
 	s.False(exists)
 }
 
 func (s *ClientSuite) TestExists_Success() {
 	client := s.constructClient()
+	s.s3cli.On("HeadObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New("NotFound", "", nil)).Once()
+	s.s3cli.On("HeadObjectWithContext", mock.Anything, mock.Anything).Return(&s3.HeadObjectOutput{}, nil)
+	s.s3cli.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(nil, nil)
 
 	key, err := blob.NewKeyFromString("TestExistsSuccess.blob")
 	s.NoError(err)
@@ -153,6 +165,7 @@ func (s *ClientSuite) TestExists_Success() {
 
 func (s *ClientSuite) TestDelete_Fail_BucketNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("DeleteObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
 
 	key, err := blob.NewKeyFromString("blob.blob")
 	s.NoError(err)
@@ -163,6 +176,9 @@ func (s *ClientSuite) TestDelete_Fail_BucketNotExists() {
 
 func (s *ClientSuite) TestDelete_Success() {
 	client := s.constructClient()
+	s.s3cli.On("DeleteObjectWithContext", mock.Anything, mock.Anything).Return(nil, nil)
+	s.s3cli.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(nil, nil)
+	s.s3cli.On("HeadObjectWithContext", mock.Anything, mock.Anything).Return(nil, awserr.New("NotFound", "", nil))
 
 	key, err := blob.NewKeyFromString("blobnonexistent.blob")
 	s.NoError(err)
@@ -182,6 +198,7 @@ func (s *ClientSuite) TestDelete_Success() {
 
 func (s *ClientSuite) TestListByPrefix_Fail_BucketNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("ListObjectsV2WithContext", mock.Anything, mock.Anything).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
 
 	files, err := client.ListByPrefix(context.Background(), "bucket-not-exists", "foo")
 	s.Equal(blobstore.ErrBucketNotExists, err)
@@ -190,8 +207,27 @@ func (s *ClientSuite) TestListByPrefix_Fail_BucketNotExists() {
 
 func (s *ClientSuite) TestListByPrefix_Success() {
 	client := s.constructClient()
-
 	allFiles := []string{"matching_1.ext", "matching_2.ext", "not_matching_1.ext", "not_matching_2.ext", "matching_3.ext"}
+	s.s3cli.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(nil, nil)
+	s.s3cli.On("ListObjectsV2WithContext", mock.Anything, mock.Anything).
+		Return(func(_ context.Context, input *s3.ListObjectsV2Input, opts ...request.Option) *s3.ListObjectsV2Output {
+		objects := make([]*s3.Object, 0)
+		for _, k := range allFiles {
+			if strings.HasPrefix(k, *input.Prefix) {
+				objects = append(objects, &s3.Object{
+					Key: aws.String(k),
+				})
+			}
+		}
+		sort.SliceStable(objects, func(i, j int) bool {
+			return *objects[i].Key < *objects[j].Key
+
+		})
+		return &s3.ListObjectsV2Output{
+			Contents: objects,
+		}
+	}, nil)
+
 	for _, f := range allFiles {
 		key, err := blob.NewKeyFromString(f)
 		s.NoError(err)
@@ -209,6 +245,9 @@ func (s *ClientSuite) TestListByPrefix_Success() {
 
 func (s *ClientSuite) TestBucketMetadata_Fail_BucketNotExists() {
 	client := s.constructClient()
+	s.s3cli.On("GetBucketAclWithContext", mock.Anything, mock.MatchedBy(func(input *s3.GetBucketAclInput) bool {
+		return *input.Bucket == "bucket-not-exists"
+	})).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
 
 	metadata, err := client.BucketMetadata(context.Background(), "bucket-not-exists")
 	s.Equal(blobstore.ErrBucketNotExists, err)
@@ -217,6 +256,22 @@ func (s *ClientSuite) TestBucketMetadata_Fail_BucketNotExists() {
 
 func (s *ClientSuite) TestBucketMetadata_Success() {
 	client := s.constructClient()
+	s.s3cli.On("GetBucketAclWithContext", mock.Anything, mock.Anything).Return(&s3.GetBucketAclOutput{
+		Owner: &s3.Owner{
+			DisplayName: aws.String("default-bucket-owner"),
+		},
+	}, nil)
+
+	days := int64(defaultBucketRetentionDays)
+	s.s3cli.On("GetBucketLifecycleConfigurationWithContext", mock.Anything, mock.Anything).
+		Return(&s3.GetBucketLifecycleConfigurationOutput{Rules: []*s3.LifecycleRule{
+			{
+				Status: aws.String("Enabled"),
+				Expiration: &s3.LifecycleExpiration{
+					Days: &days,
+				},
+			},
+		}}, nil)
 
 	metadata, err := client.BucketMetadata(context.Background(), defaultBucketName)
 	s.NoError(err)
@@ -227,6 +282,10 @@ func (s *ClientSuite) TestBucketMetadata_Success() {
 
 func (s *ClientSuite) TestBucketExists() {
 	client := s.constructClient()
+	s.s3cli.On("HeadBucketWithContext", mock.Anything, mock.MatchedBy(func(input *s3.HeadBucketInput) bool {
+		return *input.Bucket == "bucket-not-exists"
+	})).Return(nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil))
+	s.s3cli.On("HeadBucketWithContext", mock.Anything, mock.Anything).Return(nil, nil)
 
 	exists, err := client.BucketExists(context.Background(), "bucket-not-exists")
 	s.NoError(err)
@@ -237,170 +296,56 @@ func (s *ClientSuite) TestBucketExists() {
 	s.True(exists)
 }
 
-type mockS3Client struct {
-	s3iface.S3API
-	fs   map[string]string
-	tags map[string]string
-}
-
-func (m *mockS3Client) HeadBucketWithContext(_ aws.Context, input *s3.HeadBucketInput, _ ...request.Option) (*s3.HeadBucketOutput, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-
-	return &s3.HeadBucketOutput{}, nil
-}
-
-func (m *mockS3Client) DeleteObjectWithContext(_ aws.Context, input *s3.DeleteObjectInput, _ ...request.Option) (*s3.DeleteObjectOutput, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-
-	c := m.fs[*input.Bucket+*input.Key]
-	if c == "" {
-		return &s3.DeleteObjectOutput{}, nil
-	}
-	delete(m.fs, *input.Bucket+*input.Key)
-	return &s3.DeleteObjectOutput{}, nil
-}
-func (m *mockS3Client) ListObjectsV2WithContext(_ aws.Context, input *s3.ListObjectsV2Input, _ ...request.Option) (*s3.ListObjectsV2Output, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-
-	objects := make([]*s3.Object, 0)
-
-	for k := range m.fs {
-		if strings.HasPrefix(k, *input.Bucket+*input.Prefix) {
-
-			str := strings.Replace(k, *input.Bucket, "", 1)
-			object := s3.Object{
-				Key: &str,
-			}
-			objects = append(objects, &object)
-		}
-	}
-	sort.SliceStable(objects, func(i, j int) bool {
-		return *objects[i].Key < *objects[j].Key
-
-	})
-	return &s3.ListObjectsV2Output{
-		Contents: objects,
-	}, nil
-}
-
-func (m *mockS3Client) GetObjectTaggingWithContext(_ aws.Context, input *s3.GetObjectTaggingInput, _ ...request.Option) (*s3.GetObjectTaggingOutput, error) {
-	t := m.tags[*input.Bucket+*input.Key]
-	ql, _ := url.ParseQuery(t)
-
-	tags := make([]*s3.Tag, 0, len(ql))
-	for k, v := range ql {
-
-		tag := s3.Tag{
-			Key:   &k,
-			Value: &v[0],
-		}
-		tags = append(tags, &tag)
-
-	}
-	return &s3.GetObjectTaggingOutput{
-		TagSet: tags,
-	}, nil
-}
-
-func (m *mockS3Client) GetBucketAclWithContext(_ aws.Context, input *s3.GetBucketAclInput, _ ...request.Option) (*s3.GetBucketAclOutput, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-	return &s3.GetBucketAclOutput{
-		Owner: &s3.Owner{
-			DisplayName: aws.String(defaultBucketOwner),
-		},
-	}, nil
-}
-
-func (m *mockS3Client) GetBucketLifecycleConfigurationWithContext(_ aws.Context, input *s3.GetBucketLifecycleConfigurationInput, _ ...request.Option) (*s3.GetBucketLifecycleConfigurationOutput, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-
-	days := int64(defaultBucketRetentionDays)
-	rules := make([]*s3.LifecycleRule, 0, 1)
-	rules = append(rules, &s3.LifecycleRule{
-		Status: aws.String("Enabled"),
-		Expiration: &s3.LifecycleExpiration{
-			Days: &days,
-		},
-	})
-
-	return &s3.GetBucketLifecycleConfigurationOutput{
-		Rules: rules,
-	}, nil
-}
-func (m *mockS3Client) PutObjectWithContext(_ aws.Context, input *s3.PutObjectInput, _ ...request.Option) (*s3.PutObjectOutput, error) {
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(input.Body)
-
-	m.fs[*input.Bucket+*input.Key] = buf.String()
-	if input.Tagging != nil {
-		m.tags[*input.Bucket+*input.Key] = *input.Tagging
-	}
-	return &s3.PutObjectOutput{}, nil
-}
-
-func (m *mockS3Client) HeadObjectWithContext(_ aws.Context, input *s3.HeadObjectInput, _ ...request.Option) (*s3.HeadObjectOutput, error) {
-	c := m.fs[*input.Bucket+*input.Key]
-	if c == "" {
-		return nil, awserr.New("NotFound", "", nil)
-
-	}
-	return &s3.HeadObjectOutput{}, nil
-}
-
-func (m *mockS3Client) GetObjectWithContext(_ aws.Context, input *s3.GetObjectInput, _ ...request.Option) (*s3.GetObjectOutput, error) {
-	if *input.Bucket == *aws.String(bucketInvalidMetadata) {
-		return &s3.GetObjectOutput{
-			Body: ioutil.NopCloser(bytes.NewReader([]byte(
-				"dummy-content"))),
-		}, nil
-	}
-	if *input.Bucket == *aws.String(bucketNoMetadata) {
-		return nil, awserr.New(s3.ErrCodeNoSuchKey, "", nil)
-
-	}
-	if !strings.HasPrefix(*input.Bucket, defaultBucketName) {
-		return nil, awserr.New(s3.ErrCodeNoSuchBucket, "", nil)
-	}
-	c := m.fs[*input.Bucket+*input.Key]
-	if c == "" {
-		return nil, awserr.New(s3.ErrCodeNoSuchKey, "", nil)
-
-	}
-	return &s3.GetObjectOutput{
-		Body: ioutil.NopCloser(bytes.NewReader([]byte(c))),
-	}, nil
-
-}
-
-func NewMockClient() (blobstore.Client, error) {
-	return &client{
-		s3cli: &mockS3Client{
-			fs:   make(map[string]string),
-			tags: make(map[string]string),
-		},
-	}, nil
-}
 func (s *ClientSuite) constructClient() blobstore.Client {
-	client, err := NewMockClient()
-	s.NoError(err)
-	s.NotNil(client)
-	return client
+	return NewClient(s.s3cli)
 }
 
 func (s *ClientSuite) assertBlobEquals(expectedTags map[string]string, expectedBody string, actual *blob.Blob) {
 	s.Equal(expectedTags, actual.Tags)
 	s.Equal(expectedBody, string(actual.Body))
+}
+
+func (s *ClientSuite) setupFsEmulation() {
+	fs  := make(map[string]string)
+	tags := make(map[string]string)
+
+	putObjectFn := func (_ aws.Context, input *s3.PutObjectInput, _ ...request.Option) (*s3.PutObjectOutput) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(input.Body)
+
+		fs[*input.Bucket+*input.Key] = buf.String()
+		if input.Tagging != nil {
+			tags[*input.Bucket+*input.Key] = *input.Tagging
+		}
+		return &s3.PutObjectOutput{}
+	}
+	getObjectFn := func(_ aws.Context, input *s3.GetObjectInput, _ ...request.Option) (*s3.GetObjectOutput) {
+		c := fs[*input.Bucket+*input.Key]
+		return &s3.GetObjectOutput{
+			Body: ioutil.NopCloser(bytes.NewReader([]byte(c))),
+		}
+
+	}
+	getTagsFn := func(_ aws.Context, input *s3.GetObjectTaggingInput, _ ...request.Option) (*s3.GetObjectTaggingOutput) {
+		t := tags[*input.Bucket+*input.Key]
+		ql, _ := url.ParseQuery(t)
+
+		tags := make([]*s3.Tag, 0, len(ql))
+		for k, v := range ql {
+
+			tag := s3.Tag{
+				Key:   &k,
+				Value: &v[0],
+			}
+			tags = append(tags, &tag)
+
+		}
+		return &s3.GetObjectTaggingOutput{
+			TagSet: tags,
+		}
+	}
+
+	s.s3cli.On("PutObjectWithContext", mock.Anything, mock.Anything).Return(putObjectFn, nil)
+	s.s3cli.On("GetObjectWithContext", mock.Anything, mock.Anything).Return(getObjectFn, nil)
+	s.s3cli.On("GetObjectTaggingWithContext", mock.Anything, mock.Anything).Return(getTagsFn, nil)
 }
