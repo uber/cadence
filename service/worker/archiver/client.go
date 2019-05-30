018e88027c3ce8194af4be517cc2db7da30a69ba
Yichao Yang <ycyang@uber.com>
2019-05-30 15:48:27 -0700

    fix lint error

--- service/worker/archiver/client.go
+++ service/worker/archiver/client.go
@@ -49,7 +49,7 @@ type (
 		BranchToken          []byte
 		NextEventID          int64
 		CloseFailoverVersion int64
-		BucketName string
+		BucketName           string
 	}
 
 	// Client is used to archive workflow histories
