2a200b4c5e9df0468474db652fcf133c4da46c1b
Bowei Xu <boweixu@uber.com>
2019-06-11 16:39:34 -0700

    User decoder

--- common/elasticsearch/client.go
+++ common/elasticsearch/client.go
@@ -83,6 +83,7 @@ func NewClient(config *Config) (Client, error) {
 	client, err := elastic.NewClient(
 		elastic.SetURL(config.URL.String()),
 		elastic.SetRetrier(elastic.NewBackoffRetrier(elastic.NewExponentialBackoff(128*time.Millisecond, 513*time.Millisecond))),
+		elastic.SetDecoder(&elastic.NumberDecoder{}),
 	)
 	if err != nil {
 		return nil, err
