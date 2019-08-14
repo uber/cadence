88da32bf0afdee9f1a60fd23f6d41a5a6601bed4
Dustin Metzgar <dmetzgar@uber.com>
2019-08-14 13:37:16 -0700

    Fix artifact folder name

--- scripts/buildkite/gocov.sh
+++ scripts/buildkite/gocov.sh
@@ -6,14 +6,14 @@ set -ex
 go get github.com/mattn/goveralls
 
 # download cover files from all the tests
-mkdir -p .build/coverage
-buildkite-agent artifact download ".build/coverage/unit_cover.out" . --step ":golang: unit test" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_cassandra_cover.out" . --step ":golang: integration test with cassandra" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_cassandra_ev2_cover.out" . --step ":golang: integration test events v2 with cassandra" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_xdc_cassandra_cover.out" . --step ":golang: integration xdc test with cassandra" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_sql_cover.out" . --step ":golang: integration test with mysql" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_sql_ev2_cover.out" . --step ":golang: integration test events v2 with mysql" --build "$BUILDKITE_BUILD_ID"
-buildkite-agent artifact download ".build/coverage/integ_xdc_sql_cover.out" . --step ":golang: integration xdc test with mysql" --build "$BUILDKITE_BUILD_ID"
+mkdir -p build/coverage
+buildkite-agent artifact download "build/coverage/unit_cover.out" . --step ":golang: unit test" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_cassandra_cover.out" . --step ":golang: integration test with cassandra" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_cassandra_ev2_cover.out" . --step ":golang: integration test events v2 with cassandra" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_xdc_cassandra_cover.out" . --step ":golang: integration xdc test with cassandra" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_sql_cover.out" . --step ":golang: integration test with mysql" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_sql_ev2_cover.out" . --step ":golang: integration test events v2 with mysql" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/coverage/integ_xdc_sql_cover.out" . --step ":golang: integration xdc test with mysql" --build "$BUILDKITE_BUILD_ID"
 
 echo "download complete"
 
@@ -21,4 +21,4 @@ echo "download complete"
 make cover_ci
 
 # cleanup
-rm -rf .build
+rm -rf build
