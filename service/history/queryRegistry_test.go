678f169ce4763f339b6bdf4c663378906ed19684
Andrew Dawson <dandrew@uber.com>
2019-09-17 16:47:20 -0700

    run make fmt

--- service/history/queryRegistry_test.go
+++ service/history/queryRegistry_test.go
@@ -25,9 +25,9 @@ package history
 import (
 	"testing"
 
-	"github.com/uber/cadence/.gen/go/shared"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"github.com/uber/cadence/.gen/go/shared"
 )
 
 type QueryRegistrySuite struct {
