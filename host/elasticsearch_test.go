a4f1c1dbc8af6e468539a9408ce4097d7eccc9c5
Wenquan Xing <wenquanx@uber.com>
2019-10-07 15:58:59 -0700

    fix test

--- host/elasticsearch_test.go
+++ host/elasticsearch_test.go
@@ -17,7 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-//+build esintegration
 
 // to run locally, make sure kafka and es is running,
 // then run cmd `go test -v ./host -run TestElasticsearchIntegrationSuite -tags esintegration`
