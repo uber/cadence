f42135d791533b6d58a0f484e81bd73f2044fad0
Wenquan Xing <wenquanx@uber.com>
2019-10-13 17:33:32 -0700

    support version history

--- service/history/mutableStateBuilder.go
+++ service/history/mutableStateBuilder.go
@@ -264,8 +264,12 @@ func (e *mutableStateBuilder) CopyToPersistence() *persistence.WorkflowMutableSt
 	state.SignalInfos = e.pendingSignalInfoIDs
 	state.SignalRequestedIDs = e.pendingSignalRequestedIDs
 	state.ExecutionInfo = e.executionInfo
-	state.ReplicationState = e.replicationState
 	state.BufferedEvents = e.bufferedEvents
+	state.VersionHistories = e.versionHistories
+
+	// TODO when 2DC is deprecated, remove this
+	state.ReplicationState = e.replicationState
+
 	return state
 }
 
