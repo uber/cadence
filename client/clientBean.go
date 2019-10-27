e482d66ad0d42b919b47a0847cd7f453b1577be5
Wenquan Xing <wenquanx@uber.com>
2019-10-26 20:41:21 -0700

    Do not initialize remote clients if cluster not enabled

--- client/clientBean.go
+++ client/clientBean.go
@@ -109,7 +109,12 @@ func NewClientBean(factory Factory, dispatcherProvider DispatcherProvider, clust
 
 	remoteAdminClients := map[string]admin.Client{}
 	remoteFrontendClients := map[string]frontend.Client{}
+Loop:
 	for clusterName, info := range clusterMetadata.GetAllClusterInfo() {
+		if !info.Enabled {
+			continue Loop
+		}
+
 		dispatcher, err := dispatcherProvider.Get(info.RPCName, info.RPCAddress)
 		if err != nil {
 			return nil, err
