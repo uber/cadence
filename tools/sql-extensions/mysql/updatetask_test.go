603d0ec656bea12ae79673ec62f0459c24575000
Quanzheng Long <longer@uber.com>
2019-11-15 11:07:44 -0800

    fix one last test

--- tools/sql-extensions/mysql/updatetask_test.go
+++ tools/sql-extensions/mysql/updatetask_test.go
@@ -67,7 +67,7 @@ func (s *UpdateSchemaTestSuite) TestDryrun() {
 	conn, err := newTestConn(s.DBName)
 	s.Nil(err)
 	defer conn.Close()
-	dir := "../../schema/mysql/v57/cadence/versioned"
+	dir := "../../../schema/mysql/v57/cadence/versioned"
 	s.RunDryrunTest(sql.BuildCLIOptions(), conn, "--db", dir, mysql.Version)
 }
 
@@ -75,6 +75,6 @@ func (s *UpdateSchemaTestSuite) TestVisibilityDryrun() {
 	conn, err := newTestConn(s.DBName)
 	s.Nil(err)
 	defer conn.Close()
-	dir := "../../schema/mysql/v57/visibility/versioned"
+	dir := "../../../schema/mysql/v57/visibility/versioned"
 	s.RunDryrunTest(sql.BuildCLIOptions(), conn, "--db", dir, mysql.VisibilityVersion)
 }
