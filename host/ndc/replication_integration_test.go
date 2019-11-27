1aa1abcb93794413fb0793b2b10ebefcfc1e8ce0
Yichao Yang <ycyang@uber.com>
2019-11-27 11:42:14 -0800

    fix ndc tests

--- host/ndc/replication_integration_test.go
+++ host/ndc/replication_integration_test.go
@@ -42,9 +42,6 @@ func (s *nDCIntegrationTestSuite) TestReplicationMessageApplication() {
 	workflowType := "event-generator-workflow-type"
 	tasklist := "event-generator-taskList"
 
-	// active has initial version 0
-	historyClient := s.active.GetHistoryClient()
-
 	var historyBatch []*shared.History
 	s.generator = test.InitializeHistoryEventGenerator(s.domainName, 1)
 
@@ -89,9 +86,6 @@ func (s *nDCIntegrationTestSuite) TestReplicationMessageDLQ() {
 	runID := uuid.New()
 	workflowType := "event-generator-workflow-type"
 	tasklist := "event-generator-taskList"
-
-	// active has initial version 0
-	historyClient := s.active.GetHistoryClient()
 
 	var historyBatch []*shared.History
 	s.generator = test.InitializeHistoryEventGenerator(s.domainName, 1)
