1e34a2f174590c1aaff06e067c98adfc295a7460
yux0 <yx@uber.com>
2020-01-10 13:44:17 -0800

    resolve merge conflicts

--- common/persistence/domainReplicationQueue.go
+++ common/persistence/domainReplicationQueue.go
@@ -79,6 +79,7 @@ type (
 	DomainReplicationQueue interface {
 		common.Daemon
 		Publish(message interface{}) error
+		PublishToDLQ(message interface{}) error
 		GetReplicationMessages(lastMessageID int, maxCount int) ([]*replicator.ReplicationTask, int, error)
 		UpdateAckLevel(lastProcessedMessageID int, clusterName string) error
 		GetAckLevels() (map[string]int, error)
