9b970fd1500b322335b6aa3690698498523dec7c
Wen Luo <wenl@uber.com>
2020-01-31 17:07:04 -0800

    Add logging when dumping in dlq

--- service/worker/replicator/replicationTask.go
+++ service/worker/replicator/replicationTask.go
@@ -514,6 +514,8 @@ func (t *workflowReplicationTask) Nack() {
 	t.metricsClient.IncCounter(t.metricsScope, metrics.ReplicatorMessages)
 	t.metricsClient.RecordTimer(t.metricsScope, metrics.ReplicatorLatency, t.timeSource.Now().Sub(t.startTime))
 
+	t.logger.Info("Request dump put in DLQ", tag.WorkflowDomainID(t.queueID.DomainID), tag.WorkflowID(t.queueID.WorkflowID), tag.WorkflowRunID(t.queueID.RunID))
+
 	t.state = task.TaskStateNacked
 	// the underlying implementation will not return anything other than nil
 	// do logging just in case
