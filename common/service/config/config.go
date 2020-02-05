4233e83323b3f7a42756d4d01fe7c9b2bac0fdbe
bbassingthwaite <bbassingthwaite@digitalocean.com>
2020-02-05 14:32:56 -0600

    PR Feedback: Adjust doc string and add yaml tag

--- common/service/config/config.go
+++ common/service/config/config.go
@@ -292,8 +292,8 @@ type (
 		// Tags is the set of key-value pairs to be reported
 		// as part of every metric
 		Tags map[string]string `yaml:"tags"`
-		// Prefixes all outgoing metrics with the provided string
-		Prefix string
+		// Prefix sets the prefix to all outgoing metrics
+		Prefix string `yaml:"prefix"`
 	}
 
 	// Statsd contains the config items for statsd metrics reporter
