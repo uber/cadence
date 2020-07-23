75d0331695416e19a32bf15f57863acb71124abe
Mantas Sidlauskas <mantass@uber.com>
2020-07-23 16:28:26 +0300

    Move timers cli to shard section

--- tools/cli/admin.go
+++ tools/cli/admin.go
@@ -173,6 +173,54 @@ func newAdminShardManagementCommands() []cli.Command {
 				AdminRemoveTask(c)
 			},
 		},
+		{
+			Name:  "timers",
+			Usage: "get timers from multiple shards for given time range",
+			Flags: append(getDBFlags(),
+				cli.IntFlag{
+					Name:  FlagShardID,
+					Usage: "shardID",
+				},
+				cli.IntFlag{
+					Name:  FlagPageSize,
+					Usage: "page size used to query db executions table",
+					Value: 500,
+				},
+				cli.IntFlag{
+					Name:  FlagRPS,
+					Usage: "target rps of database queries",
+					Value: 100,
+				},
+				cli.StringFlag{
+					Name:  FlagStartDate,
+					Usage: "start date",
+					Value: time.Now().UTC().Format(time.RFC3339),
+				},
+				cli.StringFlag{
+					Name:  FlagEndDate,
+					Usage: "end date",
+					Value: time.Now().UTC().Add(24 * time.Hour).Format(time.RFC3339),
+				},
+				cli.StringFlag{
+					Name:  FlagDomainID,
+					Usage: "filter tasks by DomainID",
+				},
+				cli.IntSliceFlag{
+					Name: FlagTimerType,
+					Usage: "timer types: 0 - DecisionTimeoutTask, 1 - TaskTypeActivityTimeout, " +
+						"2 - TaskTypeUserTimer, 3 - TaskTypeWorkflowTimeout, 4 - TaskTypeDeleteHistoryEvent, " +
+						"5 - TaskTypeActivityRetryTimer, 6 - TaskTypeWorkflowBackoffTimer",
+					Value: &cli.IntSlice{-1},
+				},
+				cli.BoolFlag{
+					Name:  FlagSkipErrorMode,
+					Usage: "skip errors",
+				},
+			),
+			Action: func(c *cli.Context) {
+				AdminTimers(c)
+			},
+		},
 	}
 }
 
@@ -819,59 +867,6 @@ func newDBCommands() []cli.Command {
 				AdminDBScan(c)
 			},
 		},
-		{
-			Name:  "timers",
-			Usage: "get timers from multiple shards for given time range",
-			Flags: append(getDBFlags(),
-				cli.IntFlag{
-					Name:  FlagLowerShardBound,
-					Usage: "lower bound of shard to scan (inclusive)",
-					Value: 0,
-				},
-				cli.IntFlag{
-					Name:  FlagUpperShardBound,
-					Usage: "upper bound of shard to scan (exclusive)",
-					Value: 100,
-				},
-				cli.IntFlag{
-					Name:  FlagPageSize,
-					Usage: "page size used to query db executions table",
-					Value: 500,
-				},
-				cli.IntFlag{
-					Name:  FlagRPS,
-					Usage: "target rps of database queries",
-					Value: 100,
-				},
-				cli.StringFlag{
-					Name:  FlagStartDate,
-					Usage: "start date",
-					Value: time.Now().UTC().Format(time.RFC3339),
-				},
-				cli.StringFlag{
-					Name:  FlagEndDate,
-					Usage: "end date",
-					Value: time.Now().UTC().Add(24 * time.Hour).Format(time.RFC3339),
-				},
-				cli.StringFlag{
-					Name:  FlagDomainID,
-					Usage: "filter tasks by DomainID",
-				},
-				cli.BoolFlag{
-					Name:  FlagSkipErrorMode,
-					Usage: "skip errors",
-				},
-				cli.IntSliceFlag{
-					Name:  FlagTimerType,
-					Usage: "filter by timer types, default - 2 (User Timer). Can be provided multiple times",
-					Value: &cli.IntSlice{2},
-				},
-			),
-			Action: func(c *cli.Context) {
-				AdminTimers(c)
-			},
-		},
-
 		{
 			Name:    "clean",
 			Aliases: []string{"clean"},
--- tools/cli/adminTimers.go
+++ tools/cli/adminTimers.go
@@ -25,31 +25,42 @@ package cli
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 
-	"github.com/gocql/gocql"
-	"github.com/pkg/errors"
 	"github.com/urfave/cli"
-	"golang.org/x/sync/errgroup"
 
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/common/backoff"
 	"github.com/uber/cadence/common/log/loggerimpl"
 	"github.com/uber/cadence/common/persistence"
-	cassp "github.com/uber/cadence/common/persistence/cassandra"
+	"github.com/uber/cadence/common/persistence/cassandra"
 	"github.com/uber/cadence/common/quotas"
 )
 
 // AdminTimers is used to list scheduled timers.
 func AdminTimers(c *cli.Context) {
-	lowerShardBound := c.Int(FlagLowerShardBound)
-	upperShardBound := c.Int(FlagUpperShardBound)
+	shardID := getRequiredIntOption(c, FlagShardID)
+
 	startDate := c.String(FlagStartDate)
 	endDate := c.String(FlagEndDate)
 	rps := c.Int(FlagRPS)
+	domainID := c.String(FlagDomainID)
+	batchSize := c.Int(FlagBatchSize)
+	timerTypes := c.IntSlice(FlagTimerType)
+	skipErrMode := c.Bool(FlagSkipErrorMode)
 
-	ctx, cancel := newContextForLongPoll(c)
-	defer cancel()
+	if !c.IsSet(FlagTimerType) || (len(timerTypes) == 1 && timerTypes[0] == -1) {
+		timerTypes = []int{
+			persistence.TaskTypeDecisionTimeout,
+			persistence.TaskTypeActivityTimeout,
+			persistence.TaskTypeUserTimer,
+			persistence.TaskTypeWorkflowTimeout,
+			persistence.TaskTypeDeleteHistoryEvent,
+			persistence.TaskTypeActivityRetryTimer,
+			persistence.TaskTypeWorkflowBackoffTimer,
+		}
+	}
+
+	taskTypes := intSliceToMap(timerTypes)
 
 	st, err := parseSingleTs(startDate)
 	if err != nil {
@@ -63,55 +74,27 @@ func AdminTimers(c *cli.Context) {
 	session := connectToCassandra(c)
 	defer session.Close()
 
-	// shared limiter
 	limiter := quotas.NewSimpleRateLimiter(rps)
-
-	group, ctx := errgroup.WithContext(ctx)
-	for shardID := lowerShardBound; shardID < upperShardBound; shardID++ {
-		shardID := shardID
-		group.Go(func() error {
-			return printTimers(c, session, shardID, st, et, limiter)
-		})
-	}
-
-	if err := group.Wait(); err != nil {
-		ErrorAndExit("getting timers", err)
-	}
-}
-
-func printTimers(
-	c *cli.Context,
-	session *gocql.Session,
-	shardID int,
-	startDate, endDate time.Time,
-	limiter *quotas.RateLimiter,
-) error {
-	domainID := c.String(FlagDomainID)
-	skipErrMode := c.Bool(FlagSkipErrorMode)
-	batchSize := c.Int(FlagBatchSize)
-	timerTypes := intSliceToMap(c.IntSlice(FlagTimerType))
 	logger := loggerimpl.NewNopLogger()
 
-	execStore, err := cassp.NewWorkflowExecutionPersistence(shardID, session, logger)
+	execStore, err := cassandra.NewWorkflowExecutionPersistence(shardID, session, logger)
 	if err != nil {
-		err = errors.Wrapf(err, "cannot get execution manager for shardId %d", shardID)
-		if !skipErrMode {
-			return err
-		}
-		fmt.Println(err.Error())
-		return nil
+		ErrorAndExit("cannot get execution store", err)
 	}
 
-	manager := persistence.NewExecutionManagerImpl(execStore, logger)
-	ratelimitedClient := persistence.NewWorkflowExecutionPersistenceRateLimitedClient(manager, limiter, logger)
+	ratelimitedClient := persistence.NewWorkflowExecutionPersistenceRateLimitedClient(
+		persistence.NewExecutionManagerImpl(execStore, logger),
+		limiter,
+		logger,
+	)
 
 	var token []byte
 	isFirstIteration := true
 	for isFirstIteration || len(token) != 0 {
 		isFirstIteration = false
 		req := persistence.GetTimerIndexTasksRequest{
-			MinTimestamp:  startDate,
-			MaxTimestamp:  endDate,
+			MinTimestamp:  st,
+			MaxTimestamp:  et,
 			BatchSize:     batchSize,
 			NextPageToken: token,
 		}
@@ -124,21 +107,10 @@ func printTimers(
 			return err
 		}
 
-		for attempt := 0; attempt < maxDBRetries; attempt++ {
-			err = backoff.Retry(op, persistenceOperationRetryPolicy, common.IsPersistenceTransientError)
-			if err == nil {
-				break
-			}
-		}
+		err = backoff.Retry(op, persistenceOperationRetryPolicy, common.IsPersistenceTransientError)
 
 		if err != nil {
-			err := errors.Wrapf(err, "cannot get timer tasks for shardId %d", shardID)
-			if !skipErrMode {
-				return err
-			}
-
-			fmt.Println(err.Error())
-			return nil
+			ErrorAndExit("cannot get timer tasks for shard", err)
 		}
 
 		token = resp.NextPageToken
@@ -148,15 +120,14 @@ func printTimers(
 				continue
 			}
 
-			if _, ok := timerTypes[t.TaskType]; !ok {
+			if _, ok := taskTypes[t.TaskType]; !ok {
 				continue
 			}
 
 			data, err := json.Marshal(t)
 			if err != nil {
-				err = errors.Wrap(err, "cannot marshal timer to json")
 				if !skipErrMode {
-					return err
+					ErrorAndExit("cannot marshal timer to json", err)
 				}
 				fmt.Println(err.Error())
 			} else {
@@ -164,6 +135,4 @@ func printTimers(
 			}
 		}
 	}
-
-	return nil
 }
