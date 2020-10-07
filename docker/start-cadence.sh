2b76433f1bee222fe40b5e0cccf7098cb43e9cfb
Quanzheng Long <qlong@indeed.com>
2020-10-06 22:50:30 -0700

    Add read permission to non root users

--- docker/start-cadence.sh
+++ docker/start-cadence.sh
@@ -4,4 +4,6 @@ set -ex
 
 dockerize -template /etc/cadence/config/config_template.yaml:/etc/cadence/config/docker.yaml
 
+# Add read permission to non root users
+chmod 644 /etc/cadence/config/* 
 exec cadence-server --root $CADENCE_HOME --env docker start --services=$SERVICES
