f9eeef0c2caabc89a3eb11187dc9ec568ac9f650
Quanzheng Long <qlong@indeed.com>
2020-10-07 09:06:05 -0700

    leave config/ for mount

--- docker/start-cadence.sh
+++ docker/start-cadence.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-dockerize -template /etc/cadence/config/config_template.yaml:/etc/cadence/config/docker.yaml
+dockerize -template /etc/cadence/config_template.yaml:/etc/cadence/config/docker.yaml
 
-# Add read permission to non root users
-chmod 644 /etc/cadence/config/* 
 exec cadence-server --root $CADENCE_HOME --env docker start --services=$SERVICES
