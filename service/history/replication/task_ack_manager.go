daba348a296d5a3a5a45dd47c2bcf29e9adf2b92
Andrew Dawson <andrewjdawson2016@users.noreply.github.com>
2020-10-21 18:36:17 -0700

    Add correct license header to attribute to temporal and update license generation tool (#3674)

--- service/history/replication/task_ack_manager.go
+++ service/history/replication/task_ack_manager.go
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//go:generate mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE -source $GOFILE -destination task_ack_manager_mock.go
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination task_ack_manager_mock.go
 
 package replication
 
