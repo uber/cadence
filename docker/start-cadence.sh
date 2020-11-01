caf0093f09c69d91d05c959d35fc32f88d0e3a37
Quanzheng Long <qlong@indeed.com>
2020-11-01 14:05:16 -0800

    fix typo

--- docker/start-cadence.sh
+++ docker/start-cadence.sh
@@ -4,6 +4,6 @@ set -ex
 
 CONFIG_TEMPLATE_PATH="${CONFIG_TEMPLATE_PATH:/etc/cadence/config/config_template.yaml}"
 
-dockerize -template CONFIG_TEMPLATE_PATH:/etc/cadence/config/docker.yaml
+dockerize -template $CONFIG_TEMPLATE_PATH:/etc/cadence/config/docker.yaml
 
 exec cadence-server --root $CADENCE_HOME --env docker start --services=$SERVICES
