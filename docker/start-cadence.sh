71f35d2293d3d08f7c31e67fe6a24c773346dc0f
Quanzheng Long <qlong@indeed.com>
2020-11-01 09:27:22 -0800

    make options

--- docker/start-cadence.sh
+++ docker/start-cadence.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
-dockerize -template /etc/cadence/config_map/config_template.yaml:/etc/cadence/config/docker.yaml
+CONFIG_TEMPLATE_PATH="${CONFIG_TEMPLATE_PATH:/etc/cadence/config/config_template.yaml}"
+
+dockerize -template CONFIG_TEMPLATE_PATH:/etc/cadence/config/docker.yaml
 
 exec cadence-server --root $CADENCE_HOME --env docker start --services=$SERVICES
