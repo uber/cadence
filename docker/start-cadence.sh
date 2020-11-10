d686900f3fb77641228119246ace38664e579b49
Quanzheng Long <longquanzheng@users.noreply.github.com>
2020-11-10 10:27:53 -0800

    retry test

--- docker/start-cadence.sh
+++ docker/start-cadence.sh
@@ -7,3 +7,4 @@ CONFIG_TEMPLATE_PATH="${CONFIG_TEMPLATE_PATH:-/etc/cadence/config/config_templat
 dockerize -template $CONFIG_TEMPLATE_PATH:/etc/cadence/config/docker.yaml
 
 exec cadence-server --root $CADENCE_HOME --env docker start --services=$SERVICES
+
