c0d0b4f0eadac6af8db0d6cc71d6efb10edb72f4
Quanzheng Long <longquanzheng@users.noreply.github.com>
2020-12-03 17:04:51 -0800

    run test

--- common/service/config/config.go
+++ common/service/config/config.go
@@ -456,6 +456,7 @@ func (c *Config) fillDefaults() error {
 			c.Persistence.DataStores[k] = store
 		}
 	}
+	// filling RPCName if empty
 	if c.ClusterMetadata != nil {
 		for k, cluster := range c.ClusterMetadata.ClusterInformation {
 			if cluster.RPCName == "" {
