92a9ce3540c83a96e12ab8081c1ec013de325d60
Yu Xia <yx@uber.com>
2021-01-08 20:15:37 -0800

    fix test

--- service/history/failover/coordinator_test.go
+++ service/history/failover/coordinator_test.go
@@ -36,7 +36,6 @@ import (
 
 	"github.com/uber/cadence/client/history"
 	"github.com/uber/cadence/common"
-	"github.com/uber/cadence/common/cache"
 	"github.com/uber/cadence/common/metrics"
 	mmocks "github.com/uber/cadence/common/mocks"
 	"github.com/uber/cadence/common/persistence"
@@ -75,16 +74,7 @@ func (s *coordinatorSuite) SetupTest() {
 	s.config.NumberOfShards = 2
 	s.config.NotifyFailoverMarkerInterval = dynamicconfig.GetDurationPropertyFn(10 * time.Millisecond)
 	s.config.NotifyFailoverMarkerTimerJitterCoefficient = dynamicconfig.GetFloatPropertyFn(0.01)
-	s.mockResource.DomainCache.EXPECT().GetDomainByID(gomock.Any()).Return(
-		cache.NewGlobalDomainCacheEntryForTest(
-			&persistence.DomainInfo{Name: "test"},
-			nil,
-			nil,
-			0,
-			nil,
-		),
-		nil,
-	).AnyTimes()
+	s.mockResource.DomainCache.EXPECT().GetDomainName(gomock.Any()).Return("test", nil, ).AnyTimes()
 
 	s.coordinator = NewCoordinator(
 		s.mockMetadataManager,
