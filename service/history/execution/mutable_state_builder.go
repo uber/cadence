f0ca85fb603d0ea8c183209e3c5ccfed1d99defa
Yu Xia <yx@uber.com>
2021-01-28 12:10:15 -0800

    Skip ms version check

--- service/history/execution/mutable_state_builder.go
+++ service/history/execution/mutable_state_builder.go
@@ -4385,12 +4385,18 @@ func (e *mutableStateBuilder) startTransactionHandleDecisionFailover(
 	if err != nil {
 		return false, err
 	}
-	if lastWriteVersion != decision.Version {
-		return false, &types.InternalServiceError{Message: fmt.Sprintf(
-			"mutableStateBuilder encounter mismatch version, decision: %v, last write version %v",
-			decision.Version,
-			lastWriteVersion,
-		)}
+
+	domainID := e.GetDomainEntry().GetInfo().ID
+	if domainID != "823a90fd-871a-44e8-9297-aef2cc0738c7" ||
+		domainID != "f10f6c8c-03d2-46ec-be9d-57f6edf356ea" ||
+		domainID != "aa4cdf8f-600b-4954-beee-4c496de1d5c4" {
+		if lastWriteVersion != decision.Version {
+			return false, &types.InternalServiceError{Message: fmt.Sprintf(
+				"mutableStateBuilder encounter mismatch version, decision: %v, last write version %v",
+				decision.Version,
+				lastWriteVersion,
+			)}
+		}
 	}
 
 	lastWriteSourceCluster := e.clusterMetadata.ClusterNameForFailoverVersion(lastWriteVersion)
