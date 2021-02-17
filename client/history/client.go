9916102b25dbb7189385c98881ffa7de35860b8f
Yu Xia <yx@uber.com>
2021-02-17 13:07:44 -0800

    Revert changes to use string casting for shard ID

--- client/history/client.go
+++ client/history/client.go
@@ -22,7 +22,6 @@ package history
 
 import (
 	"context"
-	"strconv"
 	"sync"
 	"time"
 
@@ -1049,7 +1048,7 @@ func (c *clientImpl) getClientForDomainID(domainID string) (Client, error) {
 }
 
 func (c *clientImpl) getClientForShardID(shardID int) (Client, error) {
-	client, err := c.clients.GetClientForKey(strconv.Itoa(shardID))
+	client, err := c.clients.GetClientForKey(string(rune(shardID)))
 	if err != nil {
 		return nil, err
 	}
