3037490c927b48455c7807418fc2c705603e79b3
Yu Xia <yx@uber.com>
2021-02-18 00:13:11 -0800

    update header

--- service/history/historyEngine.go
+++ service/history/historyEngine.go
@@ -1,4 +1,5 @@
-// Copyright (c) 2017 Uber Technologies, Inc.
+// Copyright (c) 2017-2021 Uber Technologies, Inc.
+// Portions of the Software are attributed to Copyright (c) 2021 Temporal Technologies Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
