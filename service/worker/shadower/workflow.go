15a44efd23a723c7bbe3dcb4908ba968d9ac56e7
Yichao Yang <ycyang@uber.com>
2021-03-08 14:04:01 -0800

    use shadower idl in workflow impl

--- service/worker/shadower/workflow.go
+++ service/worker/shadower/workflow.go
@@ -28,25 +28,9 @@ import (
 	"go.uber.org/cadence/worker"
 	"go.uber.org/cadence/workflow"
 
-	"github.com/uber/cadence/common/types"
-)
-
-// The following constants should be identical to those defined in the client repo
-// TODO: move those definitions to thrift/proto definition
-
-const (
-	scanWorkflowActivityName            = "scanWorkflowActivity"
-	replayWorkflowExecutionActivityName = "replayWorkflowExecutionActivity"
-)
-
-const (
-	errMsgDomainNotExists           = "domain not exists"
-	errMsgInvalidQuery              = "invalid visibility query"
-	errMsgWorkflowTypeNotRegistered = "workflow type not registered"
-)
-
-const (
-	shadowWorkflowName = "cadence-shadow-workflow"
+	"github.com/uber/cadence/.gen/go/shadower"
+	"github.com/uber/cadence/.gen/go/shared"
+	"github.com/uber/cadence/common"
 )
 
 const (
@@ -57,79 +41,18 @@ const (
 	defaultMaxShadowCountPerRun = 100000
 )
 
-type (
-	shadowWorkflowParams struct {
-		Domain   string
-		TaskList string
-
-		WorkflowQuery string
-		NextPageToken []byte
-		SamplingRate  float64
-
-		ShadowMode    shadowMode
-		ExitCondition exitCondition
-
-		Concurrency int
-
-		LastRunResult shadowWorkflowResult
-	}
-
-	shadowWorkflowResult struct {
-		Succeeded int
-		Skipped   int
-		Failed    int
-	}
-
-	scanWorkflowActivityParams struct {
-		Domain        string
-		WorkflowQuery string
-		NextPageToken []byte
-		PageSize      int
-		SamplingRate  float64
-	}
-
-	scanWorkflowActivityResult struct {
-		Executions    []types.WorkflowExecution
-		NextPageToken []byte
-	}
-
-	replayWorkflowActivityParams struct {
-		Domain     string
-		Executions []types.WorkflowExecution
-	}
-
-	replayWorkflowActivityResult struct {
-		Succeeded int
-		Skipped   int
-		Failed    int
-		// FailedExecutions []types.WorkflowExecution
-	}
-
-	shadowMode int
-
-	exitCondition struct {
-		ExpirationInterval time.Duration
-		ShadowingCount     int
-	}
-)
-
-const (
-	shadowModeNormal shadowMode = iota + 1
-	shadowModeContinuous
-)
-
 func register(worker worker.Worker) {
 	worker.RegisterWorkflowWithOptions(
 		shadowWorkflow,
-		workflow.RegisterOptions{Name: shadowWorkflowName},
+		workflow.RegisterOptions{Name: shadower.ShadowWorkflowName},
 	)
 	worker.RegisterActivity(verifyActiveDomainActivity)
 }
 
 func shadowWorkflow(
 	ctx workflow.Context,
-	params shadowWorkflowParams,
-) (shadowWorkflowResult, error) {
+	params shadower.ShadowWorkflowParams,
+) (shadower.ShadowWorkflowResult, error) {
 	lao := workflow.LocalActivityOptions{
 		ScheduleToCloseTimeout: time.Second * 5,
 		RetryPolicy: &cadence.RetryPolicy{
@@ -141,37 +64,42 @@ func shadowWorkflow(
 	ctx = workflow.WithLocalActivityOptions(ctx, lao)
 
 	var domainActive bool
-	if err := workflow.ExecuteLocalActivity(ctx, verifyActiveDomainActivity, params.Domain).Get(ctx, &domainActive); err != nil {
-		return shadowWorkflowResult{}, err
+	if err := workflow.ExecuteLocalActivity(ctx, verifyActiveDomainActivity, params.GetDomain()).Get(ctx, &domainActive); err != nil {
+		return shadower.ShadowWorkflowResult{}, err
 	}
 
 	// TODO: we probably should make this configurable by user so that they can control is shadowing workflow
 	// should be run in active or passive side
 	if !domainActive {
-		return shadowWorkflowResult{}, nil
+		return shadower.ShadowWorkflowResult{}, nil
 	}
 
-	if params.Concurrency > defaultMaxReplayConcurrency {
-		params.Concurrency = defaultMaxReplayConcurrency
+	concurrency := params.GetConcurrency()
+	if concurrency > defaultMaxReplayConcurrency || concurrency == 0 {
+		concurrency = defaultMaxReplayConcurrency
 	}
 
 	replayStartTime := workflow.Now(ctx)
 	workflowTimeout := time.Duration(workflow.GetInfo(ctx).ExecutionStartToCloseTimeoutSeconds) * time.Second
 	retryPolicy := &cadence.RetryPolicy{
-		InitialInterval:          time.Second,
-		BackoffCoefficient:       2,
-		MaximumInterval:          time.Minute,
-		ExpirationInterval:       workflowTimeout, // retry until workflow timeout
-		NonRetriableErrorReasons: []string{errMsgDomainNotExists, errMsgInvalidQuery, errMsgWorkflowTypeNotRegistered},
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2,
+		MaximumInterval:    time.Minute,
+		ExpirationInterval: workflowTimeout, // retry until workflow timeout
+		NonRetriableErrorReasons: []string{
+			shadower.ErrReasonDomainNotExists,
+			shadower.ErrReasonInvalidQuery,
+			shadower.ShadowerTaskList,
+		},
 	}
 	scanWorkflowCtx := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskList:               params.TaskList,
+		TaskList:               params.GetTaskList(),
 		ScheduleToStartTimeout: time.Minute,
 		StartToCloseTimeout:    time.Minute,
 		RetryPolicy:            retryPolicy,
 	})
 	replayWorkflowCtx := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskList:               params.TaskList,
+		TaskList:               params.GetTaskList(),
 		ScheduleToStartTimeout: time.Minute,
 		StartToCloseTimeout:    time.Hour,
 		// do not use a short heartbeat timeout here,
@@ -180,40 +108,49 @@ func shadowWorkflow(
 		RetryPolicy:      retryPolicy,
 	})
 
-	shadowResult := params.LastRunResult
-	scanParams := scanWorkflowActivityParams{
+	var shadowResult shadower.ShadowWorkflowResult
+	if params.GetLastRunResult() != nil {
+		shadowResult = *params.GetLastRunResult()
+	} else {
+		shadowResult = shadower.ShadowWorkflowResult{
+			Succeeded: common.Int32Ptr(0),
+			Skipped:   common.Int32Ptr(0),
+			Failed:    common.Int32Ptr(0),
+		}
+	}
+	scanParams := shadower.ScanWorkflowActivityParams{
 		Domain:        params.Domain,
 		WorkflowQuery: params.WorkflowQuery,
 		NextPageToken: params.NextPageToken,
-		PageSize:      defaultScanWorkflowPageSize,
+		PageSize:      common.Int32Ptr(defaultScanWorkflowPageSize),
 		SamplingRate:  params.SamplingRate,
 	}
 	for {
-		var scanResult scanWorkflowActivityResult
-		if err := workflow.ExecuteActivity(scanWorkflowCtx, scanWorkflowActivityName, scanParams).Get(scanWorkflowCtx, &scanResult); err != nil {
+		var scanResult shadower.ScanWorkflowActivityResult
+		if err := workflow.ExecuteActivity(scanWorkflowCtx, shadower.ScanWorkflowActivityName, scanParams).Get(scanWorkflowCtx, &scanResult); err != nil {
 			return shadowResult, err
 		}
 
-		replayFutures := make([]workflow.Future, 0, params.Concurrency)
-		for _, executions := range splitExecutions(scanResult.Executions, params.Concurrency) {
-			replayParams := replayWorkflowActivityParams{
+		replayFutures := make([]workflow.Future, 0, concurrency)
+		for _, executions := range splitExecutions(scanResult.Executions, int(concurrency)) {
+			replayParams := shadower.ReplayWorkflowActivityParams{
 				Domain:     params.Domain,
 				Executions: executions,
 			}
-			future := workflow.ExecuteActivity(replayWorkflowCtx, replayWorkflowExecutionActivityName, replayParams)
+			future := workflow.ExecuteActivity(replayWorkflowCtx, shadower.ReplayWorkflowActivityName, replayParams)
 			replayFutures = append(replayFutures, future)
 		}
 
 		for _, future := range replayFutures {
-			var replayResult replayWorkflowActivityResult
+			var replayResult shadower.ReplayWorkflowActivityResult
 			if err := future.Get(replayWorkflowCtx, &replayResult); err != nil {
 				return shadowResult, err
 			}
-			shadowResult.Succeeded += replayResult.Succeeded
-			shadowResult.Skipped += replayResult.Skipped
-			shadowResult.Failed += replayResult.Failed
+			*shadowResult.Succeeded += replayResult.GetSucceeded()
+			*shadowResult.Skipped += replayResult.GetSkipped()
+			*shadowResult.Failed += replayResult.GetFailed()
 
-			if exitConditionMet(ctx, params.ExitCondition, replayStartTime, shadowResult) {
+			if exitConditionMet(ctx, params.GetExitCondition(), replayStartTime, shadowResult) {
 				return shadowResult, nil
 			}
 		}
@@ -228,7 +165,7 @@ func shadowWorkflow(
 		}
 	}
 
-	if params.ShadowMode == shadowModeContinuous {
+	if params.GetShadowMode() == shadower.ShadowModeContinuous {
 		return shadowResult, getContinueAsNewError(ctx, params, replayStartTime, shadowResult, nil)
 	}
 
@@ -236,10 +173,10 @@ func shadowWorkflow(
 }
 
 func splitExecutions(
-	executions []types.WorkflowExecution,
+	executions []*shared.WorkflowExecution,
 	concurrency int,
-) [][]types.WorkflowExecution {
-	var result [][]types.WorkflowExecution
+) [][]*shared.WorkflowExecution {
+	var result [][]*shared.WorkflowExecution
 	size := (len(executions) + concurrency - 1) / concurrency
 	for start := 0; start < len(executions); start += size {
 		end := start + size
@@ -253,17 +190,23 @@ func splitExecutions(
 
 func exitConditionMet(
 	ctx workflow.Context,
-	exitCondition exitCondition,
+	exitCondition *shadower.ExitCondition,
 	startTime time.Time,
-	currentResult shadowWorkflowResult,
+	currentResult shadower.ShadowWorkflowResult,
 ) bool {
-	if exitCondition.ExpirationInterval != 0 &&
-		workflow.Now(ctx).Sub(startTime) > exitCondition.ExpirationInterval {
+	if exitCondition == nil {
+		return false
+	}
+
+	expirationInterval := time.Duration(exitCondition.GetExpirationIntervalInSeconds()) * time.Second
+	if expirationInterval != 0 &&
+		workflow.Now(ctx).Sub(startTime) > expirationInterval {
 		return true
 	}
 
-	if exitCondition.ShadowingCount != 0 &&
-		currentResult.Succeeded+currentResult.Failed >= exitCondition.ShadowingCount {
+	shadowCount := exitCondition.GetShadowCount()
+	if shadowCount != 0 &&
+		currentResult.GetSucceeded()+currentResult.GetFailed() >= shadowCount {
 		return true
 	}
 
@@ -271,30 +214,34 @@ func exitConditionMet(
 }
 
 func shouldContinueAsNew(
-	currentResult shadowWorkflowResult,
+	currentResult shadower.ShadowWorkflowResult,
 ) bool {
-	return currentResult.Succeeded+currentResult.Skipped+currentResult.Failed > defaultMaxShadowCountPerRun
+	return currentResult.GetSucceeded()+currentResult.GetSkipped()+currentResult.GetFailed() > defaultMaxShadowCountPerRun
 }
 
 func getContinueAsNewError(
 	ctx workflow.Context,
-	params shadowWorkflowParams,
+	params shadower.ShadowWorkflowParams,
 	startTime time.Time,
-	currentResult shadowWorkflowResult,
+	currentResult shadower.ShadowWorkflowResult,
 	nextPageToken []byte,
 ) error {
 	params.NextPageToken = nextPageToken
-	if params.ExitCondition.ExpirationInterval != 0 {
-		params.ExitCondition.ExpirationInterval -= workflow.Now(ctx).Sub(startTime)
+	if params.GetExitCondition() != nil {
+		if expirationInterval := params.ExitCondition.GetExpirationIntervalInSeconds(); expirationInterval != 0 {
+			params.ExitCondition.ExpirationIntervalInSeconds = common.Int32Ptr(expirationInterval - int32(workflow.Now(ctx).Sub(startTime).Seconds()))
+		}
+
+		if shadowCount := params.ExitCondition.GetShadowCount(); shadowCount != 0 {
+			params.ExitCondition.ShadowCount = common.Int32Ptr(shadowCount - (currentResult.GetSucceeded() + currentResult.GetFailed()))
+		}
 	}
-	if params.ExitCondition.ShadowingCount != 0 {
-		params.ExitCondition.ShadowingCount -= currentResult.Succeeded + currentResult.Failed
-	}
-	params.LastRunResult = currentResult
+
+	params.LastRunResult = &currentResult
 
 	return workflow.NewContinueAsNewError(
 		ctx,
-		shadowWorkflowName,
+		shadower.ShadowWorkflowName,
 		params,
 	)
 }
