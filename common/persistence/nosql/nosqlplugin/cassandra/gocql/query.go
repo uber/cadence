ed82bb73b4aa7887529773a8fb7b1b1c2926775b
Yichao Yang <ycyang@uber.com>
2021-03-16 17:14:38 -0700

    Refresh gocql session on no connection error (#4058)

--- common/persistence/nosql/nosqlplugin/cassandra/gocql/query.go
+++ common/persistence/nosql/nosqlplugin/cassandra/gocql/query.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2017-2020 Uber Technologies, Inc.
+// Portions of the Software are attributed to Copyright (c) 2020 Temporal Technologies Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -31,8 +32,53 @@ var _ Query = (*query)(nil)
 type (
 	query struct {
 		*gocql.Query
+
+		session *session
 	}
 )
+
+func newQuery(
+	session *session,
+	gocqlQuery *gocql.Query,
+) *query {
+	return &query{
+		Query:   gocqlQuery,
+		session: session,
+	}
+}
+
+func (q *query) Exec() error {
+	err := q.Query.Exec()
+	return q.handleError(err)
+}
+
+func (q *query) Scan(
+	dest ...interface{},
+) error {
+	err := q.Query.Scan(dest...)
+	return q.handleError(err)
+}
+
+func (q *query) ScanCAS(
+	dest ...interface{},
+) (bool, error) {
+	applied, err := q.Query.ScanCAS(dest...)
+	return applied, q.handleError(err)
+}
+
+func (q *query) MapScan(
+	m map[string]interface{},
+) error {
+	err := q.Query.MapScan(m)
+	return q.handleError(err)
+}
+
+func (q *query) MapScanCAS(
+	dest map[string]interface{},
+) (bool, error) {
+	applied, err := q.Query.MapScanCAS(dest)
+	return applied, q.handleError(err)
+}
 
 func (q *query) Iter() Iter {
 	iter := q.Query.Iter()
@@ -67,12 +113,14 @@ func (q *query) WithContext(ctx context.Context) Query {
 	if q2 == nil {
 		return nil
 	}
-	return &query{
-		Query: q2,
-	}
+	return newQuery(q.session, q2)
 }
 
 func (q *query) Bind(v ...interface{}) Query {
 	q.Query.Bind(v...)
 	return q
+}
+
+func (q *query) handleError(err error) error {
+	return q.session.handleError(err)
 }
