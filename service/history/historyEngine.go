7bd91051360214181464b87325615631a1712bb7
demirkayaender <80807029+demirkayaender@users.noreply.github.com>
2021-04-01 14:03:34 -0700

    Support CLI Delay Start (#4097)
    * add delayed start support
    
    * Address comments
    
    * Add delay start support to cli
    
    * Add integration test

--- service/history/historyEngine.go
+++ service/history/historyEngine.go
@@ -777,7 +777,10 @@ UpdateWorkflowLoop:
 		}
 
 		if signalWithStartRequest != nil {
-			startRequest = getStartRequest(domainID, signalWithStartRequest.SignalWithStartRequest)
+			startRequest, err = getStartRequest(domainID, signalWithStartRequest.SignalWithStartRequest)
+			if err != nil {
+				return nil, err
+			}
 		}
 
 		err = e.addStartEventsAndTasks(
@@ -2283,7 +2286,11 @@ func (e *historyEngineImpl) SignalWithStartWorkflowExecution(
 	}
 
 	// Start workflow and signal
-	startRequest := getStartRequest(domainID, sRequest)
+	startRequest, err := getStartRequest(domainID, sRequest)
+	if err != nil {
+		return nil, err
+	}
+
 	sigWithStartArg := &signalWithStartArg{
 		signalWithStartRequest: signalWithStartRequest,
 		prevMutableState:       prevMutableState,
@@ -2757,7 +2764,7 @@ func getScheduleID(
 func getStartRequest(
 	domainID string,
 	request *types.SignalWithStartWorkflowExecutionRequest,
-) *types.HistoryStartWorkflowExecutionRequest {
+) (*types.HistoryStartWorkflowExecutionRequest, error) {
 
 	req := &types.StartWorkflowExecutionRequest{
 		Domain:                              request.Domain,
@@ -2775,10 +2782,15 @@ func getStartRequest(
 		Memo:                                request.Memo,
 		SearchAttributes:                    request.SearchAttributes,
 		Header:                              request.Header,
+		DelayStartSeconds:                   request.DelayStartSeconds,
 	}
 
-	startRequest := common.CreateHistoryStartWorkflowRequest(domainID, req, time.Now())
-	return startRequest
+	startRequest, err := common.CreateHistoryStartWorkflowRequest(domainID, req, time.Now())
+	if err != nil {
+		return nil, err
+	}
+
+	return startRequest, nil
 }
 
 func (e *historyEngineImpl) applyWorkflowIDReusePolicyForSigWithStart(
