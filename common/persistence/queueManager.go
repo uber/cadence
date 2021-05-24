d68b43eb2ff0f586117096910fee5d07f53d1e73
Yu Xia <yx@uber.com>
2021-05-24 10:55:11 -0700

    Update code for transaction updates

--- common/persistence/queueManager.go
+++ common/persistence/queueManager.go
@@ -68,20 +68,7 @@ func (q *queueManager) DeleteMessagesBefore(ctx context.Context, messageID int64
 }
 
 func (q *queueManager) UpdateAckLevel(ctx context.Context, messageID int64, clusterName string) error {
-	ackLevels, err := q.persistence.GetAckLevels(ctx)
-	if err != nil {
-		return err
-	}
-	if ackLevel, ok := ackLevels[clusterName]; ok && messageID > ackLevel {
-		// Update if the ack level moves
-		if messageID > ackLevel {
-			return q.persistence.UpdateAckLevel(ctx, messageID, clusterName)
-		}
-	} else {
-		// Insert new ack level if there is no cluster found
-		return q.persistence.UpdateAckLevel(ctx, messageID, clusterName)
-	}
-	return nil
+	return q.persistence.UpdateAckLevel(ctx, messageID, clusterName)
 }
 
 func (q *queueManager) GetAckLevels(ctx context.Context) (map[string]int64, error) {
