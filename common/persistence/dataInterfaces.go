d5fbc5e3b9c978f7ba9b1c93cb162e261df6b650
Yichao Yang <ycyang@uber.com>
2021-05-28 14:24:21 -0700

    Add cross cluster processing queue state to data/persistence interface (#4230)

--- common/persistence/dataInterfaces.go
+++ common/persistence/dataInterfaces.go
@@ -252,24 +252,25 @@ type (
 
 	// ShardInfo describes a shard
 	ShardInfo struct {
-		ShardID                       int                               `json:"shard_id"`
-		Owner                         string                            `json:"owner"`
-		RangeID                       int64                             `json:"range_id"`
-		StolenSinceRenew              int                               `json:"stolen_since_renew"`
-		UpdatedAt                     time.Time                         `json:"updated_at"`
-		ReplicationAckLevel           int64                             `json:"replication_ack_level"`
-		ReplicationDLQAckLevel        map[string]int64                  `json:"replication_dlq_ack_level"`
-		TransferAckLevel              int64                             `json:"transfer_ack_level"`
-		TimerAckLevel                 time.Time                         `json:"timer_ack_level"`
-		ClusterTransferAckLevel       map[string]int64                  `json:"cluster_transfer_ack_level"`
-		ClusterTimerAckLevel          map[string]time.Time              `json:"cluster_timer_ack_level"`
-		TransferProcessingQueueStates *types.ProcessingQueueStates      `json:"transfer_processing_queue_states"`
-		TimerProcessingQueueStates    *types.ProcessingQueueStates      `json:"timer_processing_queue_states"`
-		TransferFailoverLevels        map[string]TransferFailoverLevel  // uuid -> TransferFailoverLevel
-		TimerFailoverLevels           map[string]TimerFailoverLevel     // uuid -> TimerFailoverLevel
-		ClusterReplicationLevel       map[string]int64                  `json:"cluster_replication_level"`
-		DomainNotificationVersion     int64                             `json:"domain_notification_version"`
-		PendingFailoverMarkers        []*types.FailoverMarkerAttributes `json:"pending_failover_markers"`
+		ShardID                        int                               `json:"shard_id"`
+		Owner                          string                            `json:"owner"`
+		RangeID                        int64                             `json:"range_id"`
+		StolenSinceRenew               int                               `json:"stolen_since_renew"`
+		UpdatedAt                      time.Time                         `json:"updated_at"`
+		ReplicationAckLevel            int64                             `json:"replication_ack_level"`
+		ReplicationDLQAckLevel         map[string]int64                  `json:"replication_dlq_ack_level"`
+		TransferAckLevel               int64                             `json:"transfer_ack_level"`
+		TimerAckLevel                  time.Time                         `json:"timer_ack_level"`
+		ClusterTransferAckLevel        map[string]int64                  `json:"cluster_transfer_ack_level"`
+		ClusterTimerAckLevel           map[string]time.Time              `json:"cluster_timer_ack_level"`
+		TransferProcessingQueueStates  *types.ProcessingQueueStates      `json:"transfer_processing_queue_states"`
+		CrossClusterProcessQueueStates *types.ProcessingQueueStates      `json:"cross_cluster_queue_states"`
+		TimerProcessingQueueStates     *types.ProcessingQueueStates      `json:"timer_processing_queue_states"`
+		TransferFailoverLevels         map[string]TransferFailoverLevel  // uuid -> TransferFailoverLevel
+		TimerFailoverLevels            map[string]TimerFailoverLevel     // uuid -> TimerFailoverLevel
+		ClusterReplicationLevel        map[string]int64                  `json:"cluster_replication_level"`
+		DomainNotificationVersion      int64                             `json:"domain_notification_version"`
+		PendingFailoverMarkers         []*types.FailoverMarkerAttributes `json:"pending_failover_markers"`
 	}
 
 	// TransferFailoverLevel contains corresponding start / end level
@@ -802,7 +803,7 @@ type (
 		ShardInfo *ShardInfo
 	}
 
-	// UpdateShardRequest  is used to update shard information
+	// UpdateShardRequest is used to update shard information
 	UpdateShardRequest struct {
 		ShardInfo       *ShardInfo
 		PreviousRangeID int64
