0f93d5772151661188344472c34d9c8dfa1db073
demirkayaender <80807029+demirkayaender@users.noreply.github.com>
2021-06-09 20:45:35 -0700

    Support Feature Flags (#4257)

--- tools/cli/adminClusterCommands.go
+++ tools/cli/adminClusterCommands.go
@@ -31,6 +31,7 @@ import (
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/service/worker/failovermanager"
 
+	cc "github.com/uber/cadence/common/client"
 	"github.com/uber/cadence/common/types"
 )
 
@@ -60,7 +61,7 @@ func AdminAddSearchAttribute(c *cli.Context) {
 		SecurityToken: c.String(FlagSecurityToken),
 	}
 
-	err := adminClient.AddSearchAttribute(ctx, request)
+	err := adminClient.AddSearchAttribute(ctx, request, cc.GetDefaultCLIYarpcCallOptions()...)
 	if err != nil {
 		ErrorAndExit("Add search attribute failed.", err)
 	}
