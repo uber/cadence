5848c942aa51ce34c52a01439188117b1e615a93
demirkayaender <80807029+demirkayaender@users.noreply.github.com>
2021-06-16 10:18:16 -0700

    Support Cross Cluster Operations in SQL type DBs (#4271)

--- common/persistence/persistence-tests/shardPersistenceTest.go
+++ common/persistence/persistence-tests/shardPersistenceTest.go
@@ -31,7 +31,6 @@ import (
 
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/common/cluster"
-	"github.com/uber/cadence/common/config"
 	p "github.com/uber/cadence/common/persistence"
 	"github.com/uber/cadence/common/types"
 )
@@ -105,9 +104,6 @@ func (s *ShardPersistenceSuite) TestGetShard() {
 
 // TestUpdateShard test
 func (s *ShardPersistenceSuite) TestUpdateShard() {
-	// TODO: remove after cross-cluster queue states is persisted in SQL
-	skipCrossClusterQueueCheck := s.TestBase.Config().DefaultStore != config.StoreTypeCassandra
-
 	ctx, cancel := context.WithTimeout(context.Background(), testContextTimeout)
 	defer cancel()
 
@@ -181,9 +177,7 @@ func (s *ShardPersistenceSuite) TestUpdateShard() {
 	s.EqualTimes(updatedCurrentClusterTimerAckLevel, info1.ClusterTimerAckLevel[cluster.TestCurrentClusterName])
 	s.EqualTimes(updatedAlternativeClusterTimerAckLevel, info1.ClusterTimerAckLevel[cluster.TestAlternativeClusterName])
 	s.Equal(updatedInfo.TimerProcessingQueueStates, info1.TimerProcessingQueueStates)
-	if !skipCrossClusterQueueCheck {
-		s.Equal(updatedInfo.CrossClusterProcessingQueueStates, info1.CrossClusterProcessingQueueStates)
-	}
+	s.Equal(updatedInfo.CrossClusterProcessingQueueStates, info1.CrossClusterProcessingQueueStates)
 	s.Equal(updatedReplicationAckLevel, info1.ReplicationAckLevel)
 	s.Equal(updatedInfo.ReplicationDLQAckLevel, info1.ReplicationDLQAckLevel)
 	s.Equal(updatedStolenSinceRenew, info1.StolenSinceRenew)
@@ -209,9 +203,7 @@ func (s *ShardPersistenceSuite) TestUpdateShard() {
 	s.EqualTimes(updatedCurrentClusterTimerAckLevel, info2.ClusterTimerAckLevel[cluster.TestCurrentClusterName])
 	s.EqualTimes(updatedAlternativeClusterTimerAckLevel, info2.ClusterTimerAckLevel[cluster.TestAlternativeClusterName])
 	s.Equal(updatedInfo.TimerProcessingQueueStates, info2.TimerProcessingQueueStates)
-	if !skipCrossClusterQueueCheck {
-		s.Equal(updatedInfo.CrossClusterProcessingQueueStates, info2.CrossClusterProcessingQueueStates)
-	}
+	s.Equal(updatedInfo.CrossClusterProcessingQueueStates, info2.CrossClusterProcessingQueueStates)
 	s.Equal(updatedReplicationAckLevel, info2.ReplicationAckLevel)
 	s.Equal(updatedInfo.ReplicationDLQAckLevel, info2.ReplicationDLQAckLevel)
 	s.Equal(updatedStolenSinceRenew, info2.StolenSinceRenew)
@@ -267,9 +259,6 @@ func (s *ShardPersistenceSuite) TestCreateGetShardBackfill() {
 }
 
 func (s *ShardPersistenceSuite) TestCreateGetUpdateGetShard() {
-	// TODO: remove after cross-cluster queue states is persisted in SQL
-	skipCrossClusterQueueCheck := s.TestBase.Config().DefaultStore != config.StoreTypeCassandra
-
 	ctx, cancel := context.WithTimeout(context.Background(), testContextTimeout)
 	defer cancel()
 
@@ -330,9 +319,7 @@ func (s *ShardPersistenceSuite) TestCreateGetUpdateGetShard() {
 	s.EqualTimes(shardInfo.ClusterTimerAckLevel[cluster.TestCurrentClusterName], resp.ClusterTimerAckLevel[cluster.TestCurrentClusterName])
 	s.EqualTimes(shardInfo.ClusterTimerAckLevel[cluster.TestAlternativeClusterName], resp.ClusterTimerAckLevel[cluster.TestAlternativeClusterName])
 
-	if skipCrossClusterQueueCheck {
-		resp.CrossClusterProcessingQueueStates = shardInfo.CrossClusterProcessingQueueStates
-	}
+	resp.CrossClusterProcessingQueueStates = shardInfo.CrossClusterProcessingQueueStates
 	resp.TimerAckLevel = shardInfo.TimerAckLevel
 	resp.UpdatedAt = shardInfo.UpdatedAt
 	resp.ClusterTimerAckLevel = shardInfo.ClusterTimerAckLevel
@@ -394,9 +381,7 @@ func (s *ShardPersistenceSuite) TestCreateGetUpdateGetShard() {
 	s.EqualTimes(shardInfo.ClusterTimerAckLevel[cluster.TestCurrentClusterName], resp.ClusterTimerAckLevel[cluster.TestCurrentClusterName])
 	s.EqualTimes(shardInfo.ClusterTimerAckLevel[cluster.TestAlternativeClusterName], resp.ClusterTimerAckLevel[cluster.TestAlternativeClusterName])
 
-	if skipCrossClusterQueueCheck {
-		resp.CrossClusterProcessingQueueStates = shardInfo.CrossClusterProcessingQueueStates
-	}
+	resp.CrossClusterProcessingQueueStates = shardInfo.CrossClusterProcessingQueueStates
 	resp.UpdatedAt = shardInfo.UpdatedAt
 	resp.TimerAckLevel = shardInfo.TimerAckLevel
 	resp.ClusterTimerAckLevel = shardInfo.ClusterTimerAckLevel
