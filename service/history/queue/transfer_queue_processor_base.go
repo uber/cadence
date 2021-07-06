de9a086942455cbb34e46296ca7114906ff9cd3e
Yu Xia <yx@uber.com>
2021-07-06 16:58:44 -0700

    Add cross cluster queue processor (#4269)
    *Add cross-cluster queue processor

--- service/history/queue/transfer_queue_processor_base.go
+++ service/history/queue/transfer_queue_processor_base.go
@@ -53,11 +53,6 @@ type (
 		taskID int64
 	}
 
-	pollTime struct {
-		time       time.Time
-		changeable bool
-	}
-
 	transferQueueProcessorBase struct {
 		*processorBase
 
@@ -328,7 +323,7 @@ processorPumpLoop:
 
 			t.processQueueCollections()
 		case <-updateAckTimer.C:
-			processFinished, err := t.updateAckLevel()
+			processFinished, _, err := t.updateAckLevel()
 			if err == shard.ErrShardClosed || (err == nil && processFinished) {
 				go t.Stop()
 				break processorPumpLoop
@@ -514,7 +509,7 @@ func (t *transferQueueProcessorBase) readTasks(
 		return err
 	}
 
-	err := backoff.Retry(op, persistenceOperationRetryPolicy, persistence.IsTransientError)
+	err := backoff.Retry(op, persistenceOperationRetryPolicy, persistence.IsBackgroundTransientError)
 	if err != nil {
 		return nil, false, err
 	}
