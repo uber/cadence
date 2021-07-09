3f5689f8bf1a28c16934b9e43a0696eb83c318cb
Rodrigo Villarreal <noiarek@gmail.com>
2021-07-09 03:11:06 -0700

    Adding header validation

--- common/authorization/oauthAuthorizer.go
+++ common/authorization/oauthAuthorizer.go
@@ -21,75 +21,89 @@
 package authorization
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"github.com/cristalhq/jwt/v3"
-	"github.com/uber/cadence/common/config"
-	"google.golang.org/grpc/metadata"
-	"strings"
-	"time"
+    "context"
+    "encoding/json"
+    "fmt"
+    "strings"
+    "time"
+
+    "github.com/cristalhq/jwt/v3"
+    "google.golang.org/grpc/metadata"
+
+    "github.com/uber/cadence/common/config"
 )
 
-type oauthAuthority struct{
-	cfg *config.Config
+type oauthAuthority struct {
+    cfg *config.Config
 }
 
 type jwtClaims struct {
-	name string
-	permission string
-	domain string
-	iat int64
+    name       string
+    permission string
+    domain     string
+    iat        int64
 }
 
 // NewOAuthAuthorizer creates a no-op authority
 func NewOAuthAuthorizer(
-	cfg *config.Config,
-	) Authorizer {
-	return &oauthAuthority{
-		cfg: cfg,
-	}
+    cfg *config.Config,
+) Authorizer {
+    return &oauthAuthority{
+        cfg: cfg,
+    }
 }
 
 func (a *oauthAuthority) Authorize(
-	ctx context.Context,
-	attributes *Attributes,
+    ctx context.Context,
+    attributes *Attributes,
 ) (Result, error) {
 
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return Result{Decision: DecisionDeny}, fmt.Errorf("ctx metadata was not found. Unable to authorize")
-	}
-	// potentially claims will populate the context, otherwise we can
-	// return bool here
-	_, err := a.validateAndGetClaims(md["authorization"])
-	if err != nil {
-		return Result{Decision: DecisionDeny}, fmt.Errorf("authentication required")
-	}
-	return Result{Decision: DecisionAllow}, nil
+    md, ok := metadata.FromIncomingContext(ctx)
+    if !ok {
+        return Result{Decision: DecisionDeny}, fmt.Errorf("ctx metadata was not found. Unable to authorize")
+    }
+    authorization, ok := md["authorization"]
+    if !ok {
+        return Result{Decision: DecisionDeny}, fmt.Errorf("authorization header not populated")
+    }
+    // parseTokenAndVerify could either return the claims or a bool. I'm returning the claims currently
+    // because we might use it to populate values in the context
+    _, err := a.parseTokenAndVerify(authorization)
+    if err != nil {
+        return Result{Decision: DecisionDeny}, err
+    }
+    return Result{Decision: DecisionAllow}, nil
 }
 
-func (a *oauthAuthority) validateAndGetClaims(authorization []string) (*jwtClaims, error) {
-	key := []byte(a.cfg.Authorization.OAuthAuthorizer.JwtCredentials.PrivateKey)
-	algorithm := jwt.Algorithm(a.cfg.Authorization.OAuthAuthorizer.JwtCredentials.Algorithm)
-	verifier, err := jwt.NewVerifierHS(algorithm, key)
-	if err != nil {
-		return nil, err
-	}
-	tokenStr := strings.Join(authorization, ".")
-	token, verifyErr := jwt.ParseAndVerifyString(tokenStr, verifier)
-	if verifyErr != nil {
-		return nil, verifyErr
-	}
-	var claims jwtClaims
-	claimsErr := json.Unmarshal(token.RawClaims(), &claims)
-	if claimsErr != nil {
-		return nil, err
-	}
-	if claims.iat < time.Now().Unix() {
-		return nil, fmt.Errorf("JWT has expired")
-	}
+func (a *oauthAuthority) parseTokenAndVerify(authorization []string) (*jwtClaims, error) {
+    key := []byte(a.cfg.Authorization.OAuthAuthorizer.JwtCredentials.PrivateKey)
+    algorithm := jwt.Algorithm(a.cfg.Authorization.OAuthAuthorizer.JwtCredentials.Algorithm)
+    verifier, err := jwt.NewVerifierHS(algorithm, key)
+    if err != nil {
+        return nil, err
+    }
+    tokenStr := strings.Join(authorization, ".")
+    token, verifyErr := jwt.ParseAndVerifyString(tokenStr, verifier)
+    if verifyErr != nil {
+        return nil, verifyErr
+    }
+    var claims jwtClaims
+    claimsErr := json.Unmarshal(token.RawClaims(), &claims)
+    if claimsErr != nil {
+        return nil, err
+    }
+    validationErr := a.validateClaims(claims)
+    if validationErr != nil {
+        return nil, validationErr
+    }
 
-	return &claims, nil
+    return &claims, nil
 }
 
+func (a *oauthAuthority) validateClaims(claims jwtClaims) error {
+    if claims.iat < time.Now().Unix() {
+        return fmt.Errorf("JWT has expired")
+    }
+
+    return nil
+}
