decf85dcf20286020373e4e80ca5004ee3169884
Allen Liu <allen.liu@uber.com>
2021-08-13 17:40:15 -0400

    regenerated mocks

--- common/persistence/dataManagerInterfaces.go
+++ common/persistence/dataManagerInterfaces.go
@@ -19,6 +19,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination dataManagerInterfaces_mock.go -self_package github.com/uber/cadence/common/persistence
+
 package persistence
 
 import (
