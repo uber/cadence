25381311f30f88b2ba9e05e55c7f93045ab08c82
Quanzheng Long <qlong@indeed.com>
2021-09-24 20:33:09 -0700

    comment

--- common/config/config.go
+++ common/config/config.go
@@ -242,14 +242,20 @@ type (
 	// SQL is the configuration for connecting to a SQL backed datastore
 	SQL struct {
 		// User is the username to be used for the conn
+		// If useMultipleDatabases, must be empty and provide it via multipleDatabasesConfig instead
 		User string `yaml:"user"`
 		// Password is the password corresponding to the user name
+		// If useMultipleDatabases, must be empty and provide it via multipleDatabasesConfig instead
 		Password string `yaml:"password"`
 		// PluginName is the name of SQL plugin
 		PluginName string `yaml:"pluginName" validate:"nonzero"`
 		// DatabaseName is the name of SQL database to connect to
+		// If useMultipleDatabases, must be empty and provide it via multipleDatabasesConfig instead
+		// Required if not useMultipleDatabases
 		DatabaseName string `yaml:"databaseName"`
 		// ConnectAddr is the remote addr of the database
+		// If useMultipleDatabases, must be empty and provide it via multipleDatabasesConfig instead
+		// Required if not useMultipleDatabases
 		ConnectAddr string `yaml:"connectAddr"`
 		// ConnectProtocol is the protocol that goes with the ConnectAddr ex - tcp, unix
 		ConnectProtocol string `yaml:"connectProtocol" validate:"nonzero"`
