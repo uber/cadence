c67279d61c4cce34a3970fcd045222db02d0af93
Quanzheng Long <qlong@indeed.com>
2021-10-07 13:43:53 -0700

    Add console as logging encoding type

--- common/config/config.go
+++ common/config/config.go
@@ -317,14 +317,20 @@ type (
 
 	// Logger contains the config items for logger
 	Logger struct {
-		// Stdout is true if the output needs to goto standard out
+		// Stdout is true then the output needs to goto standard out
+		// By default this is false and output will go to standard error
 		Stdout bool `yaml:"stdout"`
 		// Level is the desired log level
 		Level string `yaml:"level"`
 		// OutputFile is the path to the log output file
+		// Stdout must be false, otherwise Stdout will take precedence
 		OutputFile string `yaml:"outputFile"`
-		// levelKey is the desired log level, defaults to "level"
+		// LevelKey is the desired log level, defaults to "level"
 		LevelKey string `yaml:"levelKey"`
+		// Encoding decides the format, supports "console" and "json".
+		// "json" will print the log in JSON format(better for machine), while "console" will print in plain-text format(more human friendly)
+		// Default is "json"
+		Encoding string `yaml:"encoding"`
 	}
 
 	// DCRedirectionPolicy contains the frontend datacenter redirection policy
