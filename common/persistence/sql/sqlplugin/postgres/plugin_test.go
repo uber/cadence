4048370c8defd7e49dfd5d06eeffa244f7357cdf
Agrim Asthana <agrimrules@gmail.com>
2021-10-08 12:39:28 -0500

    URL encode postgres credentials (#4550)

---
+++ common/persistence/sql/sqlplugin/postgres/plugin_test.go
@@ -0,0 +1,59 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2017-2020 Uber Technologies Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package postgres
+
+import "testing"
+
+var testCases = []struct {
+	name     string
+	username string
+	password string
+	want     string
+}{
+	{
+		name:     "default",
+		username: "cadence",
+		password: "cadence",
+		want:     "cadence:cadence",
+	},
+	{
+		name:     "with forward slash",
+		username: "cadence",
+		password: "cad/ence",
+		want:     "cadence:cad%2Fence",
+	},
+	{
+		name:     "with question mark",
+		username: "cadence",
+		password: "cad?ence",
+		want:     "cadence:cad%3Fence",
+	},
+}
+
+func TestGenerateCredentialString(t *testing.T) {
+	for _, tc := range testCases {
+		if userPass := generateCredentialString(tc.username, tc.password); userPass != tc.want {
+			t.Errorf("%v: got %v, want %v", tc.name, userPass, tc.want)
+		}
+	}
+}
