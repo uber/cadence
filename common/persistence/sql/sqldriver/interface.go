4b4cef0adbb0b1beb311fa7e9ece7b762441bdee
Quanzheng Long <qlong@indeed.com>
2021-10-21 09:21:40 -0700

    fix comment

--- common/persistence/sql/sqldriver/interface.go
+++ common/persistence/sql/sqldriver/interface.go
@@ -46,13 +46,10 @@ type (
 		Close() error
 
 		// ExecDDL executes a DDL query
-		// For multiple databases, it will execute the query in all of them
 		ExecDDL(ctx context.Context, dbShardID int, query string, args ...interface{}) (sql.Result, error)
 		// SelectForSchemaQuery executes a select query for schema(returning multiple rows).
-		// For multiple databases, it will execute the query in all of them and validate they are the same using deep equal
 		SelectForSchemaQuery(dbShardID int, dest interface{}, query string, args ...interface{}) error
 		// GetForSchemaQuery executes a get query for schema(returning single row).
-		// For multiple databases, it will execute the query in all of them and validate they are the same using deep equal
 		GetForSchemaQuery(dbShardID int, dest interface{}, query string, args ...interface{}) error
 	}
 
