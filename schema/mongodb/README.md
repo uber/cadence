9a0abd4e2b9acaca25f7576422f43701d5ed881f
Quanzheng Long <qlong@indeed.com>
2021-10-23 11:03:48 -0700

    init

--- schema/mongodb/README.md
+++ schema/mongodb/README.md
@@ -2,4 +2,70 @@ What
 ----
 This directory contains the mongodb schema for every database that cadence owns. The directory structure is as follows
 
-TODO
+
+```
+./schema
+   - cadence/               -- Contains schema for default data models
+        - schema.json       -- Contains the latest & greatest snapshot of the schema for the keyspace
+        - schema.go         -- Contains the collection schema in Golang structs -- because MongoDB collection is shemaless.  
+        - versioned
+             - v0.1/
+             - v0.2/        -- One directory per schema version change
+             - v1.0/
+                - manifest.json    -- json file describing the change
+                - changes.json     -- changes in this version, only [create collection/index/documents] commands are allowed
+```
+
+## MongoDB JSON schema format
+Below is an example of a schema JSON file containing two commands, for collection/index/documents creation. 
+```json
+[
+  {
+    "create": "collectionName"
+  },
+  {
+    "createIndexes": "collectionName",
+    "indexes": [
+      {
+        "key": {
+          "fieldNameA": 1,
+          "fieldNameB": -1
+        },
+        "name": "rowType_version"
+      }
+    ],
+    "writeConcern": { "w": "majority" }
+  },
+  {
+    "insert": "collectionName",
+    "documents": [
+      {
+        "fieldNameA": 1,
+        "fieldNameB": 0,
+        "fieldNameC": "1234"
+      },
+      {
+        "fieldNameA": 2,
+        "fieldNameB": 1,
+        "fieldNameC": "12344"
+      },
+      {
+        "fieldNameA": 2,
+        "fieldNameB": 2,
+        "fieldNameC": "12345"
+      }
+    ],
+    "ordered": false
+  }
+]
+```
+
+
+How
+---
+
+Q: How do I update existing schema ?
+* Add your changes to schema.json for snapshot
+* Create a new schema version directory under ./schema/<>/versioned/vx.x
+  * Add a manifest.json
+  * Add your changes in a json file
--- schema/mongodb/cadence/schema.go
+++ schema/mongodb/cadence/schema.go
@@ -1,8 +1,7 @@
-package main
+package cadence
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -17,14 +16,15 @@ import (
 
 // NOTE: MongoDB is schemaless, so there is no schema operation to update the fields. Here uses Go struct to define the document fields directly.
 
-// ConfigStoreEntry is the schema of configStore
-type ConfigStoreEntry struct {
-	ID int `json:"_id,omitempty"`
-	RowType int `json:"rowType"`
-	Version int64 `json:"version"`
-	Data []byte `json:"data"`
+// ClusterConfigEntry is the schema of configStore
+// IMPORTANT: making change to this struct is changing the MongoDB collection schema. Please make sure it's backward compatible.
+type ClusterConfigEntry struct {
+	ID           int    `json:"_id,omitempty"`
+	RowType      int    `json:"rowType"`
+	Version      int64  `json:"version"`
+	Data         []byte `json:"data"`
 	DataEncoding string `json:"dataEncoding"`
-	Timestamp int64`json:"Timestamp"`
+	Timestamp    int64  `json:"Timestamp"`
 }
 
 func main() {
@@ -40,68 +40,82 @@ func main() {
 	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
 	db := client.Database("test4")
 
-	// create collection
-	byteValues, err := ioutil.ReadFile("./collection.json")
-	var command interface{}
-	err = bson.UnmarshalExtJSON(byteValues, false, &command)
+	// all
+	byteValues, err := ioutil.ReadFile("./all.json")
+	var commands []interface{}
+	err = bson.UnmarshalExtJSON(byteValues, false, &commands)
 	if err != nil {
 		// Print any IO errors with the .json file
 		fmt.Println("ioutil.ReadFile ERROR:", err)
 		os.Exit(0)
 	}
-	result := db.RunCommand(context.Background(), command)
-	fmt.Println("collection created", command, result.Err())
-
-	// create index
-	byteValues, err = ioutil.ReadFile("./index.json")
-	err = bson.UnmarshalExtJSON(byteValues, false, &command)
-	if err != nil {
-		// Print any IO errors with the .json file
-		fmt.Println("ioutil.ReadFile ERROR:", err)
-		os.Exit(0)
+	for _, cmd := range commands {
+		result := db.RunCommand(ctx, cmd)
+		fmt.Println("execute command", cmd, result.Err())
 	}
-	result = db.RunCommand(context.Background(), command)
-	fmt.Println("index created", command, result.Err())
 
-	// insert data
-	col := db.Collection("test4")
-	fmt.Println("Collection type:", reflect.TypeOf(col), "n")
-
-	// Load values from JSON file to model
-	byteValues, err = ioutil.ReadFile("./docs.json")
-	if err != nil {
-		// Print any IO errors with the .json file
-		fmt.Println("ioutil.ReadFile ERROR:", err)
-	} else {
-		// Print the values of the JSON docs, and insert them if no error
-		fmt.Println("byteValues:", string(byteValues))
-
-		// Declare an empty slice for the MongoFields docs
-		var docs []ConfigStoreEntry
-
-		// Unmarshal the encoded JSON byte string into the slice
-		err = json.Unmarshal(byteValues, &docs)
-
-		// Print MongoDB docs object type
-		fmt.Println("nMongoFields Docs:", reflect.TypeOf(docs))
-
-		// Iterate the slice of MongoDB struct docs
-		for i := range docs {
-
-			// Put the document element in a new variable
-			doc := docs[i]
-			fmt.Println("ndoc _id:", doc.ID)
-			fmt.Println("doc Field Str:", doc.ID)
-
-			// Call the InsertOne() method and pass the context and doc objects
-			result, insertErr := col.InsertOne(ctx, doc)
-
-			// Check for any insertion errors
-			if insertErr != nil {
-				fmt.Println("InsertOne ERROR:", insertErr)
-			} else {
-				fmt.Println("InsertOne() API result:", result)
-			}
-		}
-	}
+	// // create collection
+	// byteValues, err := ioutil.ReadFile("./collection.json")
+	// var command interface{}
+	// err = bson.UnmarshalExtJSON(byteValues, false, &command)
+	// if err != nil {
+	// 	// Print any IO errors with the .json file
+	// 	fmt.Println("ioutil.ReadFile ERROR:", err)
+	// 	os.Exit(0)
+	// }
+	// result := db.RunCommand(context.Background(), command)
+	// fmt.Println("collection created", command, result.Err())
+	//
+	// // create index
+	// byteValues, err = ioutil.ReadFile("./index.json")
+	// err = bson.UnmarshalExtJSON(byteValues, false, &command)
+	// if err != nil {
+	// 	// Print any IO errors with the .json file
+	// 	fmt.Println("ioutil.ReadFile ERROR:", err)
+	// 	os.Exit(0)
+	// }
+	// result = db.RunCommand(context.Background(), command)
+	// fmt.Println("index created", command, result.Err())
+	//
+	// // insert data
+	// col := db.Collection("test4")
+	// fmt.Println("Collection type:", reflect.TypeOf(col), "n")
+	//
+	// // Load values from JSON file to model
+	// byteValues, err = ioutil.ReadFile("./docs.json")
+	// if err != nil {
+	// 	// Print any IO errors with the .json file
+	// 	fmt.Println("ioutil.ReadFile ERROR:", err)
+	// } else {
+	// 	// Print the values of the JSON docs, and insert them if no error
+	// 	fmt.Println("byteValues:", string(byteValues))
+	//
+	// 	// Declare an empty slice for the MongoFields docs
+	// 	var docs []ClusterConfigEntry
+	//
+	// 	// Unmarshal the encoded JSON byte string into the slice
+	// 	err = json.Unmarshal(byteValues, &docs)
+	//
+	// 	// Print MongoDB docs object type
+	// 	fmt.Println("nMongoFields Docs:", reflect.TypeOf(docs))
+	//
+	// 	// Iterate the slice of MongoDB struct docs
+	// 	for i := range docs {
+	//
+	// 		// Put the document element in a new variable
+	// 		doc := docs[i]
+	// 		fmt.Println("ndoc _id:", doc.ID)
+	// 		fmt.Println("doc Field Str:", doc.ID)
+	//
+	// 		// Call the InsertOne() method and pass the context and doc objects
+	// 		result, insertErr := col.InsertOne(ctx, doc)
+	//
+	// 		// Check for any insertion errors
+	// 		if insertErr != nil {
+	// 			fmt.Println("InsertOne ERROR:", insertErr)
+	// 		} else {
+	// 			fmt.Println("InsertOne() API result:", result)
+	// 		}
+	// 	}
+	// }
 }
