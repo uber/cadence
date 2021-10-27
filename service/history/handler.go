28b4916ce932735a62cc98299fba42c306fdd1e4
Yichao Yang <ycyang@uber.com>
2021-10-26 17:56:41 -0700

    pr comments

--- service/history/handler.go
+++ service/history/handler.go
@@ -1968,10 +1968,10 @@ func (h *handlerImpl) RespondCrossClusterTasksCompleted(
 
 	response := &types.RespondCrossClusterTasksCompletedResponse{}
 	if request.FetchNewTasks {
-		ctx, cancel := common.CreateChildContext(ctx, 0.05)
+		fetchTaskCtx, cancel := common.CreateChildContext(ctx, 0.05)
 		defer cancel()
 
-		response.Tasks, err = engine.GetCrossClusterTasks(ctx, request.TargetCluster)
+		response.Tasks, err = engine.GetCrossClusterTasks(fetchTaskCtx, request.TargetCluster)
 		if err != nil {
 			return nil, h.error(err, scope, "", "")
 		}
