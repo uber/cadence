49df6717ee9872829cbc7ee8193b6fa3b0e26cd6
Yu Xia <yx@uber.com>
2021-10-29 14:16:18 -0700

    Handle error case in response info middleware (#4609)

--- common/rpc/middleware_test.go
+++ common/rpc/middleware_test.go
@@ -22,6 +22,7 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -36,16 +37,25 @@ func TestResponseInfoMiddleware(t *testing.T) {
 	assert.Equal(t, 12345, responseInfo.Size)
 }
 
+func TestResponseInfoMiddleware_Error(t *testing.T) {
+	m := ResponseInfoMiddleware{}
+	ctx, responseInfo := ContextWithResponseInfo(context.Background())
+	_, err := m.Call(ctx, &transport.Request{}, &fakeOutbound{err: fmt.Errorf("test")})
+	assert.Error(t, err)
+	assert.Equal(t, 0, responseInfo.Size)
+}
+
 type fakeOutbound struct {
 	verify   func(*transport.Request)
 	response *transport.Response
+	err      error
 }
 
 func (o fakeOutbound) Call(ctx context.Context, request *transport.Request) (*transport.Response, error) {
 	if o.verify != nil {
 		o.verify(request)
 	}
-	return o.response, nil
+	return o.response, o.err
 }
 func (o fakeOutbound) Start() error                      { return nil }
 func (o fakeOutbound) Stop() error                       { return nil }
