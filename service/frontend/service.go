3df4285d0ba81d727a289b9d558415212275062f
Yu Xia <yx@uber.com>
2021-10-28 18:42:42 -0700

    Revert "Change frontend drain time"
    This reverts commit 140a71649cf8f199bdabcd3dbba8a7130e3e2e9f.

--- service/frontend/service.go
+++ service/frontend/service.go
@@ -307,8 +307,8 @@ func (s *Service) Stop() {
 	// 4. Wait for a second
 	// 5. Stop everything forcefully and return
 
-	requestDrainTime := common.MaxDuration(time.Second, s.config.ShutdownDrainDuration())
-	failureDetectionTime := 10 * time.Second
+	requestDrainTime := common.MinDuration(time.Second, s.config.ShutdownDrainDuration())
+	failureDetectionTime := common.MaxDuration(0, s.config.ShutdownDrainDuration()-requestDrainTime)
 
 	s.GetLogger().Info("ShutdownHandler: Updating rpc health status to ShuttingDown")
 	s.handler.UpdateHealthStatus(HealthStatusShuttingDown)
