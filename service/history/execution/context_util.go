155fa4b6d0d3f1cb8d97f826df51f7181df57aee
Zack Kirsch <zackk@uber.com>
2022-06-14 16:03:19 -0700

    Revert "PR requests"
    This reverts commit 832bcfe82e60c080841c8cf0b42b7364adbc54b2.

--- service/history/execution/context_util.go
+++ service/history/execution/context_util.go
@@ -21,7 +21,6 @@
 package execution
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/uber/cadence/common/log"
@@ -158,14 +157,10 @@ func emitWorkflowCompletionStats(
 		)
 	case types.EventTypeWorkflowExecutionContinuedAsNew:
 		scope.IncCounter(metrics.WorkflowContinuedAsNew)
-		logger.Info("workflow continued as new",
-			tag.WorkflowID(workflowID),
-			tag.WorkflowRunID(runID),
-			tag.WorkflowDomainName(domainName),
-		)
 	default:
 		scope.IncCounter(metrics.WorkflowCompletedUnknownType)
-		logger.Warn(fmt.Sprintf("Workflow completed with an unknown event type %s", event.EventType.String()),
+		logger.Info("NOT emitting completion stats for event type",
+			tag.WorkflowEventType(event.EventType.String()),
 			tag.WorkflowID(workflowID),
 			tag.WorkflowRunID(runID),
 			tag.WorkflowDomainName(domainName),
