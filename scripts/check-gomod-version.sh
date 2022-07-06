2789b266ba51da3feed0d249a2b840ada73a514a
Steven Littiebrant <stevenl@uber.com>
2022-07-06 00:10:57 -0500

    yea, set -x is the obvious choice here

--- scripts/check-gomod-version.sh
+++ scripts/check-gomod-version.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [[ $2 = "-v" ]]; then
-    v () {
-        >&2 echo "$@"
-    }
-else
-    v () {
-        true # noop
-    }
-fi
+[[ $2 = "-v" ]] && set -x;
 
 if ! gomod="$(go list -mod=readonly -f '{{ .Module }}' "$1")"; then
     >&2 echo 'Error checking main go.mod.'
     exit 1
 fi
 
-v "go.mod:                $gomod"
-
 if ! toolmod="$(go list -mod=readonly -modfile=internal/tools/go.mod -f '{{ .Module }}' "$1")"; then
     >&2 echo 'Error checking tools go.mod, cd to internal/tools to modify it.'
     exit 1
 fi
-
-v "internal/tools/go.mod: $toolmod"
 
 if [[ $gomod != $toolmod ]]; then
 	>&2 echo "error: mismatched go.mod and tools go.mod"
