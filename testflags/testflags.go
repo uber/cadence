6d5323ac9057df72045c7f29eccfe974e6415229
Zack Kirsch <zackk@uber.com>
2022-08-30 18:46:29 -0700

    Change testflags to use environment variables instead of testflags.

--- testflags/testflags.go
+++ testflags/testflags.go
@@ -23,46 +23,52 @@
 package testflags
 
 import (
-	"flag"
+	"fmt"
+	"os"
 	"testing"
 )
 
-var TestFlags struct {
-	mysql     bool
-	postgres  bool
-	mongodb   bool
-	cassandra bool
-}
-
-func init() {
-	flag.BoolVar(&TestFlags.mysql, "mysql", false, "MySQL external dependency")
-	flag.BoolVar(&TestFlags.postgres, "postgres", false, "PostGreSQL external dependency")
-	flag.BoolVar(&TestFlags.mongodb, "mongodb", false, "MongoDB external dependency")
-	flag.BoolVar(&TestFlags.cassandra, "cassandra", false, "Cassandra external dependency")
-}
-
+// Pass these testflags in as environment variables in order to run tests marked as needing an
+// external dependency.
+// Tests can be marked as having an external dependency by calling the Require* functions below.
 // In order to start external dependencies, see documentation at https://github.com/uber/cadence
 
+var (
+	cassandra = "CASSANDRA"
+	mongodb   = "MONGODB"
+	mysql     = "MYSQL"
+	postgres  = "POSTGRES"
+)
+
+// NOTE: We are using environment variables instead of go testflags or go build directives, because we:
+// 1) Want tests to be built all the time (go build directives don't give us that)
+// 2) Want tests to be marked as skipped (go build directives don't give us that)
+// 3) Want to be able to run individual tests (go testflags errors if the test doesn't import
+// something that defines the testflags.)
+
 func RequireMySQL(t *testing.T) {
-	if !TestFlags.mysql {
-		t.Skip("Skipping test that requires 'mysql' to run - start mysql and append '-mysql' to run this test.")
-	}
+	require(t, mysql)
 }
 
 func RequirePostgres(t *testing.T) {
-	if !TestFlags.postgres {
-		t.Skip("Skipping test that requires 'postgres' to run - start postgres and append '-postgres' to run this test.")
-	}
+	require(t, postgres)
 }
 
 func RequireMongoDB(t *testing.T) {
-	if !TestFlags.mongodb {
-		t.Skip("Skipping test that requires 'mongodb' to run - start mongodb and append '-mongodb' to run this test.")
-	}
+	require(t, mongodb)
 }
 
 func RequireCassandra(t *testing.T) {
-	if !TestFlags.cassandra {
-		t.Skip("Skipping test that requires 'cassandra' to run - start cassandra and append '-cassandra' to run this test.")
+	require(t, cassandra)
+}
+
+func require(t *testing.T, name string) {
+	if !checkEnv(name) {
+		t.Skip(fmt.Sprintf("Skipping test that requires %s to run - start %s and set '%s=1' environment variable to run this test.",
+			name, name, name))
 	}
+}
+
+func checkEnv(name string) bool {
+	return os.Getenv(name) != ""
 }
