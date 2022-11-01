eb561b8f7c172fa61e0656907ba16769bd51f1d5
Allen Chen <allenchen2244@uber.com>
2022-11-01 15:18:26 -0700

    add script SKIP VERSION

--- common/version.go
+++ common/version.go
@@ -3,4 +3,4 @@ package common
 const MajorVersion = "0.25"
 
 // VersionString this is updated via scripts/update-version.sh
-const VersionString = "0.25.1667340140"
+const VersionString = "0.25.1667341052"
