5b453833aeca13e34670efa7fa62ef5f503d3936
Allen Chen <allenchen2244@uber.com>
2022-11-01 14:55:30 -0700

    SKIP VERSION

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 cd "$(dirname $0)"
-
 # 0.25
+
 majorVersion="$(grep MajorVersion ../common/version.go | awk '{print $4}' | sed 's/"//g')"
 versionString="$majorVersion.$(date +%s)"
 goLiteral="const VersionString = \"$versionString\""
