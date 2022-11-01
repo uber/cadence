5bad2e0bf253f0c90ae0fc09aee7dcad34251f97
Allen Chen <allenchen2244@uber.com>
2022-11-01 14:19:10 -0700

    TEST MESSAGE

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -2,6 +2,7 @@
 cd "$(dirname $0)"
 
 # 0.25
+
 majorVersion="$(grep MajorVersion ../common/version.go | awk '{print $4}' | sed 's/"//g')"
 versionString="$majorVersion.$(date +%s)"
 goLiteral="const VersionString = \"$versionString\""
