f9868a693124909a0e775a9381a0f424414e1233
Allen Chen <allenchen2244@uber.com>
2022-11-01 16:16:57 -0700

    fix SKIP VERSION

--- common/metrics/version.go
+++ common/metrics/version.go
@@ -25,4 +25,4 @@ package metrics
 const MajorVersion = "0.25"
 
 // VersionString this is updated via scripts/update-version.sh
-const VersionString = "0.25.1667341052"
+const VersionString = "0.25.1667344608"
--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -3,6 +3,7 @@ cd "$(dirname $0)"
 
 majorVersion="$(grep MajorVersion ../common/metrics/version.go | awk '{print $4}' | sed 's/"//g')"
 versionString="$majorVersion.$(date +%s)"
+
 goLiteral="const VersionString = \"$versionString\""
 
 sed -i "" "s/^const VersionString = \".*\"$/$goLiteral/" ../common/metrics/version.go
