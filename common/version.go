fd7631f5ec36869824c40e36ca7e3996861e1cbe
Allen Chen <allenchen2244@uber.com>
2022-10-31 18:24:42 -0700

    Test

--- common/version.go
+++ common/version.go
@@ -2,5 +2,5 @@ package common
 
 const MajorVersion = "0.25"
 
-// VersionString this updated via scripts/update-version.sh
+// VersionString this is updated via scripts/update-version.sh
 const VersionString = "0.25.1667258661"
