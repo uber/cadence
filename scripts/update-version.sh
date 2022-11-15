2727f8e505e63cfdd1a007f461d500603a7556ed
Allen Chen <allenchen2244@uber.com>
2022-11-15 14:37:12 -0800

    revert to simple hardcode

--- scripts/update-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-cd "$(dirname $0)"
-
-majorVersion="$(grep MajorVersion ../common/metrics/version.go | awk '{print $4}' | sed 's/"//g')"
-versionString="$majorVersion.$(date +%s)"
-
-goLiteral="const VersionString = \"$versionString\""
-
-sed -i "" "s/^const VersionString = \".*\"$/$goLiteral/" ../common/metrics/version.go
