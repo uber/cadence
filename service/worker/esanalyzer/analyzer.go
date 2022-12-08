2f51cbd99ea7707cafbcd6158cc0920e3743ccef
Allen Chen <allenchen2244@uber.com>
2022-12-07 23:34:02 -0800

    try not in wf

--- service/worker/esanalyzer/analyzer.go
+++ service/worker/esanalyzer/analyzer.go
@@ -21,123 +21,124 @@
 package esanalyzer
 
 import (
-	"context"
-	"time"
+    "context"
+    "time"
 
-	"github.com/opentracing/opentracing-go"
-	"github.com/uber-go/tally"
-	"go.uber.org/cadence/.gen/go/cadence/workflowserviceclient"
-	"go.uber.org/cadence/.gen/go/shared"
-	cclient "go.uber.org/cadence/client"
-	"go.uber.org/cadence/worker"
+    "github.com/opentracing/opentracing-go"
+    "github.com/uber-go/tally"
+    "go.uber.org/cadence/.gen/go/cadence/workflowserviceclient"
+    "go.uber.org/cadence/.gen/go/shared"
+    cclient "go.uber.org/cadence/client"
+    "go.uber.org/cadence/worker"
 
-	"github.com/uber/cadence/client"
-	"github.com/uber/cadence/client/frontend"
-	"github.com/uber/cadence/common"
-	"github.com/uber/cadence/common/cache"
-	"github.com/uber/cadence/common/config"
-	"github.com/uber/cadence/common/dynamicconfig"
-	es "github.com/uber/cadence/common/elasticsearch"
-	"github.com/uber/cadence/common/log"
-	"github.com/uber/cadence/common/log/tag"
-	"github.com/uber/cadence/common/metrics"
-	"github.com/uber/cadence/common/resource"
-	"github.com/uber/cadence/service/worker/workercommon"
+    "github.com/uber/cadence/client"
+    "github.com/uber/cadence/client/frontend"
+    "github.com/uber/cadence/common"
+    "github.com/uber/cadence/common/cache"
+    "github.com/uber/cadence/common/config"
+    "github.com/uber/cadence/common/dynamicconfig"
+    es "github.com/uber/cadence/common/elasticsearch"
+    "github.com/uber/cadence/common/log"
+    "github.com/uber/cadence/common/log/tag"
+    "github.com/uber/cadence/common/metrics"
+    "github.com/uber/cadence/common/resource"
+    "github.com/uber/cadence/service/worker/workercommon"
 )
 
 type (
-	// Analyzer is the background sub-system to query ElasticSearch and execute mitigations
-	Analyzer struct {
-		svcClient           workflowserviceclient.Interface
-		frontendClient      frontend.Client
-		clientBean          client.Bean
-		esClient            es.GenericClient
-		logger              log.Logger
-		scopedMetricClient  metrics.Scope
-		tallyScope          tally.Scope
-		visibilityIndexName string
-		resource            resource.Resource
-		domainCache         cache.DomainCache
-		config              *Config
-	}
+    // Analyzer is the background sub-system to query ElasticSearch and execute mitigations
+    Analyzer struct {
+        svcClient           workflowserviceclient.Interface
+        frontendClient      frontend.Client
+        clientBean          client.Bean
+        esClient            es.GenericClient
+        logger              log.Logger
+        scopedMetricClient  metrics.Scope
+        tallyScope          tally.Scope
+        visibilityIndexName string
+        resource            resource.Resource
+        domainCache         cache.DomainCache
+        config              *Config
+    }
 
-	// Config contains all configs for ElasticSearch Analyzer
-	Config struct {
-		ESAnalyzerPause                          dynamicconfig.BoolPropertyFn
-		ESAnalyzerTimeWindow                     dynamicconfig.DurationPropertyFn
-		ESAnalyzerMaxNumDomains                  dynamicconfig.IntPropertyFn
-		ESAnalyzerMaxNumWorkflowTypes            dynamicconfig.IntPropertyFn
-		ESAnalyzerLimitToTypes                   dynamicconfig.StringPropertyFn
-		ESAnalyzerEnableAvgDurationBasedChecks   dynamicconfig.BoolPropertyFn
-		ESAnalyzerLimitToDomains                 dynamicconfig.StringPropertyFn
-		ESAnalyzerNumWorkflowsToRefresh          dynamicconfig.IntPropertyFnWithWorkflowTypeFilter
-		ESAnalyzerBufferWaitTime                 dynamicconfig.DurationPropertyFnWithWorkflowTypeFilter
-		ESAnalyzerMinNumWorkflowsForAvg          dynamicconfig.IntPropertyFnWithWorkflowTypeFilter
-		ESAnalyzerWorkflowDurationWarnThresholds dynamicconfig.StringPropertyFn
-		ESAnalyzerWorkflowVersionDomains         dynamicconfig.StringPropertyFn
-	}
+    // Config contains all configs for ElasticSearch Analyzer
+    Config struct {
+        ESAnalyzerPause                          dynamicconfig.BoolPropertyFn
+        ESAnalyzerTimeWindow                     dynamicconfig.DurationPropertyFn
+        ESAnalyzerMaxNumDomains                  dynamicconfig.IntPropertyFn
+        ESAnalyzerMaxNumWorkflowTypes            dynamicconfig.IntPropertyFn
+        ESAnalyzerLimitToTypes                   dynamicconfig.StringPropertyFn
+        ESAnalyzerEnableAvgDurationBasedChecks   dynamicconfig.BoolPropertyFn
+        ESAnalyzerLimitToDomains                 dynamicconfig.StringPropertyFn
+        ESAnalyzerNumWorkflowsToRefresh          dynamicconfig.IntPropertyFnWithWorkflowTypeFilter
+        ESAnalyzerBufferWaitTime                 dynamicconfig.DurationPropertyFnWithWorkflowTypeFilter
+        ESAnalyzerMinNumWorkflowsForAvg          dynamicconfig.IntPropertyFnWithWorkflowTypeFilter
+        ESAnalyzerWorkflowDurationWarnThresholds dynamicconfig.StringPropertyFn
+        ESAnalyzerWorkflowVersionDomains         dynamicconfig.StringPropertyFn
+    }
 )
 
 const startUpDelay = time.Second * 10
 
 // New returns a new instance as daemon
 func New(
-	svcClient workflowserviceclient.Interface,
-	frontendClient frontend.Client,
-	clientBean client.Bean,
-	esClient es.GenericClient,
-	esConfig *config.ElasticSearchConfig,
-	logger log.Logger,
-	metricsClient metrics.Client,
-	tallyScope tally.Scope,
-	resource resource.Resource,
-	domainCache cache.DomainCache,
-	config *Config,
+    svcClient workflowserviceclient.Interface,
+    frontendClient frontend.Client,
+    clientBean client.Bean,
+    esClient es.GenericClient,
+    esConfig *config.ElasticSearchConfig,
+    logger log.Logger,
+    metricsClient metrics.Client,
+    tallyScope tally.Scope,
+    resource resource.Resource,
+    domainCache cache.DomainCache,
+    config *Config,
 ) *Analyzer {
-	return &Analyzer{
-		svcClient:           svcClient,
-		frontendClient:      frontendClient,
-		clientBean:          clientBean,
-		esClient:            esClient,
-		logger:              logger,
-		scopedMetricClient:  getScopedMetricsClient(metricsClient),
-		tallyScope:          tallyScope,
-		visibilityIndexName: esConfig.Indices[common.VisibilityAppName],
-		resource:            resource,
-		domainCache:         domainCache,
-		config:              config,
-	}
+    return &Analyzer{
+        svcClient:           svcClient,
+        frontendClient:      frontendClient,
+        clientBean:          clientBean,
+        esClient:            esClient,
+        logger:              logger,
+        scopedMetricClient:  getScopedMetricsClient(metricsClient),
+        tallyScope:          tallyScope,
+        visibilityIndexName: esConfig.Indices[common.VisibilityAppName],
+        resource:            resource,
+        domainCache:         domainCache,
+        config:              config,
+    }
 }
 
 func getScopedMetricsClient(metricsClient metrics.Client) metrics.Scope {
-	return metricsClient.Scope(metrics.ESAnalyzerScope)
+    return metricsClient.Scope(metrics.ESAnalyzerScope)
 }
 
 // Start starts the scanner
 func (a *Analyzer) Start() error {
-	ctx := context.Background()
-	a.StartWorkflow(ctx)
+    ctx := context.Background()
+    a.StartWorkflow(ctx)
 
-	workerOpts := worker.Options{
-		MetricsScope:              a.tallyScope,
-		BackgroundActivityContext: ctx,
-		Tracer:                    opentracing.GlobalTracer(),
-	}
-	esWorker := worker.New(a.svcClient, common.SystemLocalDomainName, taskListName, workerOpts)
-	err := esWorker.Start()
-	return err
+    workerOpts := worker.Options{
+        MetricsScope:              a.tallyScope,
+        BackgroundActivityContext: ctx,
+        Tracer:                    opentracing.GlobalTracer(),
+    }
+    esWorker := worker.New(a.svcClient, common.SystemLocalDomainName, taskListName, workerOpts)
+    err := esWorker.Start()
+    return err
 }
 
 func (a *Analyzer) StartWorkflow(ctx context.Context) {
-	initWorkflow(a)
-	go workercommon.StartWorkflowWithRetry(esanalyzerWFTypeName, startUpDelay, a.resource, func(client cclient.Client) error {
-		_, err := client.StartWorkflow(ctx, wfOptions, esanalyzerWFTypeName)
-		switch err.(type) {
-		case *shared.WorkflowExecutionAlreadyStartedError:
-			return nil
-		default:
-			a.logger.Error("Failed to start ElasticSearch Analyzer", tag.Error(err))
-			return err
-		}
-	})
+    initWorkflow(a)
+    a.scopedMetricClient.UpdateGauge(metrics.WorkflowVersionCount, float64(12345))
+    go workercommon.StartWorkflowWithRetry(esanalyzerWFTypeName, startUpDelay, a.resource, func(client cclient.Client) error {
+        _, err := client.StartWorkflow(ctx, wfOptions, esanalyzerWFTypeName)
+        switch err.(type) {
+        case *shared.WorkflowExecutionAlreadyStartedError:
+            return nil
+        default:
+            a.logger.Error("Failed to start ElasticSearch Analyzer", tag.Error(err))
+            return err
+        }
+    })
 }
