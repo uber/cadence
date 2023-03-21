d72cff01e15bc4ee769fc32ae686fd47ba6f950e
Allen Chen <allenchen2244@uber.com>
2023-03-21 14:38:37 -0700

    more logs

--- service/history/execution/context_util.go
+++ service/history/execution/context_util.go
@@ -35,7 +35,10 @@ import (
 
 func emitLargeWorkflowShardIDStats(logger log.Logger, metricsClient metrics.Client, config *config.Config, shardID int,
 	domainName string, workflowID string, newHistorySize int64, newHistoryCount int64, oldHistorySize int64, oldHistoryCount int64, blobSize int64) {
-	logger.Info("WE CAN SEE THIS")
+	if domainName == "cadence-killers-staging" {
+		logger.Info("INFORMATION DUMP", tag.Dynamic("newHistorySize", newHistorySize), tag.Dynamic("newHistoryCount", newHistoryCount),
+			tag.Dynamic("oldHistorySize", oldHistorySize), tag.Dynamic("oldHistoryCount", oldHistoryCount), tag.Dynamic("blobSize", blobSize))
+	}
 	if config.EnableShardIDMetrics() {
 		shardIDStr := strconv.Itoa(shardID)
 		// check if blob size is larger than threshold in Dynamic config if so alert on it every time
