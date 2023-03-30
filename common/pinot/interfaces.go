94cb58846919a754c7cc5273d3aafa1528890b0a
Bowen Xiao <xbowen@uber.com>
2023-03-30 16:08:14 -0700

    fix a failed unit test & add mock genericClient component

--- common/pinot/interfaces.go
+++ common/pinot/interfaces.go
@@ -19,6 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination GenericClient_mock.go -self_package github.com/uber/cadence/common/pinot
 
 package pinot
 
