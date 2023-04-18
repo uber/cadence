c843c7ae59fbf1418527d6de5915a7b10e8e41c5
Mantas Sidlauskas <mantass@netapp.com>
2023-04-18 12:08:04 +0300

    fix test case

--- common/elasticsearch/query/bool_query_test.go
+++ common/elasticsearch/query/bool_query_test.go
@@ -39,7 +39,7 @@ func TestBoolQuery(t *testing.T) {
 		t.Fatalf("marshaling to JSON failed: %v", err)
 	}
 	got := string(data)
-	expected := `{"bool":{"must_not":[{"range":{"age":{"from":10,"include_lower":true,"include_upper":true,"to":20}}}]}}`
+	expected := `{"bool":{"must_not":{"range":{"age":{"from":10,"include_lower":true,"include_upper":true,"to":20}}}}}`
 	if got != expected {
 		t.Errorf("expected\n%s\n,got:\n%s", expected, got)
 	}
