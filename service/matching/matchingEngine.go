519e470967d68ed685f215d48242caadc96503eb
Tim Li <ltim@uber.com>
2023-05-01 11:37:19 -0700

    add hostinfo log

--- service/matching/matchingEngine.go
+++ service/matching/matchingEngine.go
@@ -302,12 +302,24 @@ func (e *matchingEngineImpl) AddDecisionTask(
 		return false, err
 	}
 
+	newReq := types.MatchingListTaskListPartitionsRequest{
+		Domain:   domainName,
+		TaskList: request.TaskList,
+	}
+
+	parHost, err := e.listTaskListPartitions(&newReq, persistence.TaskListTypeDecision)
+
+	if err != nil {
+		return false, err
+	}
+
 	// Only emit traffic metrics if the tasklist is not sticky and is not forwarded
 	if int32(request.GetTaskList().GetKind()) == 0 && request.ForwardedFrom == "" {
 		e.metricsClient.Scope(metrics.MatchingAddTaskScope).Tagged(metrics.DomainTag(domainName),
 			metrics.TaskListTag(taskListName), metrics.TaskListTypeTag("decision_task")).IncCounter(metrics.CadenceTasklistRequests)
 		e.emitInfoOrDebugLog(domainID, "Emitting tasklist counter on decision task", tag.Dynamic("tasklistName", taskListName),
 			tag.Dynamic("taskListBaseName", taskList.baseName))
+		e.logger.Info("Hostinfo for decision tasklist", tag.Dynamic("host", parHost))
 	}
 
 	tlMgr, err := e.getTaskListManager(taskList, taskListKind)
