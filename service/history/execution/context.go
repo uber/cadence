612bcf85a08199dc9cc5d3b63acf5e42cfa23f0c
Ketsia <ketsia.mbaku@uber.com>
2023-05-09 16:26:50 -0700

    add more tags to logger

--- service/history/execution/context.go
+++ service/history/execution/context.go
@@ -382,7 +382,7 @@ func (c *contextImpl) CreateWorkflowExecution(
 		domainName,
 		resp.MutableStateUpdateSessionStats,
 	)
-	c.logger.Info("CreateWorkflowExecution called", tag.WorkflowID(c.workflowExecution.WorkflowID), tag.WorkflowDomainID(c.GetDomainID()))
+	c.logger.Info("CreateWorkflowExecution called", tag.WorkflowID(newWorkflow.ExecutionInfo.WorkflowID), tag.WorkflowDomainName(c.GetDomainName()), tag.WorkflowType(newWorkflow.ExecutionInfo.WorkflowTypeName))
 	return nil
 }
 
