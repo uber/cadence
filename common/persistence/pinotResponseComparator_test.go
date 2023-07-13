19e8d4991276e668f93737d4a88de8de66c30172
Bowen Xiao <xbowen@uber.com>
2023-07-13 14:16:19 -0700

    add unit test starter code

---
+++ common/persistence/pinotResponseComparator_test.go
@@ -0,0 +1,19 @@
+package persistence
+
+import "testing"
+
+func TestInterfaceToMap(t *testing.T) {
+
+}
+
+func TestCompareSearchAttributes(t *testing.T) {
+
+}
+
+func TestCompareListWorkflowExecutionsResponse(t *testing.T) {
+
+}
+
+func TestComparePinotESListResponse(t *testing.T) {
+
+}
