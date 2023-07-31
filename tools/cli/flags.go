0b1eefd7fb4d309b83ca1a1ebbaadcf9e02d9290
Manasa <manasa.lingireddy@uber.com>
2023-07-31 14:26:53 -0700

    Fixed naming for flag

--- tools/cli/flags.go
+++ tools/cli/flags.go
@@ -299,7 +299,7 @@ const (
 	FlagIsolationGroupSetDrains           = "set-drains"
 	FlagIsolationGroupJSONConfigurations  = "json"
 	FlagIsolationGroupsRemoveAllDrains    = "remove-all-drains"
-	FlagSearchAttribute                   = "search-attribute"
+	FlagSearchAttribute                   = "search-attr"
 )
 
 var flagsForExecution = []cli.Flag{
