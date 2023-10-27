uber/cadence

commit 4a26eea9c6efbe2f5a3a4f3aa9454f1d725e976f

Add a "stale workflow" scanner/fixer impl, to remove data beyond reasonable retention (#5361)
For a variety of reasons, we have workflows sitting in some of our databases far beyond any reasonable time range.
E.g. a workflow completed months ago in a domain with 7 day retention -> unfortunately still retained.

We've been gradually finding and fixing problems that cause this, but the old data needs to be cleaned up
at some point.  They can sometimes cause problems, particularly if they appear to be "running".

So this is a *conservative* scanner + fixer for workflows that meet these criteria.
It does not handle all states, nor does it support domains configured with >200 day retention (hardcoded but
trivially changeable, as a sanity check), but it's catching a large amount internally and seems safe to share
and run in more locations.

---

Since building and verifying this required figuring out the scanner/fixer code in detail,
and that proved to be a significant effort, this also includes three major additions on top
of the stale scanner:
- there have been a variety of renames and public -> private changes to make naming consistent
  - this helped discover a significant issue with the current execution scanner, so I'm keeping it
- concrete execution fixer invariants can now be individually enabled and disabled via dynamic config
- a new readme.md file that attempts to summarize the learnings, and how to config and verify scanner/fixer.

service/worker/scanner/timers/timers.go
@@ -84,18 +84,20 @@ func FixerWorkflow(
 
 // ScannerHooks provides hooks for timers scanner.
 func ScannerHooks() *shardscanner.ScannerHooks {
-	h, err := shardscanner.NewScannerHooks(Manager, Iterator)
+	h, err := shardscanner.NewScannerHooks(Manager, Iterator, Config)
 	if err != nil {
 		return nil
 	}
-	h.SetConfig(Config)
 
 	return h
 }
 
 // FixerHooks provides hooks needed for timers fixer.
 func FixerHooks() *shardscanner.FixerHooks {
-	h, err := shardscanner.NewFixerHooks(FixerManager, FixerIterator)
+	noConfig := func(fixer shardscanner.FixerContext) shardscanner.CustomScannerConfig {
+		return nil
+	}
+	h, err := shardscanner.NewFixerHooks(FixerManager, FixerIterator, noConfig)
 	if err != nil {
 		return nil
 	}
@@ -155,7 +157,7 @@ func FixerManager(
 }
 
 // Config resolves dynamic config for timers scanner.
-func Config(ctx shardscanner.Context) shardscanner.CustomScannerConfig {
+func Config(ctx shardscanner.ScannerContext) shardscanner.CustomScannerConfig {
 	res := shardscanner.CustomScannerConfig{}
 	res[periodStartKey] = strconv.Itoa(ctx.Config.DynamicCollection.GetIntProperty(dynamicconfig.TimersScannerPeriodStart)())
 	res[periodEndKey] = strconv.Itoa(ctx.Config.DynamicCollection.GetIntProperty(dynamicconfig.TimersScannerPeriodEnd)())

common/dynamicconfig/constants.go
@@ -1709,6 +1709,30 @@ const (
 	// Default value: true
 	// Allowed filters: N/A
 	ConcreteExecutionsScannerInvariantCollectionHistory
+	// ConcreteExecutionsFixerInvariantCollectionStale indicates if the stale workflow invariant should be run
+	// KeyName: worker.executionsFixerInvariantCollectionStale
+	// Value type: Bool
+	// Default value: false
+	// Allowed filters: N/A
+	ConcreteExecutionsFixerInvariantCollectionStale
+	// ConcreteExecutionsFixerInvariantCollectionMutableState is indicates if mutable state invariant checks should be run
+	// KeyName: worker.executionsFixerInvariantCollectionMutableState
+	// Value type: Bool
+	// Default value: true
+	// Allowed filters: N/A
+	ConcreteExecutionsFixerInvariantCollectionMutableState
+	// ConcreteExecutionsFixerInvariantCollectionHistory is indicates if history invariant checks should be run
+	// KeyName: worker.executionsFixerInvariantCollectionHistory
+	// Value type: Bool
+	// Default value: true
+	// Allowed filters: N/A
+	ConcreteExecutionsFixerInvariantCollectionHistory
+	// ConcreteExecutionsScannerInvariantCollectionStale indicates if the stale workflow invariant should be run
+	// KeyName: worker.executionsScannerInvariantCollectionStale
+	// Value type: Bool
+	// Default value: false
+	// Allowed filters: N/A
+	ConcreteExecutionsScannerInvariantCollectionStale
 	// CurrentExecutionsScannerEnabled is indicates if current executions scanner should be started as part of worker.Scanner
 	// KeyName: worker.currentExecutionsScannerEnabled
 	// Value type: Bool
@@ -3883,6 +3907,26 @@ var BoolKeys = map[BoolKey]DynamicBool{
 		KeyName:      "worker.executionsScannerInvariantCollectionHistory",
 		Description:  "ConcreteExecutionsScannerInvariantCollectionHistory is indicates if history invariant checks should be run",
 		DefaultValue: true,
+	},
+	ConcreteExecutionsScannerInvariantCollectionStale: DynamicBool{
+		KeyName:      "worker.executionsScannerInvariantCollectionStale",
+		Description:  "ConcreteExecutionsScannerInvariantCollectionStale indicates if the stale-workflow invariant should be run",
+		DefaultValue: false, // may be enabled after further verification, but for now it's a bit too risky to enable by default
+	},
+	ConcreteExecutionsFixerInvariantCollectionMutableState: DynamicBool{
+		KeyName:      "worker.executionsFixerInvariantCollectionMutableState",
+		Description:  "ConcreteExecutionsFixerInvariantCollectionMutableState is indicates if mutable state invariant checks should be run",
+		DefaultValue: true,
+	},
+	ConcreteExecutionsFixerInvariantCollectionHistory: DynamicBool{
+		KeyName:      "worker.executionsFixerInvariantCollectionHistory",
+		Description:  "ConcreteExecutionsFixerInvariantCollectionHistory is indicates if history invariant checks should be run",
+		DefaultValue: true,
+	},
+	ConcreteExecutionsFixerInvariantCollectionStale: DynamicBool{
+		KeyName:      "worker.executionsFixerInvariantCollectionStale",
+		Description:  "ConcreteExecutionsFixerInvariantCollectionStale indicates if the stale-workflow invariant should be run",
+		DefaultValue: false, // may be enabled after further verification, but for now it's a bit too risky to enable by default
 	},
 	CurrentExecutionsScannerEnabled: DynamicBool{
 		KeyName:      "worker.currentExecutionsScannerEnabled",

common/persistence/retryer.go
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination retryer_mock.go -package persistence github.com/uber/cadence/common/persistence Retryer
+
 package persistence
 
 import (

common/persistence/retryer_mock.go
@@ -1,0 +1,218 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2017-2020 Uber Technologies Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Code generated by MockGen. DO NOT EDIT.
+// Source: retryer.go
+
+// Package persistence is a generated GoMock package.
+package persistence
+
+import (
+	context "context"
+	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
+)
+
+// MockRetryer is a mock of Retryer interface.
+type MockRetryer struct {
+	ctrl     *gomock.Controller
+	recorder *MockRetryerMockRecorder
+}
+
+// MockRetryerMockRecorder is the mock recorder for MockRetryer.
+type MockRetryerMockRecorder struct {
+	mock *MockRetryer
+}
+
+// NewMockRetryer creates a new mock instance.
+func NewMockRetryer(ctrl *gomock.Controller) *MockRetryer {
+	mock := &MockRetryer{ctrl: ctrl}
+	mock.recorder = &MockRetryerMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockRetryer) EXPECT() *MockRetryerMockRecorder {
+	return m.recorder
+}
+
+// CompleteTimerTask mocks base method.
+func (m *MockRetryer) CompleteTimerTask(ctx context.Context, request *CompleteTimerTaskRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CompleteTimerTask", ctx, request)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// CompleteTimerTask indicates an expected call of CompleteTimerTask.
+func (mr *MockRetryerMockRecorder) CompleteTimerTask(ctx, request interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CompleteTimerTask", reflect.TypeOf((*MockRetryer)(nil).CompleteTimerTask), ctx, request)
+}
+
+// DeleteCurrentWorkflowExecution mocks base method.
+func (m *MockRetryer) DeleteCurrentWorkflowExecution(arg0 context.Context, arg1 *DeleteCurrentWorkflowExecutionRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteCurrentWorkflowExecution", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// DeleteCurrentWorkflowExecution indicates an expected call of DeleteCurrentWorkflowExecution.
+func (mr *MockRetryerMockRecorder) DeleteCurrentWorkflowExecution(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteCurrentWorkflowExecution", reflect.TypeOf((*MockRetryer)(nil).DeleteCurrentWorkflowExecution), arg0, arg1)
+}
+
+// DeleteWorkflowExecution mocks base method.
+func (m *MockRetryer) DeleteWorkflowExecution(arg0 context.Context, arg1 *DeleteWorkflowExecutionRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteWorkflowExecution", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// DeleteWorkflowExecution indicates an expected call of DeleteWorkflowExecution.
+func (mr *MockRetryerMockRecorder) DeleteWorkflowExecution(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteWorkflowExecution", reflect.TypeOf((*MockRetryer)(nil).DeleteWorkflowExecution), arg0, arg1)
+}
+
+// GetCurrentExecution mocks base method.
+func (m *MockRetryer) GetCurrentExecution(arg0 context.Context, arg1 *GetCurrentExecutionRequest) (*GetCurrentExecutionResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetCurrentExecution", arg0, arg1)
+	ret0, _ := ret[0].(*GetCurrentExecutionResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetCurrentExecution indicates an expected call of GetCurrentExecution.
+func (mr *MockRetryerMockRecorder) GetCurrentExecution(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetCurrentExecution", reflect.TypeOf((*MockRetryer)(nil).GetCurrentExecution), arg0, arg1)
+}
+
+// GetShardID mocks base method.
+func (m *MockRetryer) GetShardID() int {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetShardID")
+	ret0, _ := ret[0].(int)
+	return ret0
+}
+
+// GetShardID indicates an expected call of GetShardID.
+func (mr *MockRetryerMockRecorder) GetShardID() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetShardID", reflect.TypeOf((*MockRetryer)(nil).GetShardID))
+}
+
+// GetTimerIndexTasks mocks base method.
+func (m *MockRetryer) GetTimerIndexTasks(arg0 context.Context, arg1 *GetTimerIndexTasksRequest) (*GetTimerIndexTasksResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetTimerIndexTasks", arg0, arg1)
+	ret0, _ := ret[0].(*GetTimerIndexTasksResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetTimerIndexTasks indicates an expected call of GetTimerIndexTasks.
+func (mr *MockRetryerMockRecorder) GetTimerIndexTasks(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTimerIndexTasks", reflect.TypeOf((*MockRetryer)(nil).GetTimerIndexTasks), arg0, arg1)
+}
+
+// GetWorkflowExecution mocks base method.
+func (m *MockRetryer) GetWorkflowExecution(arg0 context.Context, arg1 *GetWorkflowExecutionRequest) (*GetWorkflowExecutionResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetWorkflowExecution", arg0, arg1)
+	ret0, _ := ret[0].(*GetWorkflowExecutionResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetWorkflowExecution indicates an expected call of GetWorkflowExecution.
+func (mr *MockRetryerMockRecorder) GetWorkflowExecution(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetWorkflowExecution", reflect.TypeOf((*MockRetryer)(nil).GetWorkflowExecution), arg0, arg1)
+}
+
+// IsWorkflowExecutionExists mocks base method.
+func (m *MockRetryer) IsWorkflowExecutionExists(arg0 context.Context, arg1 *IsWorkflowExecutionExistsRequest) (*IsWorkflowExecutionExistsResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "IsWorkflowExecutionExists", arg0, arg1)
+	ret0, _ := ret[0].(*IsWorkflowExecutionExistsResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// IsWorkflowExecutionExists indicates an expected call of IsWorkflowExecutionExists.
+func (mr *MockRetryerMockRecorder) IsWorkflowExecutionExists(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsWorkflowExecutionExists", reflect.TypeOf((*MockRetryer)(nil).IsWorkflowExecutionExists), arg0, arg1)
+}
+
+// ListConcreteExecutions mocks base method.
+func (m *MockRetryer) ListConcreteExecutions(arg0 context.Context, arg1 *ListConcreteExecutionsRequest) (*ListConcreteExecutionsResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListConcreteExecutions", arg0, arg1)
+	ret0, _ := ret[0].(*ListConcreteExecutionsResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ListConcreteExecutions indicates an expected call of ListConcreteExecutions.
+func (mr *MockRetryerMockRecorder) ListConcreteExecutions(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListConcreteExecutions", reflect.TypeOf((*MockRetryer)(nil).ListConcreteExecutions), arg0, arg1)
+}
+
+// ListCurrentExecutions mocks base method.
+func (m *MockRetryer) ListCurrentExecutions(arg0 context.Context, arg1 *ListCurrentExecutionsRequest) (*ListCurrentExecutionsResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListCurrentExecutions", arg0, arg1)
+	ret0, _ := ret[0].(*ListCurrentExecutionsResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ListCurrentExecutions indicates an expected call of ListCurrentExecutions.
+func (mr *MockRetryerMockRecorder) ListCurrentExecutions(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListCurrentExecutions", reflect.TypeOf((*MockRetryer)(nil).ListCurrentExecutions), arg0, arg1)
+}
+
+// ReadHistoryBranch mocks base method.
+func (m *MockRetryer) ReadHistoryBranch(arg0 context.Context, arg1 *ReadHistoryBranchRequest) (*ReadHistoryBranchResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ReadHistoryBranch", arg0, arg1)
+	ret0, _ := ret[0].(*ReadHistoryBranchResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// ReadHistoryBranch indicates an expected call of ReadHistoryBranch.
+func (mr *MockRetryerMockRecorder) ReadHistoryBranch(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ReadHistoryBranch", reflect.TypeOf((*MockRetryer)(nil).ReadHistoryBranch), arg0, arg1)
+}

common/reconciliation/invariant/collection_enumer.go
@@ -29,11 +29,11 @@ import (
 	"strings"
 )
 
-const _CollectionName = "CollectionMutableStateCollectionHistoryCollectionDomain"
+const _CollectionName = "CollectionMutableStateCollectionHistoryCollectionDomainCollectionStale"
 
-var _CollectionIndex = [...]uint8{0, 22, 39, 55}
+var _CollectionIndex = [...]uint8{0, 22, 39, 55, 70}
 
-const _CollectionLowerName = "collectionmutablestatecollectionhistorycollectiondomain"
+const _CollectionLowerName = "collectionmutablestatecollectionhistorycollectiondomaincollectionstale"
 
 func (i Collection) String() string {
 	if i < 0 || i >= Collection(len(_CollectionIndex)-1) {
@@ -49,9 +49,10 @@ func _CollectionNoOp() {
 	_ = x[CollectionMutableState-(0)]
 	_ = x[CollectionHistory-(1)]
 	_ = x[CollectionDomain-(2)]
+	_ = x[CollectionStale-(3)]
 }
 
-var _CollectionValues = []Collection{CollectionMutableState, CollectionHistory, CollectionDomain}
+var _CollectionValues = []Collection{CollectionMutableState, CollectionHistory, CollectionDomain, CollectionStale}
 
 var _CollectionNameToValueMap = map[string]Collection{
 	_CollectionName[0:22]:       CollectionMutableState,
@@ -60,12 +61,15 @@ var _CollectionNameToValueMap = map[string]Collection{
 	_CollectionLowerName[22:39]: CollectionHistory,
 	_CollectionName[39:55]:      CollectionDomain,
 	_CollectionLowerName[39:55]: CollectionDomain,
+	_CollectionName[55:70]:      CollectionStale,
+	_CollectionLowerName[55:70]: CollectionStale,
 }
 
 var _CollectionNames = []string{
 	_CollectionName[0:22],
 	_CollectionName[22:39],
 	_CollectionName[39:55],
+	_CollectionName[55:70],
 }
 
 // CollectionString retrieves an enum value from the enum constants string name.

commit 3fb451e27484e3c1dccd66eb913a7d956d586086

Bugfix: add non-empty custom scanner config to fix timer fixer
This was accidentally broken in #5361 - this timer-fixer uses the shared
fixer workflow, which now expects non-empty config about what invariants
are enabled.

Since that'll help make this clearer / less "enable by default"-prone for
future timer-invariants, I've just added a static non-empty config.

common/reconciliation/invariant/timerInvalid.go
@@ -31,6 +31,8 @@ import (
 	"github.com/uber/cadence/common/types"
 )
 
+const TimerInvalidName = "TimerInvalid"
+
 type TimerInvalid struct {
 	pr    persistence.Retryer
 	cache cache.DomainCache
@@ -161,5 +163,5 @@ func (h *TimerInvalid) Fix(
 }
 
 func (h *TimerInvalid) Name() Name {
-	return "TimerInvalid"
+	return TimerInvalidName
 }

service/worker/scanner/timers/timers.go
@@ -94,14 +94,22 @@ func ScannerHooks() *shardscanner.ScannerHooks {
 
 // FixerHooks provides hooks needed for timers fixer.
 func FixerHooks() *shardscanner.FixerHooks {
-	noConfig := func(fixer shardscanner.FixerContext) shardscanner.CustomScannerConfig {
-		return nil
-	}
-	h, err := shardscanner.NewFixerHooks(FixerManager, FixerIterator, noConfig)
+	h, err := shardscanner.NewFixerHooks(FixerManager, FixerIterator, timerCustomConfig)
 	if err != nil {
 		return nil
 	}
 	return h
+}
+
+func timerCustomConfig(_ shardscanner.FixerContext) shardscanner.CustomScannerConfig {
+	// must be non-empty to pass backwards-compat check,
+	// and this allows safely adding more invariants in the future.
+	//
+	// currently this is not read anywhere because "fixer enabled"
+	// means "run this one invariant's fixes".
+	return map[string]string{
+		invariant.TimerInvalidName: "true",
+	}
 }
 
 // Manager provides invariant manager for timers scanner.