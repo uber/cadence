35ba5350b6f75888cf85b6b3b62316979eee316d
Steven L <imgroxx@gmail.com>
2023-12-19 22:20:01 -0600

    uh.  prefix redirection?

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -5,7 +5,7 @@ set -ex
 make go-generate && make fmt && make lint && make copyright
 
 # intentionally capture stderr, so status-errors are also PR-failing
-if [ -n "$(git status --porcelain >&1)" ]; then
+if [ -n "$(>&1 git status --porcelain)" ]; then
   echo "There are changes after make go-generate && make fmt && make lint && make copyright"
   echo "Please rerun the command and commit the changes"
   git status --porcelain
