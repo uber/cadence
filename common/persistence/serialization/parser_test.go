62528e656fdb8a9504a2303bd158a3d120e38ae3
Steven L <imgroxx@gmail.com>
2023-12-20 15:17:33 -0600

    fmt

--- common/persistence/serialization/parser_test.go
+++ common/persistence/serialization/parser_test.go
@@ -30,10 +30,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/uber/cadence/common/types"
 
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/common/persistence"
+	"github.com/uber/cadence/common/types"
 )
 
 func TestParserRoundTrip(t *testing.T) {
