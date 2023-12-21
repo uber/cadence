5050584ba098eaa92663ede23fce1d27887328e7
Steven L <imgroxx@gmail.com>
2023-12-20 21:42:53 -0600

    why does that not fail WHY

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -14,7 +14,9 @@ if [ -z "$BUILDKITE" ]; then
 fi
 
 # intentionally capture stderr, so status-errors are also PR-failing
-if [ -n "$( >&1 git status --porcelain)" ]; then
+git status --porcelain
+echo $?
+if [ -n "$(git status --porcelain  >&1)" ]; then
   echo "There are changes after make go-generate && make fmt && make lint && make copyright"
   echo "Please rerun the command and commit the changes"
   git status --porcelain
