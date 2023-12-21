24bf2c64e7b74f73bff2c252307d6e4f134ada87
Steven L <imgroxx@gmail.com>
2023-12-20 21:14:54 -0600

    More fix for golint.sh

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -9,7 +9,7 @@ make go-generate && make fmt && make lint && make copyright
 #
 # that's fine, just override it if it looks like we're in buildkite.
 # elsewhere it's probably best to not touch this, and the path is likely wrong.
-if [[ -n $BUILDKITE_AGENT_ID ]]; then
+if [ -n "$BUILDKITE_AGENT_ID" ]; then
   git config --global --add safe.directory /cadence
 fi
 
