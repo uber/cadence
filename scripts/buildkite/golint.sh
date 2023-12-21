4f81e1b78cd442115fa0308980677d602d37cdcb
Steven L <imgroxx@gmail.com>
2023-12-20 21:29:13 -0600

    well well well, apparently that documented env var does not exist.  GREAT.

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -9,8 +9,7 @@ make go-generate && make fmt && make lint && make copyright
 #
 # that's fine, just override it if it looks like we're in buildkite.
 # elsewhere it's probably best to not touch this, and the path is likely wrong.
-printenv
-if [ -n "$BUILDKITE_AGENT_ID" ]; then
+if [ -n "$BUILDKITE" ]; then
   git config --global --add safe.directory /cadence
 fi
 
