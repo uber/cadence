dc87e1fb2e8370515b672bfb010ae9e60a7a9032
Steven L <imgroxx@gmail.com>
2023-12-20 21:52:51 -0600

    works works works

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -9,12 +9,13 @@ make go-generate && make fmt && make lint && make copyright
 #
 # that's fine, just override it if it looks like we're in buildkite.
 # elsewhere it's probably best to not touch this, and the path is likely wrong.
-if [ -z "$BUILDKITE" ]; then
+if [ -n "$BUILDKITE" ]; then
   git config --global --add safe.directory /cadence
 fi
 
 # intentionally capture stderr, so status-errors are also PR-failing.
-# in particular this catches "dubious ownership" failures.
+# in particular this catches "dubious ownership" failures, which otherwise
+# do not fail this check and the $() hides the exit code.
 if [ -n "$(git status --porcelain  2>&1)" ]; then
   echo "There are changes after make go-generate && make fmt && make lint && make copyright"
   echo "Please rerun the command and commit the changes"
