a8921b2348b0a5af527be8ad560bded3f8cc1685
Steven L <imgroxx@gmail.com>
2024-01-08 13:45:57 -0600

    Properly catch errors in ldflag-gathering and fail the build (#5539)
    The dubious-ownership issue in CI appears to be happening here too,
    and might be why some builds have "unknown" as their commit SHAs.
    
    Building this change locally properly fails to build binaries when
    it cannot be gathered, and includes the data when it can:
    ```
    VERSION:
       CLI feature version: 1.7.0
       Release version: v1.2.7-prerelease2-dirty
       Build commit: 2023-12-22T20:30:21+00:00-d4d205fe3
    ```
    
    ---
    
    Previously there were basically three issues at play:
    - git's new `safe.directory` thing, now applied everywhere rather than just one target
    - the in-variable-expansion shell error would be swallowed and
    (AFAICT) be completely undetectable elsewhere in Make.
    - [SC2155](https://github.com/koalaman/shellcheck/wiki/SC2155) and similar "you captured output but you also suppressed the error code" issues in scripts.
    
    Since fixing the "always include the version, fail build if it does not work" issue
    involved discovering and fixing all three, they're all in this commit.

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -4,15 +4,6 @@ set -ex
 
 make go-generate && make fmt && make lint && make copyright
 
-# buildkite-git complains about "dubious ownership",
-# i.e. user-who-created is different than user-who-statused.
-#
-# that's fine, just override it if it looks like we're in buildkite.
-# elsewhere it's probably best to not touch this, and the path is likely wrong.
-if [ -n "$BUILDKITE" ]; then
-  git config --global --add safe.directory /cadence
-fi
-
 # intentionally capture stderr, so status-errors are also PR-failing.
 # in particular this catches "dubious ownership" failures, which otherwise
 # do not fail this check and the $() hides the exit code.
--- scripts/get-ldflags.sh
+++ scripts/get-ldflags.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eo pipefail
+
+# add verbose env var for debugging
+test -n "$V" && set -x
 
 MODE=$1
 
@@ -7,20 +12,18 @@ if [ "$MODE" != "LDFLAG" ] && [ "$MODE" != "ECHO" ]; then
     exit 1
 fi
 
-# $(git rev-parse --short HEAD) \"$(git log -1 --format=%cI-%h)\"
-export GIT_REVISION=$(git log -1 --format=%cI-%h) # use commit date time and hash: e.g. 2021-07-27 19:36:53 -0700-40c5f1896, doc: https://git-scm.com/docs/pretty-formats
-export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-export BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:45
-export BUILD_TS_UNIX=$(date '+%s') # second since epoch
-export BASE_PACKAGE=github.com/uber/cadence/common/metrics
+GIT_REVISION=$(git log -1 --format=%cI-%h) # use commit date time and hash: e.g. 2021-07-27 19:36:53 -0700-40c5f1896, doc: https://git-scm.com/docs/pretty-formats
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:45
+BUILD_TS_UNIX=$(date '+%s') # second since epoch
+BASE_PACKAGE=github.com/uber/cadence/common/metrics
 if [ -z ${CADENCE_RELEASE_VERSION} ]; then
   # If not set CADENCE_RELEASE_VERSION, then use the most recent tag.
-  export RELEASE_VERSION=$(git describe --tags --abbrev=0 --dirty 2>/dev/null || echo unknown)
+  RELEASE_VERSION=$(git describe --tags --abbrev=0 --dirty 2>/dev/null || echo unknown)
 else
   # If passing a CADENCE_RELEASE_VERSION explicitly, then use it
-  export RELEASE_VERSION=${CADENCE_RELEASE_VERSION}
+  RELEASE_VERSION=${CADENCE_RELEASE_VERSION}
 fi
-
 
 if [ "$MODE" = "LDFLAG" ]; then
   LD_FLAGS="-X ${BASE_PACKAGE}.Revision=${GIT_REVISION} \
