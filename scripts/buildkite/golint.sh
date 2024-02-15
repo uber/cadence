176abe024ed4db43d33b2903ab89ae8606673416
Steven L <imgroxx@gmail.com>
2024-02-15 17:49:05 -0600

    Update the build-changed message failure

--- scripts/buildkite/golint.sh
+++ scripts/buildkite/golint.sh
@@ -4,16 +4,17 @@ set -ex
 
 make tidy
 make go-generate
+make copyright
 make fmt
 make lint
-make copyright
 
 # intentionally capture stderr, so status-errors are also PR-failing.
 # in particular this catches "dubious ownership" failures, which otherwise
 # do not fail this check and the $() hides the exit code.
 if [ -n "$(git status --porcelain  2>&1)" ]; then
-  echo "There are changes after make go-generate && make fmt && make lint && make copyright"
-  echo "Please rerun the command and commit the changes"
+  echo "There file changes after applying your diff and performing a build."
+  echo "Please run this command and commit the changes:"
+  echo "\tmake tidy && make copyright && make go-generate && make fmt && make lint"
   git status --porcelain
   git --no-pager diff
   exit 1
