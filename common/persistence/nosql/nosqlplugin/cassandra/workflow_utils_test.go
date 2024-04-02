c56c124f873639909b0b13dfb2f32f6b4b00e541
taylanisikdemir <taylan@uber.com>
2024-04-01 21:53:37 -0700

    Write tests for cassandra/tasks.go (#5828)

--- common/persistence/nosql/nosqlplugin/cassandra/workflow_utils_test.go
+++ common/persistence/nosql/nosqlplugin/cassandra/workflow_utils_test.go
@@ -52,9 +52,11 @@ type fakeSession struct {
 
 	// outputs
 	batches []*fakeBatch
+	queries []string
 }
 
-func (s *fakeSession) Query(string, ...interface{}) gocql.Query {
+func (s *fakeSession) Query(queryTmpl string, args ...interface{}) gocql.Query {
+	s.queries = append(s.queries, sanitizedRenderedQuery(queryTmpl, args...))
 	return s.query
 }
 
@@ -83,23 +85,7 @@ type fakeBatch struct {
 
 // Query is fake implementation of gocql.Batch.Query
 func (b *fakeBatch) Query(queryTmpl string, args ...interface{}) {
-	argsSanitized := make([]interface{}, len(args))
-	for i, arg := range args {
-		// use values instead of pointer so that we can compare them in tests
-		if reflect.ValueOf(arg).Kind() == reflect.Ptr && !reflect.ValueOf(arg).IsZero() {
-			argsSanitized[i] = reflect.ValueOf(arg).Elem().Interface()
-		} else {
-			argsSanitized[i] = arg
-		}
-
-		if t, ok := argsSanitized[i].(time.Time); ok {
-			// use fixed time format to avoid flakiness
-			argsSanitized[i] = t.UTC().Format(time.RFC3339)
-		}
-
-	}
-	queryTmpl = strings.ReplaceAll(queryTmpl, "?", "%v")
-	b.queries = append(b.queries, fmt.Sprintf(queryTmpl, argsSanitized...))
+	b.queries = append(b.queries, sanitizedRenderedQuery(queryTmpl, args...))
 }
 
 // WithContext is fake implementation of gocql.Batch.WithContext
@@ -121,11 +107,32 @@ func (b *fakeBatch) Consistency(gocql.Consistency) gocql.Batch {
 func (s *fakeSession) Close() {
 }
 
+func sanitizedRenderedQuery(queryTmpl string, args ...interface{}) string {
+	argsSanitized := make([]interface{}, len(args))
+	for i, arg := range args {
+		// use values instead of pointer so that we can compare them in tests
+		if reflect.ValueOf(arg).Kind() == reflect.Ptr && !reflect.ValueOf(arg).IsZero() {
+			argsSanitized[i] = reflect.ValueOf(arg).Elem().Interface()
+		} else {
+			argsSanitized[i] = arg
+		}
+
+		if t, ok := argsSanitized[i].(time.Time); ok {
+			// use fixed time format to avoid flakiness
+			argsSanitized[i] = t.UTC().Format(time.RFC3339)
+		}
+
+	}
+	queryTmpl = strings.ReplaceAll(queryTmpl, "?", "%v")
+	return fmt.Sprintf(queryTmpl, argsSanitized...)
+}
+
 // fakeIter is fake implementation of gocql.Iter
 type fakeIter struct {
 	// input parametrs
 	mapScanInputs []map[string]interface{}
 	pageState     []byte
+	closeErr      error
 
 	// output parameters
 	mapScanCalls int
@@ -158,7 +165,7 @@ func (i *fakeIter) PageState() []byte {
 // Close is fake implementation of gocql.Iter.Close
 func (i *fakeIter) Close() error {
 	i.closed = true
-	return nil
+	return i.closeErr
 }
 
 type fakeUUID struct {
