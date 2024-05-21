d606881ff3c99a794ea6b134e50d7aa264731de3
sankari gopalakrishnan <sankari@uber.com>
2024-05-21 16:21:32 +0200

    Update mutable_state_builder_methods_activity_test.go

--- service/history/execution/mutable_state_builder_methods_activity_test.go
+++ service/history/execution/mutable_state_builder_methods_activity_test.go
@@ -1,10 +1,35 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2017-2020 Uber Technologies Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package execution
 
 import (
 	"context"
-	"github.com/aws/smithy-go/ptr"
+	"testing"
+	"time"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+
 	"github.com/uber/cadence/common"
 	"github.com/uber/cadence/common/log"
 	"github.com/uber/cadence/common/persistence"
@@ -13,8 +38,6 @@ import (
 	"github.com/uber/cadence/service/history/constants"
 	"github.com/uber/cadence/service/history/events"
 	"github.com/uber/cadence/service/history/shard"
-	"testing"
-	"time"
 )
 
 func testMutableStateBuilder(t *testing.T) *mutableStateBuilder {
@@ -62,11 +85,12 @@ func Test__UpdateActivityProgress(t *testing.T) {
 func Test__ReplicateActivityInfo(t *testing.T) {
 	mb := testMutableStateBuilder(t)
 	now := time.Now()
+	nowUnix := now.UnixNano()
 	request := &types.SyncActivityRequest{
 		ScheduledID:       1,
 		Version:           1,
-		ScheduledTime:     ptr.Int64(now.UnixNano()),
-		LastHeartbeatTime: ptr.Int64(now.UnixNano()),
+		ScheduledTime:     &nowUnix,
+		LastHeartbeatTime: &nowUnix,
 	}
 	ai := &persistence.ActivityInfo{}
 
