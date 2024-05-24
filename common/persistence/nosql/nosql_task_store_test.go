fc6fae5384fac24b50cb03f57195dc3b230b29a9
Jakob Haahr Taankvist <jht@uber.com>
2024-05-24 11:59:38 +0200

    Added test for LeaseTaskList (#6042)

--- common/persistence/nosql/nosql_task_store_test.go
+++ common/persistence/nosql/nosql_task_store_test.go
@@ -96,12 +96,8 @@ func TestGetTaskListSize(t *testing.T) {
 	db.EXPECT().GetTasksCount(
 		gomock.Any(),
 		&nosqlplugin.TasksFilter{
-			TaskListFilter: nosqlplugin.TaskListFilter{
-				DomainID:     TestDomainID,
-				TaskListName: TestTaskListName,
-				TaskListType: int(types.TaskListTypeDecision),
-			},
-			MinTaskID: 456,
+			TaskListFilter: *getDecisionTaskListFilter(),
+			MinTaskID:      456,
 		},
 	).Return(int64(123), nil)
 
@@ -118,6 +114,132 @@ func TestGetTaskListSize(t *testing.T) {
 		&persistence.GetTaskListSizeResponse{Size: 123},
 		size,
 	)
+}
+
+func TestLeaseTaskList_emptyTaskList(t *testing.T) {
+	store, _ := setupNoSQLStoreMocks(t)
+
+	req := getValidLeaseTaskListRequest()
+	req.TaskList = ""
+	_, err := store.LeaseTaskList(context.Background(), req)
+
+	assert.ErrorAs(t, err, new(*types.InternalServiceError))
+	assert.ErrorContains(t, err, "LeaseTaskList requires non empty task list")
+}
+
+func TestLeaseTaskList_selectErr(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	req := getValidLeaseTaskListRequest()
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(nil, assert.AnError)
+	// The error is _not_ a NotFoundError
+	db.EXPECT().IsNotFoundError(assert.AnError).Return(false).Times(2)
+	db.EXPECT().IsTimeoutError(assert.AnError).Return(true).Times(1)
+
+	_, err := store.LeaseTaskList(context.Background(), req)
+
+	assert.Error(t, err)
+	// The function does not wrap the error, it just adds it to the message
+	assert.ErrorContains(t, err, assert.AnError.Error())
+}
+
+func TestLeaseTaskList_selectErrNotFound(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	req := getValidLeaseTaskListRequest()
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(nil, assert.AnError)
+	// The error _is_ a NotFoundError
+	db.EXPECT().IsNotFoundError(assert.AnError).Return(true)
+
+	// We then expect the tasklist to be inserted
+	db.EXPECT().InsertTaskList(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(ctx context.Context, taskList *nosqlplugin.TaskListRow) error {
+			checkTaskListRowExpected(t, taskList)
+			return nil
+		})
+
+	resp, err := store.LeaseTaskList(context.Background(), req)
+
+	assert.NoError(t, err)
+	checkTaskListInfoExpected(t, resp.TaskListInfo)
+}
+
+func TestLeaseTaskList_BadRenew(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	req := getValidLeaseTaskListRequest()
+	req.RangeID = 1 // Greater than 0, so we are trying to renew
+
+	taskListRow := getExpectedTaskListRow()
+	taskListRow.RangeID = 5 // The range ID in the DB is different from the one in the request
+
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(taskListRow, nil)
+
+	_, err := store.LeaseTaskList(context.Background(), req)
+
+	assert.ErrorAs(t, err, new(*persistence.ConditionFailedError))
+	expectedMessage := "leaseTaskList:renew failed: taskList:test-tasklist, taskListType:0, haveRangeID:1, gotRangeID:5"
+	assert.ErrorContains(t, err, expectedMessage)
+}
+
+func TestLeaseTaskList_Renew(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	taskListRow := getExpectedTaskListRow()
+	taskListRow.RangeID = 0 // The range ID in the DB is the same as the one in the request
+
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(taskListRow, nil)
+	db.EXPECT().UpdateTaskList(gomock.Any(), gomock.Any(), int64(0)).
+		DoAndReturn(func(ctx context.Context, taskList *nosqlplugin.TaskListRow, previousRangeID int64) error {
+			checkTaskListRowExpected(t, taskList)
+			return nil
+		})
+
+	resp, err := store.LeaseTaskList(context.Background(), getValidLeaseTaskListRequest())
+
+	assert.NoError(t, err)
+	checkTaskListInfoExpected(t, resp.TaskListInfo)
+}
+
+func TestLeaseTaskList_RenewUpdateFailed_OperationConditionFailure(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	taskListRow := getExpectedTaskListRow()
+	taskListRow.RangeID = 0 // The range ID in the DB is the same as the one in the request
+
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(taskListRow, nil)
+	db.EXPECT().UpdateTaskList(gomock.Any(), gomock.Any(), int64(0)).
+		DoAndReturn(func(ctx context.Context, taskList *nosqlplugin.TaskListRow, previousRangeID int64) error {
+			checkTaskListRowExpected(t, taskList)
+			return &nosqlplugin.TaskOperationConditionFailure{RangeID: 10}
+		})
+
+	_, err := store.LeaseTaskList(context.Background(), getValidLeaseTaskListRequest())
+
+	assert.ErrorAs(t, err, new(*persistence.ConditionFailedError))
+	expectedMessage := "leaseTaskList: taskList:test-tasklist, taskListType:0, haveRangeID:1, gotRangeID:10"
+	assert.ErrorContains(t, err, expectedMessage)
+}
+
+func TestLeaseTaskList_RenewUpdateFailed_OtherError(t *testing.T) {
+	store, db := setupNoSQLStoreMocks(t)
+
+	taskListRow := getExpectedTaskListRow()
+	taskListRow.RangeID = 0 // The range ID in the DB is the same as the one in the request
+
+	db.EXPECT().SelectTaskList(gomock.Any(), getDecisionTaskListFilter()).Return(taskListRow, nil)
+	db.EXPECT().UpdateTaskList(gomock.Any(), gomock.Any(), int64(0)).
+		DoAndReturn(func(ctx context.Context, taskList *nosqlplugin.TaskListRow, previousRangeID int64) error {
+			checkTaskListRowExpected(t, taskList)
+			return assert.AnError
+		})
+	db.EXPECT().IsNotFoundError(assert.AnError).Return(true)
+
+	_, err := store.LeaseTaskList(context.Background(), getValidLeaseTaskListRequest())
+
+	assert.Error(t, err)
+	// The function does not wrap the error, it just adds it to the message
+	assert.ErrorContains(t, err, assert.AnError.Error())
 }
 
 func TestGetTasks(t *testing.T) {
@@ -167,6 +289,49 @@ func TestGetTasks_Empty(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Empty(t, resp.Tasks)
+}
+
+func getValidLeaseTaskListRequest() *persistence.LeaseTaskListRequest {
+	return &persistence.LeaseTaskListRequest{
+		DomainID:     TestDomainID,
+		DomainName:   TestDomainName,
+		TaskList:     TestTaskListName,
+		TaskType:     int(types.TaskListTypeDecision),
+		TaskListKind: int(types.TaskListKindNormal),
+		RangeID:      0,
+	}
+}
+
+func getExpectedTaskListRow() *nosqlplugin.TaskListRow {
+	return &nosqlplugin.TaskListRow{
+		DomainID:        TestDomainID,
+		TaskListName:    TestTaskListName,
+		TaskListType:    int(types.TaskListTypeDecision),
+		RangeID:         initialRangeID,
+		TaskListKind:    int(types.TaskListKindNormal),
+		AckLevel:        initialAckLevel,
+		LastUpdatedTime: time.Now(),
+	}
+}
+
+func checkTaskListRowExpected(t *testing.T, taskList *nosqlplugin.TaskListRow) {
+	expectedRow := getExpectedTaskListRow()
+	// Check the duration
+	assert.WithinDuration(t, expectedRow.LastUpdatedTime, taskList.LastUpdatedTime, time.Second)
+
+	// Set the expected time to the actual time to make the comparison work
+	expectedRow.LastUpdatedTime = taskList.LastUpdatedTime
+	assert.Equal(t, expectedRow, taskList)
+}
+
+func checkTaskListInfoExpected(t *testing.T, taskListInfo *persistence.TaskListInfo) {
+	assert.Equal(t, TestDomainID, taskListInfo.DomainID)
+	assert.Equal(t, TestTaskListName, taskListInfo.Name)
+	assert.Equal(t, int(types.TaskListTypeDecision), taskListInfo.TaskType)
+	assert.Equal(t, initialRangeID, taskListInfo.RangeID)
+	assert.Equal(t, initialAckLevel, taskListInfo.AckLevel)
+	assert.Equal(t, int(types.TaskListKindNormal), taskListInfo.Kind)
+	assert.WithinDuration(t, time.Now(), taskListInfo.LastUpdated, time.Second)
 }
 
 func taskRowEqualTaskInfo(t *testing.T, taskrow1 nosqlplugin.TaskRow, taskInfo1 *persistence.InternalTaskInfo) {
