bc5926e357324695487de74883221e63d0df15a6
Steven L <imgroxx@gmail.com>
2024-05-29 14:42:23 -0500

    go back to flaky

--- common/log/testlogger/testlogger_test.go
+++ common/log/testlogger/testlogger_test.go
@@ -6,18 +6,14 @@ import (
 	"time"
 )
 
-var done = make(chan struct{})
-
 func TestMain(m *testing.M) {
 	m.Run()
-	close(done)
 	time.Sleep(100 * time.Millisecond)
 }
 func TestABefore(t *testing.T) {
 	go func() {
 		t.Logf("sleeping")
 		time.Sleep(10 * time.Millisecond)
-		<-done
 		t.Logf("too late")
 		fmt.Println("goroutine done") // prove it ran
 	}()
