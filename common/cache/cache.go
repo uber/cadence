36ff81e5942af8674df39552217f8129559734f5
taylan isikdemir <taylan@uber.com>
2024-06-05 14:47:21 -0700

    fix

--- common/cache/cache.go
+++ common/cache/cache.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination interface_mocks.go -self_package github.com/uber/cadence/common/cache
+//go:generate mockgen -package $GOPACKAGE -source $GOFILE -destination interface_mock.go -self_package github.com/uber/cadence/common/cache
 
 package cache
 
