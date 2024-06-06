b0ae76a3f0045cd21b0861d0654333cd87a055a5
Xu-Yang Jia <xy.jia@aliyun.com>
2024-06-07 06:17:45 +0800

    Change permission of DescribeCluster to Read (#6081)
    * feat: change permission or DescribeCluster to Read
    
    * feat: add test for admin.DescribeCluster
    
    * Lint test file access_controlled_test.go
    
    Signed-off-by: jiaxuyang <xuyang.jia@gmail.com>
    
    ---------
    
    Signed-off-by: jiaxuyang <xuyang.jia@gmail.com>

--- service/frontend/wrappers/accesscontrolled/access_controlled_test.go
+++ service/frontend/wrappers/accesscontrolled/access_controlled_test.go
@@ -33,6 +33,8 @@ import (
 	"github.com/uber/cadence/common/authorization"
 	"github.com/uber/cadence/common/metrics"
 	"github.com/uber/cadence/common/metrics/mocks"
+	"github.com/uber/cadence/common/types"
+	"github.com/uber/cadence/service/frontend/admin"
 )
 
 func TestIsAuthorized(t *testing.T) {
@@ -88,6 +90,57 @@ func TestIsAuthorized(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 				assert.Equal(t, tc.isAuthorized, got)
+			}
+		})
+	}
+}
+
+func TestDescribeCluster(t *testing.T) {
+	someErr := errors.New("some random err")
+	testCases := []struct {
+		name      string
+		mockSetup func(*authorization.MockAuthorizer, *admin.MockHandler)
+		wantErr   error
+	}{
+		{
+			name: "Success case",
+			mockSetup: func(authorizer *authorization.MockAuthorizer, adminHandler *admin.MockHandler) {
+				authorizer.EXPECT().Authorize(gomock.Any(), gomock.Any()).Return(authorization.Result{Decision: authorization.DecisionAllow}, nil)
+				adminHandler.EXPECT().DescribeCluster(gomock.Any()).Return(&types.DescribeClusterResponse{}, nil)
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Error case - unauthorized",
+			mockSetup: func(authorizer *authorization.MockAuthorizer, adminHandler *admin.MockHandler) {
+				authorizer.EXPECT().Authorize(gomock.Any(), gomock.Any()).Return(authorization.Result{Decision: authorization.DecisionDeny}, nil)
+			},
+			wantErr: errUnauthorized,
+		},
+		{
+			name: "Error case - authorization error",
+			mockSetup: func(authorizer *authorization.MockAuthorizer, adminHandler *admin.MockHandler) {
+				authorizer.EXPECT().Authorize(gomock.Any(), gomock.Any()).Return(authorization.Result{}, someErr)
+			},
+			wantErr: someErr,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			controller := gomock.NewController(t)
+
+			mockAuthorizer := authorization.NewMockAuthorizer(controller)
+			mockAdminHandler := admin.NewMockHandler(controller)
+			tc.mockSetup(mockAuthorizer, mockAdminHandler)
+
+			handler := &adminHandler{authorizer: mockAuthorizer, handler: mockAdminHandler}
+			_, err := handler.DescribeCluster(context.Background())
+			if tc.wantErr != nil {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, tc.wantErr)
+			} else {
+				assert.NoError(t, err)
 			}
 		})
 	}
