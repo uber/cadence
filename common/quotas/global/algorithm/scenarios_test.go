16cc64b48dd805878fc2ed1867e2d8bd4e5b5a5c
Steven L <imgroxx@gmail.com>
2024-07-29 21:15:22 -0500

    make pr

--- common/quotas/global/algorithm/scenarios_test.go
+++ common/quotas/global/algorithm/scenarios_test.go
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2017-2020 Uber Technologies Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package algorithm
 
 import (
@@ -387,10 +409,9 @@ func TestWeightAlgorithms(t *testing.T) {
 							// if allowed less than fallback local value, add unused up to local fallback.
 							// this helps low-weight hosts accept requests when there is unused RPS.
 							return min(local, fair+splitUnused)
-						} else {
-							// weighted higher than the average, trust the rps we get
-							return fair
 						}
+						// weighted higher than the average, trust the rps we get
+						return fair
 					}
 
 					// the only change on top of the original
