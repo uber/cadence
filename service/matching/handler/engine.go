8719d4491bb9bcb4f2477a8ea7d55a8bd23cce53
Jakob Haahr Taankvist <jht@uber.com>
2024-07-30 13:45:02 +0200

    Added a defensive check in the getTaskListManager function
    This makes sure we do not steel tasklists we do not own according to the
    hashring.

--- service/matching/handler/engine.go
+++ service/matching/handler/engine.go
@@ -198,6 +198,24 @@ func (e *matchingEngineImpl) getTaskListManager(taskList *tasklist.Identifier, t
 	}
 	e.taskListsLock.RUnlock()
 
+	// Defensive check to make sure we actually own the task list
+	//   If we try to create a task list manager for a task list that is not owned by us, return an error
+	//   The new task list manager will steel the task list from the current owner, which should only happen if
+	//   the task list is owned by the current host.
+	tasklistOwner, err := e.membershipResolver.Lookup(service.Matching, taskList.GetName())
+	if err != nil {
+		return nil, fmt.Errorf("failed to lookup task list owner: %w", err)
+	}
+
+	self, err := e.membershipResolver.WhoAmI()
+	if err != nil {
+		return nil, fmt.Errorf("failed to lookup self im membership: %w", err)
+	}
+
+	if tasklistOwner.Identity() != self.Identity() {
+		return nil, errors.New("task list is not owned by this host")
+	}
+
 	// If it gets here, write lock and check again in case a task list is created between the two locks
 	e.taskListsLock.Lock()
 	if result, ok := e.taskLists[*taskList]; ok {
