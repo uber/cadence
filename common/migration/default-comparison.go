bd1e36839299eff6213f62684c54240f5a702c2a
David Porter <david.porter@uber.com>
2024-08-22 17:27:45 -0700

    Fmt

--- common/migration/default-comparison.go
+++ common/migration/default-comparison.go
@@ -1,13 +1,37 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2017-2020 Uber Technologies Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package migration
 
 import (
 	"fmt"
+	"reflect"
+
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+
 	"github.com/uber/cadence/common/log"
 	"github.com/uber/cadence/common/log/tag"
 	"github.com/uber/cadence/common/metrics"
-	"reflect"
 )
 
 var _ ComparisonFn[struct{}] = defaultComparisonFn[struct{}]
@@ -33,10 +57,10 @@ func defaultComparisonFn[T any](
 			log.Warn("mismatched result with both flows returning an error", tag.Dynamic("comparison-result-errdiff", errDiff))
 			scope.IncCounter(metrics.ShadowerComparisonDifferenceCount)
 			return false
-		} else {
-			scope.IncCounter(metrics.ShadowerComparisonMatchCount)
-			return true
 		}
+		// else they actually match in both having the same error
+		scope.IncCounter(metrics.ShadowerComparisonMatchCount)
+		return true
 	}
 
 	if backgroundErr != nil && activeErr == nil {
@@ -110,10 +134,9 @@ func defaultComparisonFn[T any](
 			tag.Dynamic("comparison-result-res-diff", diff))
 		scope.IncCounter(metrics.ShadowerComparisonDifferenceCount)
 		return false
-	} else {
-		scope.IncCounter(metrics.ShadowerComparisonMatchCount)
-		return true
 	}
+	scope.IncCounter(metrics.ShadowerComparisonMatchCount)
+	return true
 }
 
 func followPtrsAndReturnValue(in interface{}) (out interface{}, derefCount int, err error) {
