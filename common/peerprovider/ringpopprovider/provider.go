3f9f9f82a6bacd4a3bd38d770cbdab9fc0883ae4
taylan isikdemir <taylan@uber.com>
2024-09-16 17:28:23 -0700

    Support custom address broadcasting for ringpop to work in k8s

--- common/peerprovider/ringpopprovider/provider.go
+++ common/peerprovider/ringpopprovider/provider.go
@@ -23,11 +23,13 @@
 package ringpopprovider
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"strconv"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/uber/ringpop-go"
 	"github.com/uber/ringpop-go/events"
@@ -37,6 +39,7 @@ import (
 	"go.uber.org/yarpc/transport/tchannel"
 
 	"github.com/uber/cadence/common"
+	"github.com/uber/cadence/common/backoff"
 	"github.com/uber/cadence/common/log"
 	"github.com/uber/cadence/common/log/tag"
 	"github.com/uber/cadence/common/membership"
@@ -85,12 +88,40 @@ func New(
 		DiscoverProvider: discoveryProvider,
 	}
 
-	rp, err := ringpop.New(config.Name, ringpop.Channel(channel.(*tcg.Channel)))
+	ch := channel.(*tcg.Channel)
+	opts := []ringpop.Option{ringpop.Channel(ch)}
+	if config.BroadcastAddress != "" {
+		broadcastIP := net.ParseIP(config.BroadcastAddress)
+		if broadcastIP == nil {
+			return nil, fmt.Errorf("failed parsing broadcast address %q, err: %w", config.BroadcastAddress, err)
+		}
+		logger.Info("Initializing ringpop with custom broadcast address", tag.Address(broadcastIP.String()))
+		opts = append(opts, ringpop.AddressResolverFunc(broadcastAddrResolver(ch, broadcastIP)))
+	}
+	rp, err := ringpop.New(config.Name, opts...)
 	if err != nil {
 		return nil, fmt.Errorf("ringpop instance creation: %w", err)
 	}
 
 	return NewRingpopProvider(service, rp, portMap, bootstrapOpts, logger), nil
+}
+
+func broadcastAddrResolver(ch *tcg.Channel, broadcastIP net.IP) func() (string, error) {
+	return func() (string, error) {
+		peerInfo := ch.PeerInfo()
+		if peerInfo.IsEphemeralHostPort() {
+			// not listening yet
+			return "", ringpop.ErrEphemeralAddress
+		}
+
+		_, port, err := net.SplitHostPort(peerInfo.HostPort)
+		if err != nil {
+			return "", fmt.Errorf("failed splitting tchannel's hostport %q, err: %w", peerInfo.HostPort, err)
+		}
+
+		// return broadcast_ip:listen_port
+		return net.JoinHostPort(broadcastIP.String(), port), nil
+	}
 }
 
 // NewRingpopProvider sets up ringpop based peer provider
@@ -122,8 +153,15 @@ func (r *Provider) Start() {
 		return
 	}
 
-	_, err := r.ringpop.Bootstrap(r.bootParams)
-	if err != nil {
+	op := func() error {
+		_, err := r.ringpop.Bootstrap(r.bootParams)
+		if err != nil {
+			r.logger.Warn("unable to bootstrap ringpop, will retry", tag.Error(err))
+		}
+		return err
+	}
+	retry := backoff.NewThrottleRetry(backoff.WithRetryPolicy(bootstrapRetryPolicy()))
+	if err := retry.Do(context.Background(), op); err != nil {
 		r.logger.Fatal("unable to bootstrap ringpop", tag.Error(err))
 	}
 
@@ -148,9 +186,7 @@ func (r *Provider) Start() {
 }
 
 // HandleEvent handles updates from ringpop
-func (r *Provider) HandleEvent(
-	event events.Event,
-) {
+func (r *Provider) HandleEvent(event events.Event) {
 	// We only care about RingChangedEvent
 	e, ok := event.(events.RingChangedEvent)
 	if !ok {
@@ -299,4 +335,11 @@ func labelToPort(label string) (uint16, error) {
 		return 0, err
 	}
 	return uint16(port), nil
+}
+
+func bootstrapRetryPolicy() backoff.RetryPolicy {
+	policy := backoff.NewExponentialRetryPolicy(5 * time.Second)
+	policy.SetMaximumInterval(5 * time.Second)
+	policy.SetExpirationInterval(45 * time.Second)
+	return policy
 }
