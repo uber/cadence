8c6cf31ee2520fb929090bddd7ca2054b6a8b1d8
Tim Li <47233368+timl3136@users.noreply.github.com>
2024-10-11 14:23:21 -0700

    Add unit tests for remaining functions in common/types/replicator (#6356)

--- common/types/replicator_test.go
+++ common/types/replicator_test.go
@@ -800,3 +800,506 @@ func TestReplicationTask_GetDomainTaskAttributes(t *testing.T) {
 	res = nilStruct.GetDomainTaskAttributes()
 	assert.Nil(t, res)
 }
+
+func TestReplicationTask_GetSyncActivityTaskAttributes(t *testing.T) {
+	syncActivityTask := &SyncActivityTaskAttributes{}
+	testStruct := ReplicationTask{
+		SyncActivityTaskAttributes: syncActivityTask,
+	}
+
+	res := testStruct.GetSyncActivityTaskAttributes()
+	assert.Equal(t, syncActivityTask, res)
+
+	var nilStruct *ReplicationTask
+	res = nilStruct.GetSyncActivityTaskAttributes()
+	assert.Nil(t, res)
+}
+
+func TestReplicationTask_GetHistoryTaskV2Attributes(t *testing.T) {
+	historyTaskV2 := &HistoryTaskV2Attributes{}
+	testStruct := ReplicationTask{
+		HistoryTaskV2Attributes: historyTaskV2,
+	}
+
+	res := testStruct.GetHistoryTaskV2Attributes()
+	assert.Equal(t, historyTaskV2, res)
+
+	var nilStruct *ReplicationTask
+	res = nilStruct.GetHistoryTaskV2Attributes()
+	assert.Nil(t, res)
+}
+
+func TestReplicationTask_GetFailoverMarkerAttributes(t *testing.T) {
+	failoverMarker := &FailoverMarkerAttributes{}
+	testStruct := ReplicationTask{
+		FailoverMarkerAttributes: failoverMarker,
+	}
+
+	res := testStruct.GetFailoverMarkerAttributes()
+	assert.Equal(t, failoverMarker, res)
+
+	var nilStruct *ReplicationTask
+	res = nilStruct.GetFailoverMarkerAttributes()
+	assert.Nil(t, res)
+}
+
+func TestReplicationTask_GetCreationTime(t *testing.T) {
+	creationTime := int64(123456)
+	testStruct := ReplicationTask{
+		CreationTime: &creationTime,
+	}
+
+	res := testStruct.GetCreationTime()
+	assert.Equal(t, creationTime, res)
+
+	var nilStruct *ReplicationTask
+	res = nilStruct.GetCreationTime()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskInfo_GetDomainID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		DomainID: "domain-id",
+	}
+
+	res := testStruct.GetDomainID()
+	assert.Equal(t, "domain-id", res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetDomainID()
+	assert.Equal(t, "", res)
+}
+
+func TestReplicationTaskInfo_GetWorkflowID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		WorkflowID: "workflow-id",
+	}
+
+	res := testStruct.GetWorkflowID()
+	assert.Equal(t, "workflow-id", res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetWorkflowID()
+	assert.Equal(t, "", res)
+}
+
+func TestReplicationTaskInfo_GetRunID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		RunID: "run-id",
+	}
+
+	res := testStruct.GetRunID()
+	assert.Equal(t, "run-id", res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetRunID()
+	assert.Equal(t, "", res)
+}
+
+func TestReplicationTaskInfo_GetTaskType(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		TaskType: 1,
+	}
+
+	res := testStruct.GetTaskType()
+	assert.Equal(t, int16(1), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetTaskType()
+	assert.Equal(t, int16(0), res)
+}
+
+func TestReplicationTaskInfo_GetTaskID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		TaskID: 12345,
+	}
+
+	res := testStruct.GetTaskID()
+	assert.Equal(t, int64(12345), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetTaskID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskInfo_GetVersion(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		Version: 100,
+	}
+
+	res := testStruct.GetVersion()
+	assert.Equal(t, int64(100), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetVersion()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskInfo_GetFirstEventID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		FirstEventID: 11111,
+	}
+
+	res := testStruct.GetFirstEventID()
+	assert.Equal(t, int64(11111), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetFirstEventID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskInfo_GetNextEventID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		NextEventID: 22222,
+	}
+
+	res := testStruct.GetNextEventID()
+	assert.Equal(t, int64(22222), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetNextEventID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskInfo_GetScheduledID(t *testing.T) {
+	testStruct := ReplicationTaskInfo{
+		ScheduledID: 33333,
+	}
+
+	res := testStruct.GetScheduledID()
+	assert.Equal(t, int64(33333), res)
+
+	var nilStruct *ReplicationTaskInfo
+	res = nilStruct.GetScheduledID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationToken_GetShardID(t *testing.T) {
+	testStruct := ReplicationToken{
+		ShardID: 1,
+	}
+
+	res := testStruct.GetShardID()
+	assert.Equal(t, int32(1), res)
+
+	var nilStruct *ReplicationToken
+	res = nilStruct.GetShardID()
+	assert.Equal(t, int32(0), res)
+}
+
+func TestReplicationToken_GetLastRetrievedMessageID(t *testing.T) {
+	testStruct := ReplicationToken{
+		LastRetrievedMessageID: 123456,
+	}
+
+	res := testStruct.GetLastRetrievedMessageID()
+	assert.Equal(t, int64(123456), res)
+
+	var nilStruct *ReplicationToken
+	res = nilStruct.GetLastRetrievedMessageID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationToken_GetLastProcessedMessageID(t *testing.T) {
+	testStruct := ReplicationToken{
+		LastProcessedMessageID: 654321,
+	}
+
+	res := testStruct.GetLastProcessedMessageID()
+	assert.Equal(t, int64(654321), res)
+
+	var nilStruct *ReplicationToken
+	res = nilStruct.GetLastProcessedMessageID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestReplicationTaskType_Ptr(t *testing.T) {
+	taskType := ReplicationTaskTypeDomain
+	ptr := taskType.Ptr()
+
+	assert.Equal(t, &taskType, ptr)
+}
+
+func TestReplicationTaskType_String(t *testing.T) {
+	var taskType ReplicationTaskType
+
+	taskType = ReplicationTaskTypeDomain
+	assert.Equal(t, "Domain", taskType.String())
+
+	taskType = ReplicationTaskTypeHistory
+	assert.Equal(t, "History", taskType.String())
+
+	taskType = ReplicationTaskTypeSyncShardStatus
+	assert.Equal(t, "SyncShardStatus", taskType.String())
+
+	taskType = ReplicationTaskTypeSyncActivity
+	assert.Equal(t, "SyncActivity", taskType.String())
+
+	taskType = ReplicationTaskTypeHistoryMetadata
+	assert.Equal(t, "HistoryMetadata", taskType.String())
+
+	taskType = ReplicationTaskTypeHistoryV2
+	assert.Equal(t, "HistoryV2", taskType.String())
+
+	taskType = ReplicationTaskTypeFailoverMarker
+	assert.Equal(t, "FailoverMarker", taskType.String())
+
+	// Test unknown task type
+	taskType = ReplicationTaskType(10)
+	assert.Equal(t, "ReplicationTaskType(10)", taskType.String())
+}
+
+func TestReplicationTaskType_UnmarshalText(t *testing.T) {
+	var taskType ReplicationTaskType
+
+	err := taskType.UnmarshalText([]byte("DOMAIN"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeDomain, taskType)
+
+	err = taskType.UnmarshalText([]byte("HISTORY"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeHistory, taskType)
+
+	err = taskType.UnmarshalText([]byte("SYNCSHARDSTATUS"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeSyncShardStatus, taskType)
+
+	err = taskType.UnmarshalText([]byte("SYNCACTIVITY"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeSyncActivity, taskType)
+
+	err = taskType.UnmarshalText([]byte("HISTORYMETADATA"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeHistoryMetadata, taskType)
+
+	err = taskType.UnmarshalText([]byte("HISTORYV2"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeHistoryV2, taskType)
+
+	err = taskType.UnmarshalText([]byte("FAILOVERMARKER"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskTypeFailoverMarker, taskType)
+
+	// Test unknown string value
+	err = taskType.UnmarshalText([]byte("UNKNOWN"))
+	assert.Error(t, err)
+
+	// Test numeric value parsing
+	err = taskType.UnmarshalText([]byte("10"))
+	assert.NoError(t, err)
+	assert.Equal(t, ReplicationTaskType(10), taskType)
+}
+
+func TestReplicationTaskType_MarshalText(t *testing.T) {
+	var taskType ReplicationTaskType
+
+	taskType = ReplicationTaskTypeDomain
+	text, err := taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("Domain"), text)
+
+	taskType = ReplicationTaskTypeHistory
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("History"), text)
+
+	taskType = ReplicationTaskTypeSyncShardStatus
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("SyncShardStatus"), text)
+
+	taskType = ReplicationTaskTypeSyncActivity
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("SyncActivity"), text)
+
+	taskType = ReplicationTaskTypeHistoryMetadata
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("HistoryMetadata"), text)
+
+	taskType = ReplicationTaskTypeHistoryV2
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("HistoryV2"), text)
+
+	taskType = ReplicationTaskTypeFailoverMarker
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("FailoverMarker"), text)
+
+	// Test unknown task type
+	taskType = ReplicationTaskType(10)
+	text, err = taskType.MarshalText()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("ReplicationTaskType(10)"), text)
+}
+
+func TestSyncActivityTaskAttributes_GetDomainID(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		DomainID: "domain-id",
+	}
+
+	res := testStruct.GetDomainID()
+	assert.Equal(t, "domain-id", res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetDomainID()
+	assert.Equal(t, "", res)
+}
+
+func TestSyncActivityTaskAttributes_GetWorkflowID(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		WorkflowID: "workflow-id",
+	}
+
+	res := testStruct.GetWorkflowID()
+	assert.Equal(t, "workflow-id", res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetWorkflowID()
+	assert.Equal(t, "", res)
+}
+
+func TestSyncActivityTaskAttributes_GetRunID(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		RunID: "run-id",
+	}
+
+	res := testStruct.GetRunID()
+	assert.Equal(t, "run-id", res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetRunID()
+	assert.Equal(t, "", res)
+}
+
+func TestSyncActivityTaskAttributes_GetVersion(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		Version: 12345,
+	}
+
+	res := testStruct.GetVersion()
+	assert.Equal(t, int64(12345), res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetVersion()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetScheduledID(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		ScheduledID: 67890,
+	}
+
+	res := testStruct.GetScheduledID()
+	assert.Equal(t, int64(67890), res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetScheduledID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetScheduledTime(t *testing.T) {
+	scheduledTime := int64(1234567890)
+	testStruct := SyncActivityTaskAttributes{
+		ScheduledTime: &scheduledTime,
+	}
+
+	res := testStruct.GetScheduledTime()
+	assert.Equal(t, scheduledTime, res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetScheduledTime()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetStartedID(t *testing.T) {
+	testStruct := SyncActivityTaskAttributes{
+		StartedID: 11111,
+	}
+
+	res := testStruct.GetStartedID()
+	assert.Equal(t, int64(11111), res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetStartedID()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetStartedTime(t *testing.T) {
+	startedTime := int64(1234567890)
+	testStruct := SyncActivityTaskAttributes{
+		StartedTime: &startedTime,
+	}
+
+	res := testStruct.GetStartedTime()
+	assert.Equal(t, startedTime, res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetStartedTime()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetLastHeartbeatTime(t *testing.T) {
+	lastHeartbeatTime := int64(9876543210)
+	testStruct := SyncActivityTaskAttributes{
+		LastHeartbeatTime: &lastHeartbeatTime,
+	}
+
+	res := testStruct.GetLastHeartbeatTime()
+	assert.Equal(t, lastHeartbeatTime, res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetLastHeartbeatTime()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestSyncActivityTaskAttributes_GetVersionHistory(t *testing.T) {
+	versionHistory := &VersionHistory{}
+	testStruct := SyncActivityTaskAttributes{
+		VersionHistory: versionHistory,
+	}
+
+	res := testStruct.GetVersionHistory()
+	assert.Equal(t, versionHistory, res)
+
+	var nilStruct *SyncActivityTaskAttributes
+	res = nilStruct.GetVersionHistory()
+	assert.Nil(t, res)
+}
+
+func TestSyncShardStatus_GetTimestamp(t *testing.T) {
+	timestamp := int64(9876543210)
+	testStruct := SyncShardStatus{
+		Timestamp: &timestamp,
+	}
+
+	res := testStruct.GetTimestamp()
+	assert.Equal(t, timestamp, res)
+
+	var nilStruct *SyncShardStatus
+	res = nilStruct.GetTimestamp()
+	assert.Equal(t, int64(0), res)
+}
+
+func TestGetDomainReplicationMessagesRequest_SerializeForLogging(t *testing.T) {
+	// Test case where the struct is nil
+	var nilStruct *GetDomainReplicationMessagesRequest
+	res, err := nilStruct.SerializeForLogging()
+	assert.Equal(t, "", res)
+	assert.NoError(t, err)
+
+	// Test case with a non-nil struct
+	lastRetrievedMessageID := int64(12345)
+	lastProcessedMessageID := int64(67890)
+	testStruct := GetDomainReplicationMessagesRequest{
+		LastRetrievedMessageID: &lastRetrievedMessageID,
+		LastProcessedMessageID: &lastProcessedMessageID,
+		ClusterName:            "test-cluster",
+	}
+
+	res, err = testStruct.SerializeForLogging()
+	assert.NotEmpty(t, res)
+	assert.NoError(t, err)
+}
