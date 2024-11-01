ffe7ac06141cd0749e801a9cd63b1515a6d87770
taylan isikdemir <taylan@uber.com>
2024-10-31 21:49:27 -0700

    init

--- common/rpc/direct_peer_chooser.go
+++ common/rpc/direct_peer_chooser.go
@@ -22,17 +22,23 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"go.uber.org/yarpc/api/peer"
 	"go.uber.org/yarpc/api/transport"
 	"go.uber.org/yarpc/peer/direct"
+	"go.uber.org/yarpc/peer/hostport"
 	"go.uber.org/yarpc/yarpcerrors"
 
 	"github.com/uber/cadence/common/dynamicconfig"
 	"github.com/uber/cadence/common/log"
 	"github.com/uber/cadence/common/log/tag"
 	"github.com/uber/cadence/common/membership"
+)
+
+var (
+	noOpSubscriberInstance = &noOpSubscriber{}
 )
 
 // directPeerChooser is a peer.Chooser that chooses a peer based on the shard key.
@@ -45,6 +51,7 @@ type directPeerChooser struct {
 	legacyChooser        peer.Chooser
 	legacyChooserErr     error
 	mu                   sync.RWMutex
+	peers                map[string]peer.Peer
 }
 
 func newDirectChooser(serviceName string, t peer.Transport, logger log.Logger, enableConnRetainMode dynamicconfig.BoolPropertyFn) *directPeerChooser {
@@ -87,6 +94,7 @@ func (g *directPeerChooser) IsRunning() bool {
 }
 
 // Choose returns an existing peer for the shard key.
+// ShardKey is {host}:{port} of the peer. It could be tchannel or grpc address.
 func (g *directPeerChooser) Choose(ctx context.Context, req *transport.Request) (peer peer.Peer, onFinish func(error), err error) {
 	if g.enableConnRetainMode != nil && !g.enableConnRetainMode() {
 		return g.chooseFromLegacyDirectPeerChooser(ctx, req)
@@ -96,13 +104,85 @@ func (g *directPeerChooser) Choose(ctx context.Context, req *transport.Request) 
 		return nil, nil, yarpcerrors.InvalidArgumentErrorf("chooser requires ShardKey to be non-empty")
 	}
 
-	// TODO: implement connection retain mode
-	return nil, nil, yarpcerrors.UnimplementedErrorf("direct peer chooser conn retain mode unimplemented")
+	g.mu.RLock()
+	p, ok := g.peers[req.ShardKey]
+	if ok {
+		g.mu.RUnlock()
+		return p, func(error) {}, nil
+	}
+	g.mu.RUnlock()
+
+	// peer is not cached, add new peer
+	p, err = g.addPeer(req.ShardKey)
+	if err != nil {
+		return nil, nil, yarpcerrors.InternalErrorf("failed to add peer for shard key %v, err: %v", req.ShardKey, err)
+	}
+
+	return p, func(error) {}, nil
 }
 
 func (g *directPeerChooser) UpdatePeers(members []membership.HostInfo) {
-	// TODO: implement
 	g.logger.Debug("direct peer chooser got a membership update", tag.Counter(len(members)))
+
+	// Create a map of valid peer addresses given members list. This is used to remove peers that are not in the members list.
+	// Actual yarpc peers are not created for the members here. They are created lazily when a request comes in.
+	validPeerAddresses := make(map[string]bool)
+	for _, member := range members {
+		for _, portName := range []string{membership.PortTchannel, membership.PortGRPC} {
+			addr, err := member.GetNamedAddress(portName)
+			if err != nil {
+				g.logger.Error(fmt.Sprintf("failed to get %s address of member", portName), tag.Error(err), tag.Address(member.GetAddress()))
+				continue
+			}
+			validPeerAddresses[addr] = true
+		}
+	}
+
+	// Take a copy of the current peers to avoid keeping write lock while removing all peers.
+	peers := make(map[string]bool)
+	g.mu.RLock()
+	for addr := range g.peers {
+		peers[addr] = true
+	}
+	g.mu.RUnlock()
+
+	for addr := range peers {
+		if !validPeerAddresses[addr] {
+			g.removePeer(addr)
+		}
+	}
+
+	// todo: emit gauge metric for peers
+}
+
+func (g *directPeerChooser) removePeer(addr string) {
+	// TODO: is ReleasePeer thread safe? If not we need to hold the write lock while releasing the peer.
+	if err := g.t.ReleasePeer(g.peers[addr], noOpSubscriberInstance); err != nil {
+		g.logger.Error("failed to release peer", tag.Error(err), tag.Address(addr))
+	}
+
+	g.mu.Lock()
+	delete(g.peers, addr)
+	g.mu.Unlock()
+	g.logger.Debug("removed peer from direct peer chooser", tag.Address(addr))
+	// TODO: emit count metric
+}
+
+func (g *directPeerChooser) addPeer(addr string) (peer.Peer, error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	if p, ok := g.peers[addr]; ok {
+		return p, nil
+	}
+
+	p, err := g.t.RetainPeer(hostport.Identify(addr), noOpSubscriberInstance)
+	if err != nil {
+		return nil, err
+	}
+	g.peers[addr] = p
+	g.logger.Debug("added peer to direct peer chooser", tag.Address(addr))
+	// TODO: emit count metric
+	return p, nil
 }
 
 func (g *directPeerChooser) chooseFromLegacyDirectPeerChooser(ctx context.Context, req *transport.Request) (peer.Peer, func(error), error) {
@@ -142,3 +222,8 @@ func (g *directPeerChooser) getLegacyChooser() (peer.Chooser, bool) {
 
 	return g.legacyChooser, true
 }
+
+// noOpSubscriber is a peer.Subscriber that does nothing.
+type noOpSubscriber struct{}
+
+func (*noOpSubscriber) NotifyStatusChanged(peer.Identifier) {}
