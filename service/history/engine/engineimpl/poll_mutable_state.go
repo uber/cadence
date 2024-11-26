a1e18b28bb593de15fb99e6d8d97891d75d6122d
David Porter <david.porter@uber.com>
2024-11-25 16:29:04 -0800

    adding lock

--- service/history/engine/engineimpl/poll_mutable_state.go
+++ service/history/engine/engineimpl/poll_mutable_state.go
@@ -83,6 +83,12 @@ func (e *historyEngineImpl) getMutableState(
 	}
 	defer func() { release(retError) }()
 
+	err := wfContext.Lock(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't lock workflow context: %w", err)
+	}
+	defer wfContext.Unlock()
+
 	mutableState, retError := wfContext.LoadWorkflowExecution(ctx)
 	if retError != nil {
 		return
